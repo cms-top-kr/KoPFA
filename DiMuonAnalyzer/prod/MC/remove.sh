8a30ef7bf338b7ac321c1059aae723fc55a9720e
tjkim <>
2010-07-23 12:56:42 +0000

    to remove dir

---
+++ DiMuonAnalyzer/prod/MC/remove.sh
@@ -0,0 +1,13 @@
+rm -rf InclusiveMu15
+rm -rf ppMuX 
+rm -rf Zmumu 
+rm -rf Ztautau 
+rm -rf TTbar2l 
+rm -rf TTbar 
+rm -rf Wmunu 
+rm -rf Wtaunu  
+rm -rf DYmumu_M10to20
+rm -rf QCD_Pt15
+rm -rf QCD_Pt30 
+rm -rf ZJets
+rm -rf WJets
