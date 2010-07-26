8ffaf8e345016960cefd61c90b86069b20f54893
tjkim <>
2010-07-26 08:53:11 +0000

    update to use event counter

--- DiMuonAnalyzer/test/dimuonanalyzer_cfg.py
+++ DiMuonAnalyzer/test/dimuonanalyzer_cfg.py
@@ -6,23 +6,27 @@ process.load("FWCore.MessageService.MessageLogger_cfi")
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
 
-process.load("KoPFA.DiMuonAnalyzer.RD.SDMUON_MuonFilter_Run_135176_135802_cff")
-
-#process.source = cms.Source("PoolSource",
+process.source = cms.Source("PoolSource",
     # replace 'myfile.root' with the source file you want to use
-#    fileNames = cms.untracked.vstring(
-#        'file:myfile.root'
-#    )
-#)
+    fileNames = cms.untracked.vstring(
+        'file:../prod/RD/patTuple_muon.root'
+    )
+)
 
 # register TFileService
 process.TFileService = cms.Service("TFileService",
     fileName = cms.string('vallot.root')
 )
 
+process.load("KoPFA.CommonTools.countingSequences_cfi")
+
 process.DiMuon = cms.EDAnalyzer('DiMuonAnalyzer',
   muonLabel =  cms.InputTag('selectedPatMuons'),
+  filters = cms.untracked.vstring(
+                              'initialEvents',
+                              'finalEvents'
+                              ),
+
 )
 
-
-process.p = cms.Path(process.DiMuon)
+process.p = cms.Path(process.loadHistosFromRunInfo*process.DiMuon)
