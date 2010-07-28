7bdfc735e122bb289559836ede594c0c10071123
tjkim <>
2010-07-28 09:15:04 +0000

    update

--- DiMuonAnalyzer/prod/RD/lumi.sh
+++ DiMuonAnalyzer/prod/RD/lumi.sh
@@ -1,2 +1,3 @@
-lumiCalc.py -c frontier://LumiProd/CMS_LUMI_PROD -i Cert_132440-137028_7TeV_June14thReReco_Collisions10_JSON_TopFiltered_July11th.txt overview > Cert_132440-137028_7TeV_June14thReReco_Collisions10_JSON_TopFiltered_July11th_lumi.txt
-lumiCalc.py -c frontier://LumiProd/CMS_LUMI_PROD -i Cert_132440-139790_7TeV_StreamExpress_Collisions10_JSON_TopFiltered_July11th.txt overview > Cert_132440-139790_7TeV_StreamExpress_Collisions10_JSON_TopFiltered_July11th_lumi.txt 
+lumiCalc.py -c frontier://LumiProd/CMS_LUMI_PROD -i Cert_132440-137028_7TeV_June14thReReco_Collisions10_JSON.txt overview > Cert_132440-137028_7TeV_June14thReReco_Collisions10_JSON_lumi.txt 
+lumiCalc.py -c frontier://LumiProd/CMS_LUMI_PROD -i Cert_139779-1340159_7TeV_July16thReReco_Collisions10_JSON.txt  overview > Cert_139779-1340159_7TeV_July16thReReco_Collisions10_JSON_lumi.txt  
+lumiCalc.py -c frontier://LumiProd/CMS_LUMI_PROD -i Cert_132440-140182_7TeV_StreamExpress_Collisions10_JSON.txt overview > Cert_132440-140182_7TeV_StreamExpress_Collisions10_JSON_lumi.txt 
