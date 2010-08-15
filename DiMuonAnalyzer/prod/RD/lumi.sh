c31f252094389534f03367f3890d518a9a970f65
tjkim <>
2010-08-15 22:10:18 +0000

    update accordingly

--- DiMuonAnalyzer/prod/RD/lumi.sh
+++ DiMuonAnalyzer/prod/RD/lumi.sh
@@ -1,3 +1,3 @@
 lumiCalc.py -c frontier://LumiProd/CMS_LUMI_PROD -i Cert_132440-137028_7TeV_June14thReReco_Collisions10_JSON_v2_TopFiltered_135059-137028.txt overview > Aug9th_lumi.txt 
 lumiCalc.py -c frontier://LumiProd/CMS_LUMI_PROD -i Cert_139779-1340159_7TeV_July16thReReco_Collisions10_JSON.txt  overview >> Aug9th_lumi.txt  
-lumiCalc.py -c frontier://LumiProd/CMS_LUMI_PROD -i Cert_132440-141961_7TeV_StreamExpress_Collisions10_JSON_TopAug9_Filtered_138564-141961_Extended_141962-142537.txt overview >> Aug9th_lumi.txt 
+lumiCalc.py -c frontier://LumiProd/CMS_LUMI_PROD -i Cert_132440-142664_7TeV_StreamExpress_Collisions10_JSON_TopAug13_Filtered_138564-142664.txt overview >> Aug9th_lumi.txt 
