25f1c04cf7cd58ac6dad2a6cf2df0fdba0db8e00
tjkim <>
2010-08-29 21:01:43 +0000

    update

--- DiMuonAnalyzer/test/dimuonanalyzer_cfg.py
+++ DiMuonAnalyzer/test/dimuonanalyzer_cfg.py
@@ -2,5 +2,7 @@ from KoPFA.DiMuonAnalyzer.dimuonanalyzer_cff import process
 
 import FWCore.ParameterSet.Config as cms
 
+process.load("KoPFA.DiMuonAnalyzer.RD.patTuple_Run2010A_PromptReco_cff")
+
 process.VertexFilter.min = 1
 process.VertexFilter.max = 9999
