858dfa738dd5c393571aa0a060bb7960926a8870
tjkim <>
2010-09-01 18:19:51 +0000

    copy to tjkim desktop

---
+++ DiMuonAnalyzer/test/copy_mc.sh
@@ -0,0 +1,19 @@
+#rfcpMany.py /castor/cern.ch/user/t/tjkim/ntuple/MC/Summer10/InclusiveMu15 /home/tjkim/ntuple/mc/top/Summer10/InclusiveMu15 ".*root"
+#rfcpMany.py /castor/cern.ch/user/t/tjkim/ntuple/MC/Summer10/TTbar /home/tjkim/ntuple/mc/top/Summer10/TTbar ".*root"
+#rfcpMany.py /castor/cern.ch/user/t/tjkim/ntuple/MC/Summer10/Zmumu /home/tjkim/ntuple/mc/top/Summer10/Zmumu ".*root"
+#rfcpMany.py /castor/cern.ch/user/t/tjkim/ntuple/MC/Summer10/Ztautau /home/tjkim/ntuple/mc/top/Summer10/Ztautau ".*root"
+#rfcpMany.py /castor/cern.ch/user/t/tjkim/ntuple/MC/Summer10/Wmunu /home/tjkim/ntuple/mc/top/Summer10/Wmunu ".*root"
+#rfcpMany.py /castor/cern.ch/user/t/tjkim/ntuple/MC/Summer10/Wtaunu /home/tjkim/ntuple/mc/top/Summer10/Wtaunu ".*root"
+
+rfcpMany.py /castor/cern.ch/user/t/tjkim/ntuple/MC/Spring10/InclusiveMu15 /home/tjkim/ntuple/mc/top/Spring10/InclusiveMu15 ".*root"
+#rfcpMany.py /castor/cern.ch/user/t/tjkim/ntuple/MC/Spring10/TTbar /home/tjkim/ntuple/mc/top/Spring10/TTbar ".*root"
+#rfcpMany.py /castor/cern.ch/user/t/tjkim/ntuple/MC/Spring10/Zmumu /home/tjkim/ntuple/mc/top/Spring10/Zmumu ".*root"
+#rfcpMany.py /castor/cern.ch/user/t/tjkim/ntuple/MC/Spring10/Ztautau /home/tjkim/ntuple/mc/top/Spring10/Ztautau ".*root"
+#rfcpMany.py /castor/cern.ch/user/t/tjkim/ntuple/MC/Spring10/ZJets /home/tjkim/ntuple/mc/top/Spring10/ZJets ".*root"
+#rfcpMany.py /castor/cern.ch/user/t/tjkim/ntuple/MC/Spring10/WJets /home/tjkim/ntuple/mc/top/Spring10/WJets ".*root"
+#rfcpMany.py /castor/cern.ch/user/t/tjkim/ntuple/MC/Spring10/VVJets /home/tjkim/ntuple/mc/top/Spring10/VVJets ".*root"
+#rfcpMany.py /castor/cern.ch/user/t/tjkim/ntuple/MC/Spring10/SingleTop /home/tjkim/ntuple/mc/top/Spring10/SingleTop ".*root"
+#rfcpMany.py /castor/cern.ch/user/t/tjkim/ntuple/MC/Spring10/DYmumu /home/tjkim/ntuple/mc/top/Spring10/DYmumu ".*root"
+
+
+
