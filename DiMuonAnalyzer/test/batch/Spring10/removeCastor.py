785e6113c69d52f0a68913c54e7c036decbe9024
tjkim <>
2010-09-01 09:46:02 +0000

    tool

---
+++ DiMuonAnalyzer/test/batch/Spring10/removeCastor.py
@@ -0,0 +1,9 @@
+rfrm -r /castor/cern.ch/user/t/tjkim/ntuple/MC/Spring10/InclusiveMu15
+rfrm -r /castor/cern.ch/user/t/tjkim/ntuple/MC/Spring10/TTbar
+rfrm -r /castor/cern.ch/user/t/tjkim/ntuple/MC/Spring10/Zmumu
+rfrm -r /castor/cern.ch/user/t/tjkim/ntuple/MC/Spring10/Ztautau
+rfrm -r /castor/cern.ch/user/t/tjkim/ntuple/MC/Spring10/ZJets
+rfrm -r /castor/cern.ch/user/t/tjkim/ntuple/MC/Spring10/WJets
+rfrm -r /castor/cern.ch/user/t/tjkim/ntuple/MC/Spring10/VVJets
+rfrm -r /castor/cern.ch/user/t/tjkim/ntuple/MC/Spring10/SingleTop
+rfrm -r /castor/cern.ch/user/t/tjkim/ntuple/MC/Spring10/DYmumu
