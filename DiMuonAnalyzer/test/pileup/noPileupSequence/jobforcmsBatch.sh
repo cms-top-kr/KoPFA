f2b7dc48438da3d2c9ea38bc5958800cbe853951
tjkim <>
2010-09-01 09:43:15 +0000

    for no pile up enabled

---
+++ DiMuonAnalyzer/test/pileup/noPileupSequence/jobforcmsBatch.sh
@@ -0,0 +1,9 @@
+#cmsBatch.py 1 dimuonanalyzer_pileup_1_cfg.py -o Out/11 -r /castor/cern.ch/user/t/tjkim/ntuple/pileupstudy/RD_eta25/noPileupSequence/pileup1/vallot.root -b 'bsub -q 1nh < batchScript.sh' 
+#cmsBatch.py 1 dimuonanalyzer_pileup_2_cfg.py -o Out/12 -r /castor/cern.ch/user/t/tjkim/ntuple/pileupstudy/RD_eta25/noPileupSequence/pileup2/vallot.root -b 'bsub -q 1nh < batchScript.sh' 
+#cmsBatch.py 1 dimuonanalyzer_pileup_3_cfg.py -o Out/13 -r /castor/cern.ch/user/t/tjkim/ntuple/pileupstudy/RD_eta25/noPileupSequence/pileup3/vallot.root -b 'bsub -q 1nh < batchScript.sh' 
+cmsBatch.py 1 dimuonanalyzer_pileup_4_cfg.py -o Out/14 -r /castor/cern.ch/user/t/tjkim/ntuple/pileupstudy/RD_eta25/noPileupSequence/pileup4/vallot.root -b 'bsub -q 1nh < batchScript.sh'
+
+#cmsBatch.py 1 dimuonanalyzer_nopileup_1_cfg.py -o Out/21 -r /castor/cern.ch/user/t/tjkim/ntuple/pileupstudy/RD_eta25/noPileupSequence/nopileup1/vallot.root -b 'bsub -q 1nh < batchScript.sh' 
+#cmsBatch.py 1 dimuonanalyzer_nopileup_2_cfg.py -o Out/22 -r /castor/cern.ch/user/t/tjkim/ntuple/pileupstudy/RD_eta25/noPileupSequence/nopileup2/vallot.root -b 'bsub -q 1nh < batchScript.sh' 
+#cmsBatch.py 1 dimuonanalyzer_nopileup_3_cfg.py -o Out/23 -r /castor/cern.ch/user/t/tjkim/ntuple/pileupstudy/RD_eta25/noPileupSequence/nopileup3/vallot.root -b 'bsub -q 1nh < batchScript.sh' 
+#cmsBatch.py 1 dimuonanalyzer_nopileup_4_cfg.py -o Out/24 -r /castor/cern.ch/user/t/tjkim/ntuple/pileupstudy/RD_eta25/noPileupSequence/nopileup4/vallot.root -b 'bsub -q 1nh < batchScript.sh'
