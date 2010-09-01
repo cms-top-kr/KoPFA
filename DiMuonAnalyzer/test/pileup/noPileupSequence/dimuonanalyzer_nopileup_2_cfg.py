33051b0b46b4e919f3875faa99ad548baf8adace
tjkim <>
2010-09-01 09:42:44 +0000

    for no pileup enabled

---
+++ DiMuonAnalyzer/test/pileup/noPileupSequence/dimuonanalyzer_nopileup_2_cfg.py
@@ -0,0 +1,8 @@
+from KoPFA.DiMuonAnalyzer.dimuonanalyzer_cff import process
+
+import FWCore.ParameterSet.Config as cms
+
+process.load("KoPFA.DiMuonAnalyzer.RD.NoPileupSequence.patTuple_Run2010A_Jun14thReReco_cff")
+
+process.VertexFilter.min = 1
+process.VertexFilter.max = 1
