ef39697bf32ee08ca9a1d3b75a3c70222d47342e
tjkim <>
2010-09-08 22:03:03 +0000

    almost final version

--- TopAnalyzer/test/anal.C
+++ TopAnalyzer/test/anal.C
@@ -12,52 +12,54 @@
 #include "TFile.h"
 #include "TTree.h"
 #include "TMath.h"
-//#include "norm.h"
 #include "style.h"
 
 double lumi_e = 2.88;
-TString dir = "TOPDIL_noPileupSequence";
-//TString dir = "TOPDIL_NoPileupSequence";
+TString dir = "temp";
 bool print = false;
-ofstream myfile;
+//ofstream myfile;
+
+std::vector<TFile*> files;
 std::vector<TTree*> trees;
 std::vector<TString> hNames;
 std::vector<TString> labels;
 std::vector<double> norms;
 std::vector<Color_t> colors;
+
+TFile * fdata;
 TTree * data;
 
 void numberOfEvents( TCut cut, const TString& s){
 
   double ndata = data->GetEntries(cut);
-  vector<double> nbkg;
   double total = 0;
   double sumerr2 = 0;
-  cout << s.Data() << " ------------------ " <<  endl;
+  double sig = 0;
+
+  cout << s.Data() << "------------------------" << endl; 
+
   for(int i=0; i < (int) trees.size() ; i++){
     double numEvt = trees[i]->GetEntries(cut)*norms[i];
+    if(hNames[i].Contains("TTbar")) sig = numEvt;
     double err = sqrt(numEvt*norms[i]);
-    nbkg.push_back(numEvt);
     cout << hNames[i] << " = " << numEvt << " +- " << err << endl;
     total = total + numEvt;
     sumerr2 = sumerr2 + err*err;
   }
 
-  double sig = nbkg[4];
   double bkg = total - sig;
   double soverb = sig/TMath::Sqrt(sig+bkg);
 
   double eTotal = sqrt(sumerr2);
 
   cout << "Total   = " << total << " +- " << eTotal << endl;
-  cout << "------------------------ " <<  endl;
+  cout << "----------------------------- " <<  endl;
   cout << "Data    = " << ndata << endl;
   cout << "s/Sqrt(s+b) = " << soverb << endl;
-  cout << "------------------------ " <<  endl;
- 
+  cout << "----------------------------- " <<  endl;
+
 }
 
-//void plotStackBase(TH1* h_data, TH1 * h_Zmumu, TH1 * h_Ztautau, TH1* h_Wmunu, TH1* h_QCD, TH1* h_TTbar, const TString & xtitle, const TString & ytitle, const int& nbin,  const double& max, const double & min, const TString& hName, const TString& name, const bool & log){
 void plotStackBase(TH1* h_data, vector<TH1*> h_mc, const TString & xtitle, const TString & ytitle, const int& nbin,  const double& max, const double & min, const TString& hName, const TString& name, const bool & log){
 
   TCanvas* c = new TCanvas(Form("c_%s_%s",hName.Data(), name.Data()),Form("c_%s_%s",hName.Data(), name.Data()),1);
@@ -66,7 +68,7 @@ void plotStackBase(TH1* h_data, vector<TH1*> h_mc, const TString & xtitle, const
 
   //overflow adding
   h_data->AddBinContent(nbin, h_data->GetBinContent(nbin+1)) ;
-  for(int i=0; i < trees.size() ; i++){
+  for(int i=0; i < (int) trees.size() ; i++){
     h_mc[i]->AddBinContent(nbin,h_mc[i]->GetBinContent(nbin+1)) ;
     h_mc[i]->Scale(norms[i]);
     h_mc[i]->SetFillColor(colors[i]);
@@ -81,24 +83,23 @@ void plotStackBase(TH1* h_data, vector<TH1*> h_mc, const TString & xtitle, const
   h_data->GetYaxis()->SetTitle(ytitle.Data());
 
   THStack *stack = new THStack("stack","stack");
-  
-  stack->Add(h_mc[1]);
-  stack->Add(h_mc[4]);
-  stack->Add(h_mc[3]);
-  stack->Add(h_mc[2]);
-  stack->Add(h_mc[0]);
+ 
+  for(int i=0; i < (int) trees.size() ; i++){ 
+    stack->Add(h_mc[i]);
+  }
 
-  stack->SetTitle(0);
   if(max != -1)
     stack->SetMaximum(max); 
   if(min != -1)
     stack->SetMinimum(min);
+
+  stack->SetTitle(0);
   stack->Draw();
   h_data->Draw("same");
 
-  TLegend *l= new TLegend(0.75,0.65,0.90,0.88);
+  TLegend *l= new TLegend(0.73,0.57,0.88,0.88);
   l->AddEntry(h_data,"Data","p");
-  for(int i=0 ;i < trees.size() ; i++){
+  for(int i= (int)trees.size() -1 ;i >= 0 ; i--){
     l->AddEntry(h_mc[i],labels[i],"f");
   }
   l->SetTextSize(0.04);
@@ -110,18 +111,17 @@ void plotStackBase(TH1* h_data, vector<TH1*> h_mc, const TString & xtitle, const
 
 }
 
-//void histStack(TFile* fdata, TFile * fZmumu, TFile * fZtautau, TFile* fWmunu, TFile* fQCD, TFile* fTTbar, TString dir, TString var, TString & xtitle, TString & ytitle, const int& nbin, const double& max, const double & min, const TString& hName, const TString& name, const  bool & log){
+void histStack(const TString& dir, const TString& var, const TString & xtitle, const TString & ytitle, const int& nbin, const double& max, const double & min, const TString& hName, const TString& name, const  bool & log){
 
-//  TH1F * h_data = (TH1F *) fdata->Get(Form("%s/%s",dir.Data(),var.Data()));
-//  TH1F * h_Zmumu = (TH1F *) fZmumu->Get(Form("%s/%s",dir.Data(),var.Data()));
-//  TH1F * h_Ztautau = (TH1F *) fZtautau->Get(Form("%s/%s",dir.Data(),var.Data()));
-//  TH1F * h_Wmunu = (TH1F *) fWmunu->Get(Form("%s/%s",dir.Data(),var.Data()));
-//  TH1F * h_QCD = (TH1F *) fQCD->Get(Form("%s/%s",dir.Data(),var.Data()));
-//  TH1F * h_TTbar = (TH1F *) fTTbar->Get(Form("%s/%s",dir.Data(),var.Data()));
+  TH1F * h_data = (TH1F *) fdata->Get(Form("%s/%s",dir.Data(),var.Data()));
+  std::vector<TH1*> h_mc;
+  
+  for(int i=0; i < (int) trees.size() ; i++){
+    h_mc.push_back((TH1F *) files[i]->Get(Form("%s/%s",dir.Data(),var.Data())));
+  }
+  plotStackBase(h_data, h_mc, xtitle, ytitle, nbin,  max, min, hName, name, log);
 
-//  plotStackBase(h_data, h_Zmumu, h_Ztautau, h_Wmunu, h_QCD, h_TTbar, xtitle, ytitle, nbin,  max, min, hName, name, log);
-
-//}
+}
 
 
 void plotStack( TString var, TCut cut, const TString & xtitle, const TString & ytitle, const int& nbin, const double& xlow, const double& xmax, const double& max, const double & min, const TString& hName, const TString& name, const bool & log){
@@ -144,8 +144,8 @@ void plotStack( TString var, TCut cut, const TString & xtitle, const TString & y
 
 void plotStep(TCut cut, const TString& step, const double & r){
 
-  //histStack(fdata, fZmumu, fZtautau, fWmunu, fQCD, fTTbar, "h_mass","Dimuon Mass (GeV/c^{2})", "Events/5 GeV/c^{2}",40, 1000,0.02, "dimuon",Form("mass_h_%s",step.Data()), true);
-  //histStack(fdata, fZmumu, fZtautau, fWmunu, fQCD, fTTbar, "h_leadingpt","Leading p_{T} (GeV/c)", "Events/5 GeV/c",20, 30, 0, "leading",Form("pt_h_%s",step.Data()), false);
+  //histStack( "DiMuon", "h_mass","Dimuon Mass (GeV/c^{2})", "Events/5 GeV/c^{2}",40, 1000,0.02, "dimuon",Form("mass_h_%s",step.Data()), true);
+  //histStack( "DiMuon", "h_leadingpt","Leading p_{T} (GeV/c)", "Events/5 GeV/c",20, 30, 0, "leading",Form("pt_h_%s",step.Data()), false);
 
   if( step.Contains("step0") || step.Contains("step1") || step.Contains("step2") ){
     plotStack( "Z.mass()",cut, "Dimuon Mass (GeV/c^{2})", "Events/5 GeV/c^{2}",40, 0, 200, 1000*r,0.02*r, "dimuon",Form("mass_%s",step.Data()), true);
@@ -171,6 +171,8 @@ void addFile(TFile* f, const TString& h, const TString& l, const double & sigma,
   double norm = lumi_e/lumi; 
   cout << h << " = " << lumi << " pb-1 ( " << nevents << " events) " << endl; 
   TTree * tree = (TTree *) f->Get("DiMuon/tree");
+
+  files.push_back(f);
   trees.push_back(tree);
   hNames.push_back(h);
   labels.push_back(l);
@@ -178,28 +180,34 @@ void addFile(TFile* f, const TString& h, const TString& l, const double & sigma,
   colors.push_back(c);
 }  
 
-void anal(){
-  myfile.open (Form("Info_%s.txt",dir.Data()));
+void anal(bool p= false, const TString& d="temp"){
+  print = p;
+  dir = d;
+  
+  //myfile.open (Form("Info_%s.txt",dir.Data()));
 
   using namespace std;
-  //TFile * fdata = new TFile("/home/tjkim/ntuple/data/top/Aug13/vallot_presentedAug26.root");
-  //TFile * fdata = new TFile("/home/tjkim/ntuple/data/top/Aug13/vallot.root");
-  TFile * fdata = new TFile("/home/tjkim/ntuple/data/top/Sep3_noPileupSequence/vallot.root");
+  //fdata = new TFile("/home/tjkim/ntuple/data/top/Aug13/vallot_presentedAug26.root");
+  //fdata = new TFile("/home/tjkim/ntuple/data/top/Aug13/vallot.root");
+  fdata = new TFile("/home/tjkim/ntuple/data/top/Sep3_noPileupSequence/vallot.root");
   //TFile * fZmumu = new TFile("/home/tjkim/ntuple/mc/top/Spring10/vallot_Zmumu.root");
   TFile * fZmumu = new TFile("/home/tjkim/ntuple/mc/top/Spring10/vallot_ZJets.root");
   TFile * fZtautau = new TFile("/home/tjkim/ntuple/mc/top/Spring10/vallot_Ztautau.root");
   TFile * fWmunu = new TFile("/home/tjkim/ntuple/mc/top/Spring10/vallot_WJets.root");
   TFile * fQCD = new TFile("/home/tjkim/ntuple/mc/top/Spring10/vallot_QCD.root");
   TFile * fTTbar = new TFile("/home/tjkim/ntuple/mc/top/Spring10/vallot_TTbar.root");
+  TFile * fVVJets = new TFile("/home/tjkim/ntuple/mc/top/Spring10/vallot_VVJets.root");
+  TFile * fSingleTop = new TFile("/home/tjkim/ntuple/mc/top/Spring10/vallot_SingleTop.root");
 
   data = (TTree *) fdata->Get("DiMuon/tree");
 
-  addFile(fZmumu, "Zmumu", "Z #rightarrow #mu#mu", 3048, 2);
-  addFile(fQCD, "QCD", "QCD", 79688, 3);
-  addFile(fZtautau, "Ztautau", "Z #rightarrow #tau#tau", 1660, 5);
-  addFile(fWmunu, "Wmunu", "W #rightarrow #mu#nu", 31314, 6);
-  addFile(fTTbar, "TTbar", "t#bar{t}", 157.5, 4); 
-
+  addFile(fTTbar, "TTbar", "t#bar{t}", 157.5, 4);//Blue
+  addFile(fQCD, "QCD", "QCD", 79688, 3);//Green
+  addFile(fWmunu, "Wlnu", "W #rightarrow l#nu", 31314, 46);// brown
+  addFile(fVVJets, "VV", "VV", 4.8 ,6 );//Magenta
+  addFile(fSingleTop, "SingleTop", "single top", 10.6 ,7 );// sky
+  addFile(fZtautau, "Ztautau", "Z/#gamma* #rightarrow #tau#tau", 1660, 5);//Yellow
+  addFile(fZmumu, "Zll", "Z/#gamma* #rightarrow ll", 3048, 2);//Red  
 
   TCut pfiso = "(chIso1+nhIso1/0.33+phIso1)/Z.leg1().pt() < 0.21 && (chIso2+nhIso2/0.33+phIso2)/Z.leg2().pt() < 0.21 ";
 
@@ -211,7 +219,7 @@ void anal(){
   TCut step4 = step3 && "@jetspt30.size() >= 1";
   TCut step5 = step4 && "MET > 30";
 
-  //histStack(fdata, fZmumu, fZtautau, fWmunu, fQCD, fTTbar, "Muons", "cutflow","cutflow", "Muons",6, 60000, 1, "muon","cutflow", false);
+  //histStack("Muons", "cutflow","cutflow", "Muons",6, 60000, 1, "muon","cutflow", false);
 
   bool isoPlot = false;
   if(isoPlot){
@@ -225,16 +233,16 @@ void anal(){
     plotStack( "phIso2",step0, "phIso", "Events", 40, 0, 4, max , min, "iso","phIso2", true);
   }
 
-  //plotStep(step0, "step0",1); 
+  plotStep( step0, "step0",1); 
   plotStep( step1, "step1",1); 
-  //plotStep( step2, "step2",1); 
-  //plotStep( step3, "step3",0.1); 
-  //plotStep( step4, "step4",0.1); 
-  //plotStep( step5, "step5",0.1); 
+  plotStep( step2, "step2",1); 
+  plotStep( step3, "step3",0.1); 
+  plotStep( step4, "step4",0.1); 
+  plotStep( step5, "step5",0.1); 
 
   cout <<"step5" << endl;
   data->Scan("RUN:LUMI:EVENT",step5);
 
-  myfile.close();
+  //myfile.close();
 }
 
