5185233d2faf924ed1a6bba6be0e9bfb3c8ab1b2
tjkim <>
2010-09-08 09:50:46 +0000

    first version

---
+++ TopAnalyzer/python/topDiElAnalyzer_cff.py
@@ -0,0 +1,82 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("Demo")
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+
+process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+
+#process.source = cms.Source("PoolSource",
+    # replace 'myfile.root' with the source file you want to use
+#    fileNames = cms.untracked.vstring(
+#        'file:../prod/RD/patTuple_muon.root'
+#         'rfio:///castor/cern.ch/user/t/tjkim/SDMUONFILTER/July19th_v4/Run2010A-PromptReco-v4/patTuple_muon_22_0_Qie.root'
+#    )
+#)
+
+#process.load("KoPFA.DiMuonAnalyzer.RD.patTuple_muon_cff")
+
+# register TFileService
+process.TFileService = cms.Service("TFileService",
+    fileName = cms.string('vallot.root')
+)
+
+process.VertexFilter = cms.EDFilter('VertexFilter',
+    vertexLabel =  cms.InputTag('offlinePrimaryVertices'),
+    min = cms.untracked.int32(1),
+    max = cms.untracked.int32(999),
+)
+
+process.load("PFAnalyses.CommonTools.countingSequences_cfi")
+
+from PFAnalyses.CommonTools.Selectors.muonSelectorPSet_cff import muonSelectorPSet
+muonId = muonSelectorPSet.clone()
+muonId.dxy = 0.02
+muonId.eta = 2.5
+muonId.pt = 20
+from PFAnalyses.CommonTools.Selectors.muonIsoSelectorPSet_cff import muonIsoSelectorPSet
+muonIso = muonIsoSelectorPSet.clone()
+
+process.Electrons = cms.EDFilter("PATElectronSelector",
+    src = cms.InputTag("selectedPatElectronsPFlow"),
+    #cut =cms.string("pt > 20 && abs(eta) < 2.1 && mva > 0.4 && gsfTrack.trackerExpectedHitsInner.numberOfHits<=1")
+    cut =cms.string("pt > 20 && abs(eta) < 2.5")
+)
+
+process.patElectronFilter = cms.EDFilter("CandViewCountFilter",
+  src = cms.InputTag('Electrons'),
+  minNumber = cms.uint32(1)
+)
+
+
+from PFAnalyses.CommonTools.Selectors.looseJetIdPSet_cff import looseJetIdPSet
+myJetId = looseJetIdPSet.clone()
+myJetId.verbose = False 
+
+process.ElEl = cms.EDAnalyzer('TopElElAnalyzer',
+  muonLabel1 =  cms.InputTag('Electrons'),
+  muonLabel2 =  cms.InputTag('Electrons'),
+  metLabel =  cms.InputTag('patMETsPFlow'),
+  jetLabel =  cms.InputTag('selectedPatJetsPFlow'),
+  useEventCounter = cms.bool( True ),
+  filters = cms.untracked.vstring(
+                              'initialEvents',
+                              'finalEvents'
+                              ),
+  looseJetId = myJetId, 
+)
+
+process.load("HLTrigger.HLTfilters.hltHighLevel_cfi")
+process.hltHighLevel.HLTPaths = cms.vstring('HLT_Mu9')
+
+process.p = cms.Path(
+                     process.loadHistosFromRunInfo*
+#                     process.hltHighLevel*
+                     process.Electrons*
+                     process.patElectronFilter*
+                     process.VertexFilter*
+                     process.ElEl
+                    )
+
