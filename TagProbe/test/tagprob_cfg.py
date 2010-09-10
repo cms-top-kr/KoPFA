df55670c8148751e65f78432ffa8082e744df204
tjkim <>
2010-09-10 08:27:51 +0000

    modified for isolation

--- TagProbe/test/tagprob_cfg.py
+++ TagProbe/test/tagprob_cfg.py
@@ -4,24 +4,48 @@ process = cms.Process("TagProbe")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1000) )
 
-process.source = cms.Source("PoolSource", fileNames = cms.untracked.vstring(
-   'file:/afs/cern.ch/user/t/tjkim/public/forColin/pf2pat_iso.root',
-    )
+# register TFileService
+process.TFileService = cms.Service("TFileService",
+    fileName = cms.string('tagprobe_v2.root')
 )
+
+
+#process.source = cms.Source("PoolSource", fileNames = cms.untracked.vstring(
+   #'file:/afs/cern.ch/user/t/tjkim/public/forColin/pf2pat_iso.root',
+#   '/store/data/Run2010A/Mu/RECO/v4/000/142/136/1A788144-289E-DF11-AC2A-001D09F2960F.root'
+#    )
+#)
+#process.load("KoPFA.DiMuonAnalyzer.RD.patTuple_Run2010A_PromptReco_cff")
+process.load("KoPFA.DiMuonAnalyzer.MC.patTuple_Zmumu_cff")
 
 process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True))
 
-process.tagMuons = cms.EDFilter("PATMuonRefSelector",
-  src = cms.InputTag("patMuons"),
-  cut = cms.string("isGlobalMuon && muonID('GlobalMuonPromptTight') && !triggerObjectMatchesByFilter('hltSingleMu3L3Filtered3').empty()"),   ## Global Mu P.T. matched to HLT_Mu3
+from PFAnalyses.CommonTools.Selectors.muonSelectorPSet_cff import muonSelectorPSet
+muonId = muonSelectorPSet.clone()
+muonId.dxy = 0.02
+muonId.eta = 2.5
+muonId.pt = 20
+from PFAnalyses.CommonTools.Selectors.muonIsoSelectorPSet_cff import muonIsoSelectorPSet
+muonIso = muonIsoSelectorPSet.clone()
 
+process.tagMuons = cms.EDProducer(
+    "KoMuonSelector",
+    version = cms.untracked.int32( 4 ),
+    muonLabel  = cms.InputTag("selectedPatMuonsPFlow"),
+    beamSpotLabel = cms.InputTag("offlineBeamSpot"),
+    muonIdSelector = muonId,
+    muonIsoSelector = muonIso,
 )
 
-process.probeMuons = cms.EDFilter("PATMuonRefSelector",
-  src = cms.InputTag("patMuons"),
-  cut = cms.string("isGlobalMuon && pt > 3 && abs(eta) < 2.4"),   ## Global Mu, pt > 3, eta < 2.4
+process.probeMuons = cms.EDProducer(
+    "KoMuonSelector",
+    version = cms.untracked.int32( 1 ),
+    muonLabel  = cms.InputTag("selectedPatMuonsPFlow"),
+    beamSpotLabel = cms.InputTag("offlineBeamSpot"),
+    muonIdSelector = muonId,
+    muonIsoSelector = muonIso,
 )
 
 process.tagProbes = cms.EDProducer("CandViewShallowCloneCombiner",
@@ -40,8 +64,9 @@ process.tnpTree = cms.EDAnalyzer("TagProbeFitTreeProducer",
     ),
     # choice of what defines a 'passing' probe
     flags = cms.PSet(
-        isGlobalMuon = cms.string("isGlobalMuon"),
-        isHLTMu3     = cms.string("!triggerObjectMatchesByFilter('hltSingleMu3L3Filtered3').empty()"),  
+        isIso = cms.string("(chargedHadronIso + neutralHadronIso/0.33 + photonIso)/pt < 0.21"),
+        #isGlobalMuon = cms.string("isGlobalMuon"),
+        #isHLTMu3     = cms.string("!triggerObjectMatchesByFilter('hltSingleMu3L3Filtered3').empty()"),  
     ),
     ## DATA-related info
     addRunLumiInfo = cms.bool(True),
