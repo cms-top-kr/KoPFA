9adb55af664db1317e48ea625c4038585073bff3
tjkim <>
2010-09-11 20:36:22 +0000

    tagprobe batch jobs

---
+++ TagProbe/test/batch/jobforcmsBarch.sh
@@ -0,0 +1,6 @@
+cmsBatch.py 1 tagprob_data_1_cfg.py -o Out/1 -r /castor/cern.ch/user/t/tjkim/ntuple/tnp/Sep3/1/tagprobe.root -b 'bsub -q 8nm < batchScript.sh'
+cmsBatch.py 1 tagprob_data_2_cfg.py -o Out/2 -r /castor/cern.ch/user/t/tjkim/ntuple/tnp/Sep3/2/tagprobe.root -b 'bsub -q 8nm < batchScript.sh'
+cmsBatch.py 1 tagprob_data_3_cfg.py -o Out/3 -r /castor/cern.ch/user/t/tjkim/ntuple/tnp/Sep3/3/tagprobe.root -b 'bsub -q 8nm < batchScript.sh'
+cmsBatch.py 1 tagprob_data_4_cfg.py -o Out/4 -r /castor/cern.ch/user/t/tjkim/ntuple/tnp/Sep3/4/tagprobe.root -b 'bsub -q 8nm < batchScript.sh'
+
+
---
+++ TagProbe/test/batch/tagprob_data_3_cfg.py
@@ -0,0 +1,6 @@
+from KoPFA.TagProbe.tagprob_cff import process
+
+import FWCore.ParameterSet.Config as cms
+
+process.load("KoPFA.DiMuonAnalyzer.RD.patTuple_Run2010A_Jul16thReReco_cff")
+
