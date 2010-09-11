ec317aa573ef137f8840d2b57762ac7f2137af7e
tjkim <>
2010-09-11 20:38:27 +0000

    tagprobe configuration file

---
+++ TagProbe/python/tagprob_cff.py
@@ -0,0 +1,82 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("TagProbe")
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1000) )
+
+# register TFileService
+process.TFileService = cms.Service("TFileService",
+    fileName = cms.string('tagprobe.root')
+)
+
+
+#process.source = cms.Source("PoolSource", fileNames = cms.untracked.vstring(
+   #'file:/afs/cern.ch/user/t/tjkim/public/forColin/pf2pat_iso.root',
+#   '/store/data/Run2010A/Mu/RECO/v4/000/142/136/1A788144-289E-DF11-AC2A-001D09F2960F.root'
+#    )
+#)
+#process.load("KoPFA.DiMuonAnalyzer.RD.patTuple_Run2010A_PromptReco_cff")
+#process.load("KoPFA.DiMuonAnalyzer.MC.patTuple_Zmumu_cff")
+
+process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True))
+
+from PFAnalyses.CommonTools.Selectors.muonSelectorPSet_cff import muonSelectorPSet
+muonId = muonSelectorPSet.clone()
+muonId.dxy = 0.02
+muonId.eta = 2.5
+muonId.pt = 20
+from PFAnalyses.CommonTools.Selectors.muonIsoSelectorPSet_cff import muonIsoSelectorPSet
+muonIso = muonIsoSelectorPSet.clone()
+
+process.tagMuons = cms.EDProducer(
+    "KoMuonSelector",
+    version = cms.untracked.int32( 4 ),
+    muonLabel  = cms.InputTag("selectedPatMuonsPFlow"),
+    beamSpotLabel = cms.InputTag("offlineBeamSpot"),
+    muonIdSelector = muonId,
+    muonIsoSelector = muonIso,
+)
+
+process.probeMuons = cms.EDProducer(
+    "KoMuonSelector",
+    version = cms.untracked.int32( 1 ),
+    muonLabel  = cms.InputTag("selectedPatMuonsPFlow"),
+    beamSpotLabel = cms.InputTag("offlineBeamSpot"),
+    muonIdSelector = muonId,
+    muonIsoSelector = muonIso,
+)
+
+process.tagProbes = cms.EDProducer("CandViewShallowCloneCombiner",
+    decay = cms.string("tagMuons@+ probeMuons@-"), # charge coniugate states are implied; 'tagMuons' and 'trkProbes' should be collections of Candidates
+    cut   = cms.string("70 < mass < 110"),
+)
+
+process.tnpTree = cms.EDAnalyzer("TagProbeFitTreeProducer",
+    # choice of tag and probe pairs, and arbitration
+    tagProbePairs = cms.InputTag("tagProbes"),
+    arbitration   = cms.string("OneProbe"), ## that is, use only tags associated to a single probe.
+    # probe variables
+    variables = cms.PSet(
+        pt     = cms.string("pt"),
+        abseta = cms.string("abs(eta)"),
+    ),
+    # choice of what defines a 'passing' probe
+    flags = cms.PSet(
+        isIso = cms.string("(chargedHadronIso + neutralHadronIso/0.33 + photonIso)/pt < 0.21"),
+        #isGlobalMuon = cms.string("isGlobalMuon"),
+        #isHLTMu3     = cms.string("!triggerObjectMatchesByFilter('hltSingleMu3L3Filtered3').empty()"),  
+    ),
+    ## DATA-related info
+    addRunLumiInfo = cms.bool(True),
+    ## MC-related info
+    isMC = cms.bool(False), ## on MC you can set this to true, add some parameters and get extra info in the tree.
+)
+
+process.p = cms.Path(
+                      process.tagMuons
+                     *process.probeMuons
+                     *process.tagProbes
+                     *process.tnpTree
+)
