e5a004ca290727439d8e2f518e455d59e0bb812d
tjkim <>
2010-09-16 18:22:25 +0000

    InclusiveMu15

---
+++ TopAnalyzer/test/batch/MuMu/topMuMuAnalyzer_InclusiveMu15_cfg.py
@@ -0,0 +1,6 @@
+from KoPFA.TopAnalyzer.topMuMuAnalyzer_cff import process
+
+import FWCore.ParameterSet.Config as cms
+
+process.load("PFAnalyses.TTbarDIL.Sources.MU.MC.Spring10.patTuple_InclusiveMu15_cff")
+
