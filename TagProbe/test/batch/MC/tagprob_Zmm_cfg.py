9b027d14778b2756d3a979187f7c4e58068bf755
tjkim <>
2010-09-27 21:58:04 +0000

    change the MC location to PFAnalyses

--- TagProbe/test/batch/MC/tagprob_Zmm_cfg.py
+++ TagProbe/test/batch/MC/tagprob_Zmm_cfg.py
@@ -2,5 +2,4 @@ from KoPFA.TagProbe.tagprob_cff import process
 
 import FWCore.ParameterSet.Config as cms
 
-process.load("KoPFA.DiMuonAnalyzer.MC.Spring10.patTuple_Zmumu_cff")
-
+process.load("PFAnalyses.TTbarDIL.Sources.MU.MC.Spring10.patTuple_Zmumu_cff")
