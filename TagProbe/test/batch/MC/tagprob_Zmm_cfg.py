bb2f11dcfc3b61f7887394175414b424467b3b4b
tjkim <>
2010-09-30 08:44:25 +0000

    remove

--- TagProbe/test/batch/MC/tagprob_Zmm_cfg.py
+++ TagProbe/test/batch/MC/tagprob_Zmm_cfg.py
@@ -3,3 +3,9 @@ from KoPFA.TagProbe.tagprob_cff import process
 import FWCore.ParameterSet.Config as cms
 
 process.load("PFAnalyses.TTbarDIL.Sources.MU.MC.Spring10.patTuple_Zmumu_cff")
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(20000) )
+process.TFileService = cms.Service("TFileService",
+    fileName = cms.string('tagprob_mc.root')
+)
+
+
