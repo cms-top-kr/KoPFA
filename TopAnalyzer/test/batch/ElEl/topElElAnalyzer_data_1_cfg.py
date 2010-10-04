712c13c25febb55a2040a331f24b34534a6bc054
tjkim <>
2010-10-04 11:09:51 +0000

    add trigger for electron

--- TopAnalyzer/test/batch/ElEl/topElElAnalyzer_data_1_cfg.py
+++ TopAnalyzer/test/batch/ElEl/topElElAnalyzer_data_1_cfg.py
@@ -3,4 +3,4 @@ from KoPFA.TopAnalyzer.topElElAnalyzer_cff import process
 import FWCore.ParameterSet.Config as cms
 
 process.load("PFAnalyses.TTbarDIL.Sources.ELE.RD.patTuple_Commissioning10_cff")
-process.hltHighLevel.HLTPaths = cms.vstring('HLT_Ele10_LW_L1R',"HLT_Ele15_LW_L1R","HLT_Ele15_SW_L1R","HLT_Ele15_SW_CaloEleId_L1R","HLT_Ele20_SW_L1R","HLT_DoubleEle10_SW_L1R")
+process.hltHighLevel.HLTPaths = cms.vstring("HLT_Ele10_LW_L1R")
