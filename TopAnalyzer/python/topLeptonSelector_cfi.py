e6d355a639b46d3cba39258da0a9aa48e00e1033
tjkim <>
2010-10-17 23:04:42 +0000

    add electron ID

--- TopAnalyzer/python/topLeptonSelector_cfi.py
+++ TopAnalyzer/python/topLeptonSelector_cfi.py
@@ -3,14 +3,14 @@ import FWCore.ParameterSet.Config as cms
 from PFAnalyses.CommonTools.Selectors.muonSelectorPSet_cff import muonSelectorPSet
 from PFAnalyses.CommonTools.Selectors.muonIsoSelectorPSet_cff import muonIsoSelectorPSet
 muonId = muonSelectorPSet.clone()
-
+muonId.verbose = False 
 muonId.dxy = 0.02
 muonId.eta = 2.5
 muonId.pt = 20
 
 Muons = cms.EDProducer(
     "KoMuonSelector",
-    version = cms.untracked.int32( 1 ),#TOP
+    version = cms.untracked.int32( 1 ),# dxy, pt, eta
     muonLabel  = cms.InputTag("selectedPatMuonsPFlow"),
     beamSpotLabel = cms.InputTag("offlineBeamSpot"),
     muonIdSelector = muonId,
@@ -18,10 +18,21 @@ Muons = cms.EDProducer(
 
 )
 
-Electrons = cms.EDFilter("PATElectronSelector",
-    src = cms.InputTag("selectedPatElectronsPFlow"),
-    cut =cms.string("pt > 20 && abs(eta) < 2.5 && abs(gsfTrack.dxy) < 0.04 && mva > 0.4 && gsfTrack.trackerExpectedHitsInner.numberOfHits<=1 ")
-    #cut =cms.string("pt > 20 && abs(eta) < 2.5")
+from PFAnalyses.CommonTools.Selectors.electronSelectorPSet_cff import electronSelectorPSet
+from PFAnalyses.CommonTools.Selectors.newElectronIsoSelectorPSet_cff import electronIsoSelectorPSet
+
+Electrons = cms.EDProducer(
+    "KoElectronSelector",
+    version = cms.untracked.int32( 5 ),# WP90
+    electronLabel  = cms.InputTag("selectedPatElectronsPFlow"),
+    beamSpotLabel = cms.InputTag("offlineBeamSpot"),
+    electronIdSelector = electronSelectorPSet,
+    electronIsoSelector = electronIsoSelectorPSet,
 )
+
+#Electrons = cms.EDFilter("PATElectronSelector",
+#    src = cms.InputTag("WP90Electrons"),
+#    cut =cms.string("pt > 20 && abs(eta) < 2.5 && abs(gsfTrack.dxy) < 0.04 && mva > 0.4 && gsfTrack.trackerExpectedHitsInner.numberOfHits<=1 ")
+#)
 
 
