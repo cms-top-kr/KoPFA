c0f2cae0c790281facb756e798ac616dd060df2c
tjkim <>
2010-10-25 17:05:49 +0000

    add gen filter for tautau below 50 GeV

--- TopAnalyzer/test/batch/ElEl/topElElAnalyzer_Ztautau_cfg.py
+++ TopAnalyzer/test/batch/ElEl/topElElAnalyzer_Ztautau_cfg.py
@@ -3,5 +3,8 @@ from KoPFA.TopAnalyzer.topElElAnalyzer_cff import process
 import FWCore.ParameterSet.Config as cms
 
 process.load("PFAnalyses.TTbarDIL.Sources.ELE.MC.Spring10.patTuple_Ztautau_cff")
+process.GenZmassFilter.applyFilter = True
+process.GenZmassFilter.min = 0
+process.GenZmassFilter.max = 50
 
 
--- TopAnalyzer/test/batch/MuMu/topMuMuAnalyzer_Ztautau_cfg.py
+++ TopAnalyzer/test/batch/MuMu/topMuMuAnalyzer_Ztautau_cfg.py
@@ -3,5 +3,8 @@ from KoPFA.TopAnalyzer.topMuMuAnalyzer_cff import process
 import FWCore.ParameterSet.Config as cms
 
 process.load("PFAnalyses.TTbarDIL.Sources.MU.MC.Spring10.patTuple_Ztautau_cff")
+process.GenZmassFilter.applyFilter = True
+process.GenZmassFilter.min = 0
+process.GenZmassFilter.max = 50
 
 
