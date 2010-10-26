fbb7868c526d388533f43821efa6cd2fa9d323f9
tjkim <>
2010-10-26 11:51:47 +0000

    tau decay filter

--- TopAnalyzer/test/batch/ElEl/topElElAnalyzer_ZtauDecay_cfg.py
+++ TopAnalyzer/test/batch/ElEl/topElElAnalyzer_ZtauDecay_cfg.py
@@ -4,7 +4,7 @@ import FWCore.ParameterSet.Config as cms
 
 process.load("PFAnalyses.TTbarDIL.Sources.ELE.MC.Spring10.patTuple_ZJets_cff")
 process.GenZmassFilter.applyFilter = True
-process.GenZmassFilter.decayMode = cms.untracked.vint32(11,13)
+process.GenZmassFilter.decayMode = cms.untracked.vint32(15)
 process.GenZmassFilter.min = 50
 process.GenZmassFilter.max = 999
 
