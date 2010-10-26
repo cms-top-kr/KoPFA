b10426df348d65596cdebbed56489200dc010c6e
jhgoh <>
2010-10-26 14:04:37 +0000

    Wrong label

--- IsoAnalyzer/python/WFilter_cff.py
+++ IsoAnalyzer/python/WFilter_cff.py
@@ -1,14 +1,13 @@
 import FWCore.ParameterSet.Config as cms
 
 WenuMtCut = cms.EDFilter("WenuFilter",
-  eleLabel =  cms.InputTag("Muons"),
+  eleLabel =  cms.InputTag("Electrons"),
   metLabel =  cms.InputTag('patMETsPFlow'),
   pfCandidateLabel = cms.InputTag('particleFlow'),
   minMt = cms.double(70),
   mindphi = cms.double(-1.0), #anlge between lepton and neutrino should be smaller than maxdphi and larger than mindphi
   maxdphi = cms.double(999.0),
 )
-
 
 WmunuMtCut = cms.EDFilter("WmunuFilter",
   muonLabel =  cms.InputTag("Muons"),
