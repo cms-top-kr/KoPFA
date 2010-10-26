66bab80e812d25fa1d892d6593c11d279e3f8517
tjkim <>
2010-10-26 11:55:05 +0000

    remove

--- TopAnalyzer/test/batch/ElEl/topElElAnalyzer_data_3_cfg.py
+++
@@ -1,6 +0,0 @@
-from KoPFA.TopAnalyzer.topElElAnalyzer_cff import process
-
-import FWCore.ParameterSet.Config as cms
-
-process.load("PFAnalyses.TTbarDIL.Sources.ELE.RD.patTuple_Run2010A_Jul16thReReco_cff")
-process.hltHighLevel.HLTPaths = cms.vstring("HLT_Ele15_LW_L1R")
