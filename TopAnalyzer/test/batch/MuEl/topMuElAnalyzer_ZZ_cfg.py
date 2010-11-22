a4cd49a13dc8b4992bb8c220802c28c88d9e5c71
tjkim <>
2010-11-22 15:43:11 +0000

    change process name for trigger

--- TopAnalyzer/test/batch/MuEl/topMuElAnalyzer_ZZ_cfg.py
+++ TopAnalyzer/test/batch/MuEl/topMuElAnalyzer_ZZ_cfg.py
@@ -3,4 +3,5 @@ from KoPFA.TopAnalyzer.topMuElAnalyzer_cff import process
 import FWCore.ParameterSet.Config as cms
 
 process.load("PFAnalyses.TTbarDIL.Sources.EMU.MC.Fall10.patTuple_ZZ_cff")
+process.emuTriggerFilterForMC = process.emuTriggerFilterForMCREDIGI38X
 
