e906a2b595e795bf6f8d126ec39f55dd20acc0bb
tjkim <>
2010-11-22 15:38:17 +0000

    update accordingly for 38x MC and local output

--- TopAnalyzer/test/batch/jobforcmsBatch.py
+++ TopAnalyzer/test/batch/jobforcmsBatch.py
@@ -11,14 +11,15 @@ input = sys.argv[1]
 type = sys.argv[2]
 
 if type == "MuMu":
-  mclist = ["ZJets", "ZtauDecay", "Ztautau", "WJets", "VVJets", "TTbar", "SingleTop", "Zmumu", "DYmumu"]
+  mclist = ["ZJets", "ZtauDecay", "Ztautau", "Zmumu","Zee", "Wmu","We","Wtau", "TTbar", "SingleTop","DYmumu","DYee","DYtautau"]
   #qcdlist =["InclusiveMu15"]
   qcdlist =[""]
 elif type == "ElEl":
-  mclist = ["ZJets", "ZtauDecay", "Ztautau", "WJets", "VVJets", "TTbar", "SingleTop", "Zee", "DYee"]
+  mclist = ["ZJets", "ZtauDecay", "Ztautau", "Zmumu","Zee", "Wmu","We","Wtau", "TTbar", "SingleTop","DYmumu","DYee","DYtautau"]
   qcdlist =[""]
 elif type == "MuEl":
-  mclist = ["ZJets", "ZtauDecay", "Ztautau", "WJets", "VVJets", "TTbar", "SingleTop"]
+  mclist = ["ZJets", "ZtauDecay", "Ztautau", "Zmumu","Zee", "Wmu","We","Wtau", "TTbar", "SingleTop","DYmumu","DYee","DYtautau"]
+  #mclist = ["ZZ","WW","ZW"]
   qcdlist =[""]
 
 datalist = ["data_1","data_2"]
@@ -26,10 +27,15 @@ datalist = ["data_1","data_2"]
 def processSample(sample, dir):
     os.system("rfmkdir "+dir+"/"+sample)
     os.system("rm -rf Out/"+sample)
-    os.system("cmsBatch.py 1 top"+type+"Analyzer_"+sample+"_cfg.py -o Out/"+sample+" -r "+dir+"/"+sample+" -b 'bsub -q 8nm < batchScript.sh'")
+    os.system("cmsBatch.py 1 top"+type+"Analyzer_"+sample+"_cfg.py -o Out/"+sample+" -r "+dir+"/"+sample+" -b 'bsub -q 1nh < batchScript.sh'")
 
-mcdir = "/castor/cern.ch/user/t/tjkim/ntuple/top/"+type+"/MC/Spring10"
-datadir = "/castor/cern.ch/user/t/tjkim/ntuple/top/"+type+"/RD/Sep11"
+import os,commands
+currdir = commands.getoutput('pwd') 
+print currdir
+mcdir = currdir+"/test"
+datadir = mcdir
+#mcdir = "/castor/cern.ch/user/t/tjkim/ntuple/top/"+type+"/MC/Spring10"
+#datadir = "/castor/cern.ch/user/t/tjkim/ntuple/top/"+type+"/RD/Sep11"
 
 if input == "status":
   print mcdir
@@ -39,7 +45,7 @@ if input == "status":
 elif input =="run":
   for s in mclist:
     processSample(s,mcdir)
-  time.sleep(480)
+#  time.sleep(480)
   for s in datalist:
     processSample(s,datadir)
 #  for s in qcdlist:
