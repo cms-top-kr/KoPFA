02d82331b38b729a251d2245814b642a5fe76bd7
mskim <>
2010-11-25 16:26:26 +0000

    minor update

--- TopAnalyzer/test/mergeChannel.C
+++ TopAnalyzer/test/mergeChannel.C
@@ -1,56 +1,83 @@
-void merge (TString filename, TString step, TString histotype, Int_t hscale=10)
+void mergeChannel (TString filename, TString step, TString histotype, bool logscale=true, float hmax=0, float hmin=0.02)
 {
-
+  
   TFile *_file0 = TFile::Open(filename);
-
+  
   const int n = 6;
-
-  TString hist = TString(step) + TString("_") + TString(histotype);
-  TString data = TString("hData_Step_") + TString(hist);
+  
+  TString hist = TString("_Step_")+ TString(step) + TString("_") + TString(histotype);
+  TString data = TString("hData") + TString(hist);
   TString mc[n];
-  mc[0] = TString("hMC_TTbar_Step_") + TString(hist);
-  mc[1] = TString("hMC_Wl_Step_") + TString(hist);
-  mc[2] = TString("hMC_VV_Step_") + TString(hist);
-  mc[3] = TString("hMC_SingleTop_Step_") + TString(hist);
-  mc[4] = TString("hMC_DYtt_Step_") + TString(hist);
-  mc[5] = TString("hMC_DYll_Step_") + TString(hist);
-
-  TH1F* hdata = (TH1F*)(gROOT->FindObject(data));
-  hdata->GetXaxis()->SetTitleOffset(0.8);
-  hdata->GetYaxis()->SetTitleOffset(0.8);
-
-  TH1F* hmc[n];
-  for(int i=0; i<n; i++) {
+  mc[0] = TString("hMC_TTbar") + TString(hist);
+  mc[1] = TString("hMC_Wl") + TString(hist);
+  mc[2] = TString("hMC_VV") + TString(hist);
+  mc[3] = TString("hMC_SingleTop") + TString(hist);
+  mc[4] = TString("hMC_DYtt") + TString(hist);
+  mc[5] = TString("hMC_DYll") + TString(hist);
+  
+  TString legMc[n];
+  legMc[0] = TString("t#bar{t}");
+  legMc[1] = TString("W #rightarrow l#nu");
+  legMc[2] = TString("Dibosons");
+  legMc[3] = TString("Single top");
+  legMc[4] = TString("Z/#gamma* #rightarrow #tau#tau");
+  legMc[5] = TString("Z/#gamma* #rightarrow ll");
+  
+  TH1F* hData = (TH1F*)(gROOT->FindObject(data));
+  hData->GetXaxis()->SetTitleOffset(0.8);
+  hData->GetYaxis()->SetTitleOffset(0.8);
+  
+  TH1F* hMc[n];
+  THStack *hStack = new THStack("hStack",TString("MC")+hist);
+  for(int i=0; i<n; ++i) {
     cout << mc[i] << endl;
-    hmc[i] = (TH1F*)(gROOT->FindObject(mc[i]));
-    hmc[i]->SetTitle("");
-    hmc[i]->GetXaxis()->SetTitleOffset(1.2);
-    hmc[i]->GetYaxis()->SetTitleOffset(1.2);
-    hmc[i]->SetStats(0);
+    hMc[i] = (TH1F*)(gROOT->FindObject(mc[i]));
+    hMc[i]->SetTitle("");
+    hMc[i]->GetXaxis()->SetTitleOffset(1.3);
+    hMc[i]->GetYaxis()->SetTitleOffset(1.3);
+    hMc[i]->SetStats(0);
+    
+    hStack->Add(hMc[i]);
   }
-
-  THStack *stack = new THStack("stack","stack");
-  for(int i=0; i<n; i++) {
-    stack->Add(hmc[i]);
-  }
-
+  
   TCanvas *c1 = new TCanvas("c1","c1",0,0,700,500);
   c1->Divide(2,3);
-  for(int i=0; i<n; i++) {
-    c1->cd(i+1); hmc[i]->Draw();
+  for(int i=0; i<n; ++i) {
+    c1->cd(i+1); hMc[i]->Draw();
   }
-
+  
   TCanvas *c2 = new TCanvas("c2","c2",100,100,700,500);
-  hdata->Draw();
-  stack->Draw("same");
-  hdata->Draw("same");
-  //hdata->SetTitle("");
-
-  Int_t hmax = hdata->GetMaximum();
-  hdata->SetMaximum(hscale*hmax);
-  hdata->SetMinimum(0.02);
-
-  gPad->SetLogy();
-
-  //c2->BuildLegend();
+  hData->Draw();
+  hStack->Draw("same");
+  hData->Draw("same");
+  
+  hData->SetTitle("");
+  if(hmax!=0) hData->SetMaximum(hmax);
+  hData->SetMinimum(hmin);
+  
+  gPad->RedrawAxis();
+  gPad->SetTicks();
+  
+  if(logscale) gPad->SetLogy();
+  
+  TLegend *leg = new TLegend(0.70,0.57,0.88,0.88,NULL,"brNDC");
+  leg->SetBorderSize(1);
+  leg->SetTextFont(62);
+  leg->SetTextSize(0.04);
+  leg->SetLineColor(0);
+  leg->SetLineStyle(1);
+  leg->SetLineWidth(1);
+  leg->SetFillColor(0);
+  leg->SetFillStyle(1001);
+  if (hData->GetEntries()>0) leg->AddEntry(hData,"Data","p");
+  for(int i=0; i<n; ++i) {
+    int j = n-i-1;
+    if (hMc[j]->GetEntries()>0) leg->AddEntry(hMc[j],legMc[j],"f");
+  }
+  leg->Draw();
+  
+  c2->Print(TString("c")+hist+TString(".png"));
+  c2->Print(TString("c")+hist+TString(".eps"));
+  c2->Print(TString("c")+hist+TString(".pdf"));
+  
 }
