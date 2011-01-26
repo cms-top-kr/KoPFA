a44d18763bc2a52e3e6627e77a6d471de18aa20a
tjkim <>
2011-01-26 00:34:45 +0000

    to make it easy to use, make a function

--- IsoAnalyzer/test/isoOpt.C
+++ IsoAnalyzer/test/isoOpt.C
@@ -1,74 +1,84 @@
 #include "TFile.h"
 
+TGraph* getGraph(TTree *ts, TTree *tb, const TString & var, TCut csig, TCut cbkg, int low, int max, int start, const TString & name, int color, int style){
+
+  TH1* h_sig = new TH1F(Form("h_sig_%s",name.Data()),Form("h_sig_%s",name.Data()),50,0,0.5);
+  TH1* h_bkg = new TH1F(Form("h_bkg_%s",name.Data()),Form("h_bkg_%s",name.Data()),50,0,0.5);
+
+  ts->Project(Form("h_sig_%s",name.Data()),Form("%s",var.Data()), csig);
+  tb->Project(Form("h_bkg_%s",name.Data()),Form("%s",var.Data()), cbkg);
+
+  TGraph *gr = new TGraph();
+  TGraph *gr_opt = new TGraph();
+
+  double optsoverb = 0;
+  double optval = 0;
+  for(int i=low ; i < max ; i++){
+    double bin = i*1+start;
+    double point = h_bkg->GetBinLowEdge(bin)+h_bkg->GetBinWidth(bin);
+    double eff_bkg = h_bkg->Integral(1,bin)/h_bkg->GetEntries();
+    double eff_sig = h_sig->Integral(1,bin)/h_sig->GetEntries();
+    double soverb = eff_sig/sqrt(eff_sig+eff_bkg);
+
+    cout << name.Data() << ":[" << point << "]:" << "bkg eff= " << eff_bkg << " sig eff= " << eff_sig << " : s/sqrt(s+b)= " << soverb << endl;
+
+    gr->SetPoint(i-1, eff_bkg, eff_sig);
+    gr_opt->SetPoint(i-1, point, soverb);
+
+    if( soverb > optsoverb){
+      optsoverb = soverb;
+      optval = point;
+    }
+  }
+
+  gr->SetLineColor(color);
+  gr->SetLineWidth(2);
+  gr->SetMarkerColor(color);
+  gr->SetMarkerSize(1.1);
+  gr->SetMarkerStyle(style);
+
+  TCanvas *c_opt = new TCanvas(Form("c_opt_%s",name.Data()),Form("c_opt_%s",name.Data()),1);
+  gr_opt->Draw("ACP");
+  TLatex *label= new TLatex;
+  label->SetNDC();
+  label->SetTextSize(0.05);
+  label->DrawLatex(0.5,0.5,Form("%f at %5.2f",optsoverb,optval));
+  
+  return gr;
+}
+
 void isoOpt(){
 
-  //TFile * fQCD = new TFile("vallot_data_QCDPt15TOPDIL.root");
-  //TFile * fW = new TFile("vallot_data_WJetsMt70Pt15TOPDIL.root");
-  TFile * fQCD = new TFile("vallot_QCD.root");
-  TFile * fW = new TFile("vallot_WJetsMt70.root");
+  TFile * fbkg = new TFile("batch/MuMu/test/vallot_QCD.root");
+  TFile * fsig = new TFile("batch/MuMu/test/vallot_ZJets.root");
 
+  TTree * tbkg = (TTree *) fbkg->Get("MuMu/tree");
+  TTree * tsig = (TTree *) fsig->Get("MuMu/tree");
 
-  TTree * QCD = (TTree *) fQCD->Get("MuonAna/tree");
-  TTree * W = (TTree *) fW->Get("MuonAna/tree");
+  TCut cutsig = "Z.mass() > 12  && Z.sign() < 0";
+  TCut cutbkg = "Z.mass() > 12  && Z.sign() < 0";
+  TString pfiso03 = "relIso03lep1";
+  TString pfiso04 = "relIso04lep1";
+  TString pfiso05 = "relIso05lep1";
+  TString deiso = "(trackIso1+ecalIso1+hcalIso1)/Z.leg1().pt()";
 
-  TH1* h_pf_QCD = new TH1F("h_pf_QCD","h_pf_QCD",120,0,1.2);
-  TH1* h_pf_W = new TH1F("h_pf_W","h_pf_W",120,0,1.2);
-  TH1* h_de_QCD = new TH1F("h_de_QCD","h_de_QCD",120,0,1.2);
-  TH1* h_de_W = new TH1F("h_de_W","h_de_W",120,0,1.2);
-  
-  //TString pfiso = "(chIsoOpt05+nhIsoOpt05+phIsoOpt05)/pt";
-  TString pfiso = "(chIso+nhIso+phIso)/pt";
-  TString deiso = "(trackIso+ecalIso+hcalIso)/pt";
-  QCD->Project("h_pf_QCD",Form("%s",pfiso.Data()));
-  W->Project("h_pf_W",Form("%s",pfiso.Data()));
-  QCD->Project("h_de_QCD",Form("%s",deiso.Data()));
-  W->Project("h_de_W",Form("%s",deiso.Data()));
+  TGraph *gr_pf03 = getGraph(tsig, tbkg, pfiso03, cutsig, cutbkg, 1, 25 , 1, "pf03", 3, 20);
+  TGraph *gr_pf04 = getGraph(tsig, tbkg, pfiso04, cutsig, cutbkg, 1, 28 , 3, "pf04", 4, 20);
+  TGraph *gr_pf05 = getGraph(tsig, tbkg, pfiso05, cutsig, cutbkg, 1, 28 , 3, "pf05", 7, 20);
+  TGraph *gr_de = getGraph(tsig, tbkg, deiso, cutsig, cutbkg, 1, 25, 1, "de", 2, 23);
 
   TCanvas *c = new TCanvas("c","c",1);
-  TGraph *gr_pf = new TGraph();  
-  TGraph *gr_de = new TGraph();  
 
-  for(int i=1 ; i < 50 ; i++){
-    double bin = i*2+3;
-    double point = h_pf_QCD->GetBinLowEdge(bin)+h_pf_QCD->GetBinWidth(bin);
-    double effQCD_pf = h_pf_QCD->Integral(1,bin)/h_pf_QCD->GetEntries(); 
-    double effW_pf = h_pf_W->Integral(1,bin)/h_pf_W->GetEntries();
-
-    cout << "pf:[" << point << "]:" << "QCD eff= " << effQCD_pf << " W eff= " << effW_pf << endl;
- 
-    gr_pf->SetPoint(i-1, effQCD_pf, effW_pf);
-  }
-
-  for(int i=1 ; i < 50 ; i++){
-    double bin = i*2+1;
-    double point = h_de_QCD->GetBinLowEdge(bin)+h_de_QCD->GetBinWidth(bin);
-    double effQCD_de = h_de_QCD->Integral(1,bin)/h_de_QCD->GetEntries();
-    double effW_de = h_de_W->Integral(1,bin)/h_de_W->GetEntries();
-
-    cout << "de:[" << point << "]:" << "QCD eff= " << effQCD_de << " W eff= " << effW_de << endl;
-
-    gr_de->SetPoint(i-1, effQCD_de, effW_de);
-  }
-
-
-  gr_pf->SetLineColor(2);
-  gr_pf->SetLineWidth(2);
-  gr_pf->SetMarkerColor(2);
-  gr_pf->SetMarkerSize(1.1);
-  gr_pf->SetMarkerStyle(20);
-
-  gr_de->SetLineColor(4);
-  gr_de->SetLineWidth(2);
-  gr_de->SetMarkerColor(4);
-  gr_de->SetMarkerSize(1.1);
-  gr_de->SetMarkerStyle(20);
-
-  gr_pf->Draw("ACP");
+  gr_pf03->Draw("ACP");
+  gr_pf04->Draw("CPsame");
+  gr_pf05->Draw("CPsame");
   gr_de->Draw("CPsame");
 
   TLegend *l= new TLegend(0.4,0.3,0.7,0.45);
-  l->AddEntry(gr_pf,"particle based","PL");
-  l->AddEntry(gr_de,"detector based","PL");
+  l->AddEntry(gr_pf03,"particle based, 0.3","PL");
+  l->AddEntry(gr_pf04,"particle based, 0.4","PL");
+  l->AddEntry(gr_pf05,"particle based, 0.5","PL");
+  l->AddEntry(gr_de,"detector based, 0.3","PL");
   l->SetTextSize(0.04);
   l->SetFillColor(0);
   l->SetLineColor(0);
