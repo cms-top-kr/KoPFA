f3317c2a2ab4753377a70f2653c6028a513723ea
tjkim <>
2011-02-15 14:22:42 +0000

    update new dataset

--- TagProbe/test/batch/tagprob_data_1_cfg.py
+++ TagProbe/test/batch/tagprob_data_1_cfg.py
@@ -2,7 +2,8 @@ from KoPFA.TagProbe.tagprob_cff import process
 
 import FWCore.ParameterSet.Config as cms
 
-process.load("PFAnalyses.TTbarDIL.Sources.MU.RD.patTuple_Commissioning10_cff")
+process.load("KoPFA.DiMuonAnalyzer.RD.patTuple_Run2010A_Nov4ReReco_SingleMU_cff")
+
 process.TFileService = cms.Service("TFileService",
     fileName = cms.string('tagprob_1.root')
 )
--- TagProbe/test/batch/tagprob_data_2_cfg.py
+++ TagProbe/test/batch/tagprob_data_2_cfg.py
@@ -2,7 +2,8 @@ from KoPFA.TagProbe.tagprob_cff import process
 
 import FWCore.ParameterSet.Config as cms
 
-process.load("PFAnalyses.TTbarDIL.Sources.MU.RD.patTuple_Run2010A_Jun14thReReco_cff")
+process.load("KoPFA.DiMuonAnalyzer.RD.patTuple_Run2010B_Nov4ReReco_SingleMU_cff")
+
 process.TFileService = cms.Service("TFileService",
     fileName = cms.string('tagprob_2.root')
 )
