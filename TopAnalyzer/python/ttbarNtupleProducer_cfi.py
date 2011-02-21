3db593f3628033db44be8f20e8c7f68f65849db3
jhgoh <>
2011-02-21 16:22:21 +0000

    first commit

---
+++ TopAnalyzer/python/ttbarNtupleProducer_cfi.py
@@ -0,0 +1,51 @@
+import FWCore.ParameterSet.Config as cms
+
+from PFAnalyses.CommonTools.Selectors.looseJetIdPSet_cff import looseJetIdPSet
+jetIdForTTbar = looseJetIdPSet.clone()
+jetIdForTTbar.verbose = False
+
+mumu = cms.EDAnalyzer("TTbarMuMuEventNtupleProducer",
+    genParticle = cms.InputTag("genParticles"),
+
+    lepton1 = cms.InputTag("Muons"),
+    lepton2 = cms.InputTag("Muons"),
+    met = cms.InputTag("patMETsPFlow"),
+    jet = cms.InputTag("selectedPatJetsPFlow"),
+
+    jetId = jetIdForTTbar,
+    doResJec = cms.untracked.bool(False),
+
+    minJetIso1 = cms.double(0.21),
+    minJetIso2 = cms.double(0.21),
+)
+
+ee = cms.EDAnalyzer("TTbarElElEventNtupleProducer",
+    genParticle = cms.InputTag("genParticles"),
+
+    lepton1 = cms.InputTag("Electrons"),
+    lepton2 = cms.InputTag("Electrons"),
+    met = cms.InputTag("patMETsPFlow"),
+    jet = cms.InputTag("selectedPatJetsPFlow"),
+
+    jetId = jetIdForTTbar,
+    doResJec = cms.untracked.bool(False),
+
+    minJetIso1 = cms.double(0.26),
+    minJetIso2 = cms.double(0.26),
+)
+
+emu = cms.EDAnalyzer("TTbarElMuEventNtupleProducer",
+    genParticle = cms.InputTag("genParticles"),
+
+    lepton1 = cms.InputTag("Electrons"),
+    lepton2 = cms.InputTag("Muons"),
+    met = cms.InputTag("patMETsPFlow"),
+    jet = cms.InputTag("selectedPatJetsPFlow"),
+
+    jetId = jetIdForTTbar,
+    doResJec = cms.untracked.bool(False),
+
+    minJetIso1 = cms.double(0.26),
+    minJetIso2 = cms.double(0.21),
+)
+
