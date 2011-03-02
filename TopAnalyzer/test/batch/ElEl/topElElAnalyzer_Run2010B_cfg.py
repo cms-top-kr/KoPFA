793d30db9f37202674c0742ea43fa02a28e80bf4
jhgoh <>
2011-03-02 00:08:40 +0000

    Nov4ReReco, merged into 3 files

--- TopAnalyzer/test/batch/ElEl/topElElAnalyzer_Run2010B_cfg.py
+++ TopAnalyzer/test/batch/ElEl/topElElAnalyzer_Run2010B_cfg.py
@@ -16,8 +16,16 @@ process.TFileService = cms.Service("TFileService",
     fileName = cms.string('vallot_Run2010B.root')
 )
 
-process.load("PFAnalyses.TTbarDIL.Sources.ELE.RD.patTuple_Run2010B_PromptReco_cff")
+#process.load("PFAnalyses.TTbarDIL.Sources.ELE.RD.patTuple_Run2010B_Nov4ReReco_cff")
+process.source.fileNames = [
+    'rfio:/castor/cern.ch/user/j/jhgoh/TopAnalysis/pf2pat/ElEl/RD/20101209/Electron_Run2010B-Nov4ReReco_v1_1.root',
+    'rfio:/castor/cern.ch/user/j/jhgoh/TopAnalysis/pf2pat/ElEl/RD/20101209/Electron_Run2010B-Nov4ReReco_v1_2.root',
+    'rfio:/castor/cern.ch/user/j/jhgoh/TopAnalysis/pf2pat/ElEl/RD/20101209/Electron_Run2010B-Nov4ReReco_v1_3.root',
+]
+
 process.load("KoPFA.TopAnalyzer.topAnalysis_cff")
+process.ElEl.doResJec = cms.untracked.bool(True)
+process.ee.doResJec = True
 
 process.p = cms.Path(
     process.topElElAnalysisRealDataSequence
