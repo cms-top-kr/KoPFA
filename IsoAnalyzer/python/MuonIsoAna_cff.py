e3715d3fd869a7ff1b4f6758697474c3bb357274
tjkim <>
2011-03-22 14:11:52 +0000

    change max event number

--- IsoAnalyzer/python/MuonIsoAna_cff.py
+++ IsoAnalyzer/python/MuonIsoAna_cff.py
@@ -3,10 +3,11 @@ import FWCore.ParameterSet.Config as cms
 process = cms.Process("Demo")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
+process.MessageLogger.cerr.FwkReport.reportEvery = 1000
 
 process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10000) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 #process.source = cms.Source("PoolSource",
     # replace 'myfile.root' with the source file you want to use
