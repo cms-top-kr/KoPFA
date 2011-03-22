2eac4ce67cbece70a1f0e50b5ad1cc4d28ffc42f
tjkim <>
2011-03-22 13:55:50 +0000

    update source files

--- TagProbe/test/batch/tagprob_data_1_cfg.py
+++ TagProbe/test/batch/tagprob_data_1_cfg.py
@@ -2,10 +2,13 @@ from KoPFA.TagProbe.tagprob_cff import process
 
 import FWCore.ParameterSet.Config as cms
 
-process.load("KoPFA.DiMuonAnalyzer.RD.patTuple_Run2010A_Nov4ReReco_SingleMU_cff")
+#process.load("PFAnalyses.TTbarDIL.Sources.MU.RD.patTuple_Run2010A_SingleMU_cff")
+process.load("KoPFA.DiMuonAnalyzer.RD.patTuple_Run2010A_SingleMU_cff")
+#process.load("KoPFA.DiMuonAnalyzer.RD.patTuple_Run2010A_SingleMU_DisableNoPileUp_cff")
 
 process.TFileService = cms.Service("TFileService",
     fileName = cms.string('tagprob_1.root')
+    #fileName = cms.string('tagprob_1_DisableNoPileUp.root')
 )
 
 
--- TagProbe/test/batch/tagprob_data_2_cfg.py
+++ TagProbe/test/batch/tagprob_data_2_cfg.py
@@ -2,10 +2,13 @@ from KoPFA.TagProbe.tagprob_cff import process
 
 import FWCore.ParameterSet.Config as cms
 
-process.load("KoPFA.DiMuonAnalyzer.RD.patTuple_Run2010B_Nov4ReReco_SingleMU_cff")
+#process.load("PFAnalyses.TTbarDIL.Sources.MU.RD.patTuple_Run2010B_SingleMU_cff")
+process.load("KoPFA.DiMuonAnalyzer.RD.patTuple_Run2010B_SingleMU_cff")
+#process.load("KoPFA.DiMuonAnalyzer.RD.patTuple_Run2010B_SingleMU_DisableNoPileUp_cff")
 
 process.TFileService = cms.Service("TFileService",
     fileName = cms.string('tagprob_2.root')
+    #fileName = cms.string('tagprob_2_DisableNoPileUp.root')
 )
 
 
