4da8d6855ee94805f842bbe159737aea52fcc0f5
tjkim <>
2011-03-26 10:12:48 +0000

    for tight muon

---
+++ TagProbe/test/batch/tagprob_data_tight_1_cfg.py
@@ -0,0 +1,14 @@
+from KoPFA.TagProbe.tagprob_tightmu_cff import process
+
+import FWCore.ParameterSet.Config as cms
+
+#process.load("PFAnalyses.TTbarDIL.Sources.MU.RD.patTuple_Run2010A_SingleMU_cff")
+process.load("KoPFA.DiMuonAnalyzer.RD.patTuple_Run2010A_TightMU_cff")
+#process.load("KoPFA.DiMuonAnalyzer.RD.patTuple_Run2010A_SingleMU_DisableNoPileUp_cff")
+
+process.TFileService = cms.Service("TFileService",
+    fileName = cms.string('tagprob_1.root')
+    #fileName = cms.string('tagprob_1_DisableNoPileUp.root')
+)
+
+
---
+++ TagProbe/test/batch/tagprob_data_tight_2_cfg.py
@@ -0,0 +1,14 @@
+from KoPFA.TagProbe.tagprob_tightmu_cff import process
+
+import FWCore.ParameterSet.Config as cms
+
+#process.load("PFAnalyses.TTbarDIL.Sources.MU.RD.patTuple_Run2010B_SingleMU_cff")
+process.load("KoPFA.DiMuonAnalyzer.RD.patTuple_Run2010B_TightMU_cff")
+#process.load("KoPFA.DiMuonAnalyzer.RD.patTuple_Run2010B_SingleMU_DisableNoPileUp_cff")
+
+process.TFileService = cms.Service("TFileService",
+    fileName = cms.string('tagprob_2.root')
+    #fileName = cms.string('tagprob_2_DisableNoPileUp.root')
+)
+
+
