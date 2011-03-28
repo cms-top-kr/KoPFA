a4b98f0dd82774e82a4bd85e3b95dc7b81693544
tjkim <>
2011-03-28 11:12:34 +0000

    remove DQMServices head files

--- IsoAnalyzer/interface/IsoAnalysis.h
+++ IsoAnalyzer/interface/IsoAnalysis.h
@@ -8,7 +8,6 @@
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "FWCore/ServiceRegistry/interface/Service.h"
-#include "DQMServices/Core/interface/MonitorElement.h"
 #include "CommonTools/UtilAlgos/interface/TFileService.h"
 
 #include "DataFormats/PatCandidates/interface/Muon.h"
@@ -24,9 +23,6 @@
 #include "DataFormats/RecoCandidate/interface/IsoDeposit.h"
 #include "DataFormats/RecoCandidate/interface/IsoDepositVetos.h"
 #include "DataFormats/PatCandidates/interface/Isolation.h"
-
-#include "DQMServices/Core/interface/DQMStore.h"
-
 
 #include <TFile.h>
 #include <TTree.h>
