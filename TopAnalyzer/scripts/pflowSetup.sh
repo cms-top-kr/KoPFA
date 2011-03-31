44c70ef6704760d8e0b8546479b404002f3bb220
bhlee <>
2011-03-31 13:50:58 +0000

    BH_Mar31

--- TopAnalyzer/scripts/pflowSetup.sh
+++ TopAnalyzer/scripts/pflowSetup.sh
@@ -1,27 +1,29 @@
 ## Recipe from TopDileptonRefAnalysis2010Pass6
 # HBHE Filter Producer
-addpkg CommonTools/RecoAlgos
-cvs update -A -r V00-02-15 CommonTools/RecoAlgos/plugins/HBHENoiseFilterResultProducer.cc
-cvs update -r V00-02-15 CommonTools/RecoAlgos/python/HBHENoiseFilterResultProducer_cfi.py
-cvs update -r V00-02-15 CommonTools/RecoAlgos/BuildFile
+#addpkg CommonTools/RecoAlgos
+#cvs update -A -r V00-02-15 CommonTools/RecoAlgos/plugins/HBHENoiseFilterResultProducer.cc
+#cvs update -r V00-02-15 CommonTools/RecoAlgos/python/HBHENoiseFilterResultProducer_cfi.py
+#cvs update -r V00-02-15 CommonTools/RecoAlgos/BuildFile
+addpkg CommonTools/CandAlgos
 
 #ID on AOD
-cvs co -rV00-03-14-02 RecoEgamma/ElectronIdentification
+#cvs co -rV00-03-14-02 RecoEgamma/ElectronIdentification
 
 ## PF tags
-cvs co -r Jeroen_Sep28 -d AnalysisDataFormats/PFAnalyses UserCode/PFAnalyses/AnalysisDataFormats/PFAnalyses
-cvs co -r TJ_Nov9 -d PFAnalyses/CommonTools UserCode/PFAnalyses/CommonTools
-cvs co -r Colin_Sept28_newSamplesInterface -d PFAnalyses/RootTools UserCode/PFAnalyses/RootTools
-cvs co -r Jeroen_Sep29 -d PFAnalyses/TemplateAnalysis UserCode/PFAnalyses/TemplateAnalysis
+cvs co -r maxime_20101126 -d AnalysisDataFormats/PFAnalyses UserCode/PFAnalyses/AnalysisDataFormats/PFAnalyses
+cvs co -d PFAnalyses/CommonTools UserCode/PFAnalyses/CommonTools
+#cvs co -r Colin_Sept28_newSamplesInterface -d PFAnalyses/RootTools UserCode/PFAnalyses/RootTools
+#cvs co -r Jeroen_Sep29 -d PFAnalyses/TemplateAnalysis UserCode/PFAnalyses/TemplateAnalysis
 
-cvs co -r michal_V06-01-02_mod DataFormats/PatCandidates
-cvs co -r Colin_384_noMoreJetPtCutInPF2PAT_Sept30 PhysicsTools/PFCandProducer
-cvs co -r Colin_384_noMoreJetPtCutInPF2PAT_Sept30 PhysicsTools/PatAlgos
+#cvs co -r michal_V06-01-02_mod DataFormats/PatCandidates
+#cvs co -r Colin_384_noMoreJetPtCutInPF2PAT_Sept30 PhysicsTools/PFCandProducer
+#cvs co -r Colin_384_noMoreJetPtCutInPF2PAT_Sept30 PhysicsTools/PatAlgos
 
 # TTbar-dilepton packages and its dependences
 cvs co -d PFAnalyses/TTbarDIL UserCode/PFAnalyses/TTbarDIL
-cvs co -d KoPFA UserCode/KoPFA
-cvs co -r V00-02-01 ElectroWeakAnalysis/WENu
+cvs co -r TJ_Mar28 -d KoPFA UserCode/KoPFA
+cvs co -r TJ_Mar31 -d KoPFA/TopAnalyzer UserCode/KoPFA/TopAnalyzer
+#cvs co -r V00-02-01 ElectroWeakAnalysis/WENu
 
 # dont forget this
 checkdeps -a
@@ -31,4 +33,3 @@ rm -rf ISpy Fireworks
 
 ## Build'em all
 #scram build
-
