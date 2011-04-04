bf34781e3ad4f1f4715991df27b7106253d00d31
bhlee <>
2011-04-04 16:01:54 +0000

    *** empty log message ***

---
+++ TopAnalyzer/prod/MU/MC/pf2pat_muon_cfg.py
@@ -0,0 +1,34 @@
+import FWCore.ParameterSet.Config as cms
+
+from KoPFA.TopAnalyzer.pf2pat_template_cfg import *
+
+## Source
+process.source = cms.Source("PoolSource",
+                                fileNames = cms.untracked.vstring(
+  '/store/data/Run2011A/DoubleMu/RECO/PromptReco-v1/000/161/312/9CD2FBA7-E557-E011-B433-001D09F29849.root'
+  )
+)
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+
+process.load("HLTrigger.HLTfilters.hltHighLevel_cfi")
+process.hltHighLevel.HLTPaths = cms.vstring()
+process.hltHighLevel.throw = cms.bool(False)
+process.load("KoPFA.TopAnalyzer.triggerFilterByRun_cfi")
+
+process.acceptedMuons = cms.EDFilter("PATMuonSelector",
+    src = cms.InputTag("selectedPatMuonsPFlow"),
+    cut =cms.string("pt > 20 && abs(eta) < 2.4")
+)
+
+process.patMuonFilter = cms.EDFilter("CandViewCountFilter",
+  src = cms.InputTag('acceptedMuons'),
+  minNumber = cms.uint32(2)
+)
+
+#process.p += process.muonTriggerFilterByRun 
+process.p += getattr(process,"patPF2PATSequence"+postfix)
+process.p += process.acceptedMuons
+process.p += process.patMuonFilter
+
+process.MessageLogger.cerr.FwkReport.reportEvery = 100
