c0be7b03e8f796a431afad64617887f416866530
tjkim <>
2011-04-06 16:10:41 +0000

    to be able to turn on togle for MC

--- TopAnalyzer/prod/ELE/MC/pf2pat_electron_cfg.py
+++ TopAnalyzer/prod/ELE/MC/pf2pat_electron_cfg.py
@@ -2,12 +2,16 @@ import FWCore.ParameterSet.Config as cms
 
 from KoPFA.TopAnalyzer.pf2pat_template_cfg import *
 
+postfix = "PFlow"
+jetAlgo="AK5"
+usePF2PAT(process,runPF2PAT=True, jetAlgo=jetAlgo, runOnMC=True, postfix=postfix)
+
+#REMOVE ISOLATION FROM PF2PAT!!!
+process.pfIsolatedMuonsPFlow.combinedIsolationCut = cms.double(999)
+process.pfIsolatedElectronsPFlow.combinedIsolationCut = cms.double(999)
+
 ## Source
-process.source = cms.Source("PoolSource",
-                                fileNames = cms.untracked.vstring(
-  '/store/data//Run2011A/DoubleElectron/RECO/PromptReco-v1/000/161/312/0013721F-0358-E011-B924-003048F1C832.root'
-  )
-)
+# MC sample will be taken automatically from template file
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
@@ -30,5 +34,12 @@ process.patElectronFilter = cms.EDFilter("CandViewCountFilter",
 process.p += getattr(process,"patPF2PATSequence"+postfix)
 process.p += process.acceptedElectrons
 process.p += process.patElectronFilter
+
+# top projections in PF2PAT:
+getattr(process,"pfNoPileUp"+postfix).enable = True
+getattr(process,"pfNoMuon"+postfix).enable = True
+getattr(process,"pfNoElectron"+postfix).enable = True
+getattr(process,"pfNoTau"+postfix).enable = False # to use tau-cleaned jet collection : True
+getattr(process,"pfNoJet"+postfix).enable = True
 
 process.MessageLogger.cerr.FwkReport.reportEvery = 100
--- TopAnalyzer/prod/ELE/RD/pf2pat_electron_cfg.py
+++ TopAnalyzer/prod/ELE/RD/pf2pat_electron_cfg.py
@@ -2,6 +2,14 @@ import FWCore.ParameterSet.Config as cms
 
 from KoPFA.TopAnalyzer.pf2pat_template_cfg import *
 
+postfix = "PFlow"
+jetAlgo="AK5"
+usePF2PAT(process,runPF2PAT=True, jetAlgo=jetAlgo, runOnMC=False, postfix=postfix)
+
+#REMOVE ISOLATION FROM PF2PAT!!!
+process.pfIsolatedMuonsPFlow.combinedIsolationCut = cms.double(999)
+process.pfIsolatedElectronsPFlow.combinedIsolationCut = cms.double(999)
+
 ## Source
 process.source = cms.Source("PoolSource",
                                 fileNames = cms.untracked.vstring(
@@ -30,5 +38,12 @@ process.patElectronFilter = cms.EDFilter("CandViewCountFilter",
 process.p += getattr(process,"patPF2PATSequence"+postfix)
 process.p += process.acceptedElectrons
 process.p += process.patElectronFilter
+
+# top projections in PF2PAT:
+getattr(process,"pfNoPileUp"+postfix).enable = True
+getattr(process,"pfNoMuon"+postfix).enable = True
+getattr(process,"pfNoElectron"+postfix).enable = True
+getattr(process,"pfNoTau"+postfix).enable = False # to use tau-cleaned jet collection : True
+getattr(process,"pfNoJet"+postfix).enable = True
 
 process.MessageLogger.cerr.FwkReport.reportEvery = 100
