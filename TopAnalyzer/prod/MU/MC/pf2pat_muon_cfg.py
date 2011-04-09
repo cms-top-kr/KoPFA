e449f14861b2693290ed00e0fb587d5051a81283
bhlee <>
2011-04-09 04:27:07 +0000

    *** empty log message ***

--- TopAnalyzer/prod/MU/MC/pf2pat_muon_cfg.py
+++ TopAnalyzer/prod/MU/MC/pf2pat_muon_cfg.py
@@ -12,6 +12,8 @@ process.pfIsolatedElectronsPFlow.combinedIsolationCut = cms.double(999)
 
 ## Source
 # MC sample will be taken automatically from template file
+process.source.fileNames = [ ## 
+'/store/relval/CMSSW_4_1_3/RelValZMM/GEN-SIM-RECO/START311_V2-v1/0037/EEB7C520-C751-E011-94C9-0030486790BE.root' ]
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
@@ -34,6 +36,7 @@ process.patMuonFilter = cms.EDFilter("CandViewCountFilter",
 process.p += getattr(process,"patPF2PATSequence"+postfix)
 process.p += process.acceptedMuons
 process.p += process.patMuonFilter
+process.out.outputCommands += cms.untracked.vstring(*patExtraAodEventContent)
 
 # top projections in PF2PAT:
 getattr(process,"pfNoPileUp"+postfix).enable = True
