aed09bed28880b4d2fa2c47bf151b438e893b850
jhgoh <>
2011-05-02 08:57:57 +0000

    electronID name

--- TopAnalyzer/python/topLeptonSelector_cfi.py
+++ TopAnalyzer/python/topLeptonSelector_cfi.py
@@ -25,7 +25,7 @@ Electrons = cms.EDProducer(
     version = cms.untracked.int32( 5 ),# 10 = MVA, 5 = WP90?
     electronLabel  = cms.InputTag("acceptedElectrons"),
     beamSpotLabel = cms.InputTag("offlineBeamSpot"),
-    eidName = cms.untracked.string("eidTight"),
+    eidName = cms.untracked.string("eidTightMC"),
     eidBitMask = cms.untracked.int32(5), # = 101, ID and cms.Conversion
 )
 
