b5685743fc0998711da9070c8d47876135f2e4cd
tjkim <>
2011-05-02 10:43:29 +0000

    add mean plot

--- TopAnalyzer/test/relttbar.C
+++ TopAnalyzer/test/relttbar.C
@@ -9,7 +9,18 @@
 #include "TGraphAsymmErrors.h"
 
 using namespace std;
-bool print = false;
+bool print = true;
+
+void SetLatex(double x, double y, TString decayMode){
+  TLatex *label= new TLatex;
+  label->SetNDC();
+  label->SetTextSize(0.05);
+  label->DrawLatex(x,y,"at #sqrt{s} = 7 TeV");
+  if( decayMode.Contains("MuMu")) text = "in #mu#mu decay mode";
+  if( decayMode.Contains("MuEl")) text = "in e#mu decay mode";
+  if( decayMode.Contains("ElEl")) text = "in ee decay mode";
+  label->DrawLatex(x,y-0.05,text);
+}
 
 void resolutionPlot(TTree* t1, TString var, TCut cut, TString name){
 
@@ -44,8 +55,8 @@ void resolutionPlot(TTree* t1, TString var, TCut cut, TString name){
     h[i]->GetYaxis()->SetTitle("Events");
   }
 
-  TCanvas *c_resvsmass = new TCanvas(Form("c_resvsmass_%s",name.Data()),Form("c_resvsmass_%s",name.Data()),400,400);
   TGraphAsymmErrors* gr = new TGraphAsymmErrors;
+  TGraphAsymmErrors* grmean = new TGraphAsymmErrors;
   for(int i=0; i < nDet; i++){
     h[i]->Fit("gaus");
     g[i]  = h[i]->GetFunction("gaus");
@@ -54,23 +65,45 @@ void resolutionPlot(TTree* t1, TString var, TCut cut, TString name){
     double mean = g[i]->GetParameter(1);
     double sigma = g[i]->GetParameter(2);
     double mass = (detBins[i+1] + detBins[i])/2;
+    double meanerr = g[i]->GetParError(1);
+    double sigmaerr = g[i]->GetParError(2);
     gr->SetPoint(i,mass,sigma);
-    cout <<  "point= " << mass <<  " MEAN= " <<  mean << " RMS= " << rms << " sigma= " << sigma << "\n";
+    gr->SetPointEYhigh(i,sigmaerr);
+    gr->SetPointEYlow(i,sigmaerr);
+    grmean->SetPoint(i,mass,mean);
+    grmean->SetPointEYhigh(i,meanerr);
+    grmean->SetPointEYlow(i,meanerr);
+    cout <<  "point= " << mass <<  " MEAN= " <<  mean << " RMS= " << rms << " sigma= " << sigma << " error= " << sigmaerr << " mean= " << mean << " error= " << meanerr << "\n";
   }
+
+  TCanvas *c_resvsmass = new TCanvas(Form("c_resvsmass_%s",name.Data()),Form("c_resvsmass_%s",name.Data()),400,400);
   gr->Draw("ALP");
   gr->GetXaxis()->SetTitle("Generated Mass (GeV)");
   gr->GetYaxis()->SetTitle("Resolution (#DeltaM/M)");
+  gr->SetMaximum(0.48);
+  gr->SetMinimum(0.08);
+  SetLatex(0.55,0.85,decayMode);
+
+  TCanvas *c_meanvsmass = new TCanvas(Form("c_meanvsmass_%s",name.Data()),Form("c_meanvsmass_%s",name.Data()),400,400);
+  grmean->Draw("ALP");
+  grmean->GetXaxis()->SetTitle("Generated Mass (GeV)");
+  grmean->GetYaxis()->SetTitle("Mean <#DeltaM/M>");
+  grmean->SetMaximum(0.1);
+  grmean->SetMinimum(-0.3);
+  SetLatex(0.55,0.85,decayMode);
 
   if(print){
-    c_res->Print(Form("c_resCanvas_%s.eps",name.Data()));
-    c_res->Print(Form("c_resCanvas_%s.png",name.Data()));
-    c_resvsmass->Print(Form("c_resolution_%s.eps",name.Data()));
-    c_resvsmass->Print(Form("c_resolution_%s.png",name.Data()));
+    c_res->Print(Form("c_resCanvas_%s_%s.eps",name.Data(),decayMode.Data()));
+    c_res->Print(Form("c_resCanvas_%s_%s.png",name.Data(),decayMode.Data()));
+    c_resvsmass->Print(Form("c_resolution_%s_%s.eps",name.Data(),decayMode.Data()));
+    c_resvsmass->Print(Form("c_resolution_%s_%s.png",name.Data(),decayMode.Data()));
+    c_meanvsmass->Print(Form("c_mean_%s_%s.eps",name.Data(),decayMode.Data()));
+    c_meanvsmass->Print(Form("c_mean_%s_%s.png",name.Data(),decayMode.Data()));
   }
 }
 
 
-TString decayMode = "MuEl";
+TString decayMode = "MuMu";
 
 void relttbar(){
 
@@ -83,13 +116,14 @@ void relttbar(){
   TTree * t = (TTree *) f->Get(decayMode+"/tree");
 
   TCut finalcut = "Z.mass() > 12 && relIso04lep1 < 0.20 && relIso04lep2 < 0.20 && Z.sign() < 0 && @jetspt30.size() >= 2 && abs(Z.mass() - 91 ) > 15 && MET > 30";
-  //TCut mt2 = "maosMt2 > 150 && maosMt2 < 250";
-  TCut mt2 = "ttbar.Mt2() > 120 && ttbar.Mt2() < 200";
+  //TCut finalcut = "Z.mass() > 12 && relIso04lep1 < 0.20 && relIso04lep2 < 0.20 && Z.sign() < 0 && @jetspt30.size() >= 2 && abs(Z.mass() - 91 ) > 15";
+  TCut mt2 = "maosMt2 > 120 && maosMt2 < 230";
+  //TCut mt2 = "ttbar.Mt2() > 120 && ttbar.Mt2() < 230";
   TCut cut = finalcut && mt2;
  
-  //resolutionPlot(t, "maosttbarM", cut, "maos");
+  //resolutionPlot(t, "maosttbarM", finalcut, "maos");
   //resolutionPlot(t, "vsumttbarM", finalcut, "vsum");
-  resolutionPlot(t, "ttbar.M()", finalcut, "vsum");
-  //resolutionPlot(t, "ttbar.maosM()", finalcut, "maos");
+  resolutionPlot(t, "ttbar.M()", cut, "vsum");
+  //resolutionPlot(t, "ttbar.maosM()", cut, "maos");
 
 }
