2098caa1e742d637aa7b2821b7b21a105e6d49fb
jhgoh <>
2011-05-03 06:29:19 +0000

    first commit

---
+++ TopAnalyzer/test/batch/ElEl/topElElAnalyzer_TTbar_others_cfg.py
@@ -0,0 +1,34 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("Ntuple")
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+process.load('Configuration/StandardSequences/Services_cff')
+
+process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
+#process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(2621) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+process.MessageLogger.cerr.FwkReport.reportEvery = 1000
+
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring()
+)
+
+process.TFileService = cms.Service("TFileService",
+    fileName = cms.string('vallot_TTbar_others.root')
+)
+
+process.load("PFAnalyses.TTbarDIL.Sources.ELE.MC.Fall10.patTuple_TTbar_cff")
+process.source.fileNames = [
+#    'file:/data/export/jhgoh/TopAnalysis/pf2pat/ElEl/MC/20110425/TTbar.root',
+    'rfio:/castor/cern.ch/user/j/jhgoh/TopAnalysis/pf2pat/ElEl/MC/20110425/TTbar.root',
+]
+process.load("KoPFA.TopAnalyzer.topAnalysis_cff")
+
+process.topWLeptonGenFilter.applyFilter = True
+process.topElElAnalysisMCSequence.replace(process.topWLeptonGenFilter, ~process.topWLeptonGenFilter)
+
+process.p = cms.Path(
+    process.topElElAnalysisMCSequence
+)
+
