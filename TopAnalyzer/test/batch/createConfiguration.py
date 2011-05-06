da092758a9fe194eca0a635861bc1d33a08d8d86
tjkim <>
2011-05-06 03:13:40 +0000

    to produce ntuple centrally

---
+++ TopAnalyzer/test/batch/createConfiguration.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python
+# Author: Tae.Jeong.Kim@cern.ch
+# How to run: ./createConfiguration MuMu
+import os
+import re
+import sys
+import time
+import commands
+
+decay = sys.argv[1]
+
+mclist = ["ZJets", "ZtauDecay", "DYmm20to50", "DYee20to50", "DYtt20to50", "WJetsToLNu", "VVJets", "TTbarTuneZ2","SingleToptW","QCD"]
+datalist = ["Run2011A"]
+
+def common():
+  script = """import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("Ntuple")
+
+process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring()
+)
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+process.MessageLogger.cerr.FwkReport.reportEvery = 100
+
+process.load("KoPFA.TopAnalyzer.topAnalysis_cff")
+"""
+  return script
+
+def mcsample(src):
+  script = """
+process.load("KoPFA.TopAnalyzer.Sources.EMU.MC.Spring11.patTuple_%s_cff")
+""" % src
+  return script
+
+
+def mcpath():
+  script = """
+process.p = cms.Path(
+    process.top%sAnalysisMCSequence
+) 
+""" % decay
+  return script
+
+def rdsample(src):
+  script = """
+process.load("KoPFA.TopAnalyzer.Sources.MU.RD.patTuple_%s_cff")
+""" % src
+  return script
+
+
+def rdpath():
+  script = """
+process.p = cms.Path(
+    process.top%sAnalysisRealDataSequence
+) 
+""" % decay
+  return script
+
+def outfile(src):
+  script = """
+process.TFileService = cms.Service("TFileService",
+    fileName = cms.string('vallot_%s.root')
+)
+""" % src
+  return script
+
+def ttbarfilter():
+  script = """
+process.topWLeptonGenFilter.applyFilter = True
+"""
+  return script
+
+def genzmassfilter():
+  script = """
+process.GenZmassFilter.applyFilter = True
+process.GenZmassFilter.decayMode = [11, 13]
+"""
+  return script
+
+def genzmasstaudecayfilter():
+  script = """
+process.GenZmassFilter.applyFilter = True
+process.GenZmassFilter.decayMode = [15]
+"""
+  return script
+
+def genzlowmassfilter():
+  script = """
+process.GenZmassFilter.applyFilter = True
+process.GenZmassFilter.max = 50
+"""
+  return script
+
+#os.system("mkdir "+decay)
+ 
+for src in mclist:
+    out = open(decay+'/top'+decay+'Analyzer_'+src+'_cfg.py','w')
+    out.write(common())
+    out.write(mcpath())
+    out.write(outfile(src))
+    if src.find("ZtauDecay") != -1:
+      out.write(mcsample("ZJets"))
+    else:
+      out.write(mcsample(src))
+    if src.find("TTbar") != -1:
+      out.write(ttbarfilter())
+    if src.find("ZJets") != -1:
+      out.write(genzmassfilter())
+    if src.find("ZtauDecay") != -1:
+      out.write(genzmasstaudecayfilter())
+    if src.find("DY20to50") != -1:
+      out.write(genzlowmassfilter())
+    out.close()
+
+for src in datalist:
+    out = open(decay+'/top'+decay+'Analyzer_'+src+'_cfg.py','w')
+    out.write(common())
+    out.write(rdpath())
+    out.write(outfile(src))
+    out.write(rdsample(src))
+    out.close()
---
+++ TopAnalyzer/test/batch/produceNtuple.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# Author: Tae.Jeong.Kim@cern.ch
+# How to run: ./createNtuple data MuMu 
+import os
+import re
+import sys
+import time
+
+sample = sys.argv[1]
+decay = sys.argv[2]
+
+mclist = ["ZJets", "ZtauDecay", "DYmm20to50", "DYee20to50", "DYtt20to50", "WJetsToLNu", "VVJets", "TTbarTuneZ2","SingleToptW"]
+qcdlist = ["QCD"]
+datalist = ["Run2011A"]
+
+def processSample(sample, dir):
+    os.system("rm -rf "+dir+"/Log/"+sample)
+    os.system("rm -rf "+dir+"/Res/"+sample)
+    os.system("rfmkdir "+dir+"/Res/"+sample)
+    os.system("cmsBatch.py 1 "+decay+"/top"+decay+"Analyzer_"+sample+"_cfg.py -o "+"Out/"+decay+"/Log/"+sample+" -r "+dir+"/Res/"+sample+" -b 'bsub -q 1nh < batchScript.sh'")
+
+import os,commands
+currdir = commands.getoutput('pwd') 
+print currdir
+
+outdir = currdir+"/Out/"+decay
+
+#if you want to save ntuple in castor
+#mcdir = "/castor/cern.ch/user/t/tjkim/ntuple/top/"+decay
+#datadir = "/castor/cern.ch/user/t/tjkim/ntuple/top/"+decay
+
+#to save log information in local
+os.system("rfmkdir Out")
+os.system("rfmkdir "+outdir)
+os.system("rfmkdir "+outdir+"/Log")
+os.system("rfmkdir "+outdir+"/Res")
+
+if sample == "all":
+  for s in mclist:
+    processSample(s,outdir)
+  time.sleep(60)
+  for s in datalist:
+    processSample(s,outdir)
+elif sample == "mc":
+  for s in mclist:
+    processSample(s,outdir)
+elif sample == "qcd":
+  for s in qcdlist:
+    processSample(s,outdir)
+elif sample == "data":
+  for s in datalist:
+    processSample(s,outdir)
+else:
+  print "ERROR!"
+  print "OPTION: 'all MuMu', 'mc MuMu', 'data MuMu', 'qcd MuMu'"
