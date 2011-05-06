df3d699bd2eada05648580331f1fdd791ff7352b
jhgoh <>
2011-05-06 19:06:41 +0000

    Produce two ntuples for default and bypass in the same file

--- ElectronAnalysis/test/analyzeEleId_cfg.py
+++ ElectronAnalysis/test/analyzeEleId_cfg.py
@@ -11,23 +11,29 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 # register TFileService
 process.TFileService = cms.Service("TFileService",
-    fileName = cms.string('analyzeEleId_default.root')
+    fileName = cms.string('analyzeEleId.root')
 )
 
 ## Source
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-        'file:recoPF.root',
+        'file:rerecoPF.root',
     )
 )
 
 ## User defined modules and sequences
 process.genElectrons = cms.EDFilter("GenParticleSelector",
     src = cms.InputTag("genParticles"),
-    cut = cms.string("abs(pdgId) == 11 && abs(eta) < 2.4"),
+    cut = cms.string("abs(pdgId) == 11 && abs(eta) < 2.4 && pt > 10"),
 )
 
-process.e = cms.EDAnalyzer("ElectronAnalyzer",
+process.Default = cms.EDAnalyzer("ElectronAnalyzer",
+    genParticles = cms.InputTag("genElectrons"),
+    electron = cms.InputTag("electronsWithPresel"),
+    pfCandidate = cms.InputTag("particleFlow", "", "RECO"),
+)
+
+process.HZZVeryLoose = cms.EDAnalyzer("ElectronAnalyzer",
     genParticles = cms.InputTag("genElectrons"),
     electron = cms.InputTag("electronsHZZVeryLoose"),
     pfCandidate = cms.InputTag("pfElectronsID", "", "RERECOPF"),
@@ -35,6 +41,6 @@ process.e = cms.EDAnalyzer("ElectronAnalyzer",
 
 process.p = cms.Path(
     process.genElectrons
-  * process.e
+  * process.Default + process.HZZVeryLoose
 )
 
