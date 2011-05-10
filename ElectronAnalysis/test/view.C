13934a6edec8c99c480a5ea71692f6f8202f8562
jhgoh <>
2011-05-10 18:11:15 +0000

    Completing plots

--- ElectronAnalysis/test/view.C
+++ ElectronAnalysis/test/view.C
@@ -1,89 +1,127 @@
-void view(TString treeName = "HZZVeryLoose/tree")
+void view()
+{
+  gStyle->SetOptStat(111111);
+  view("HZZVeryLoose");
+  view("Default");
+}
+
+void view(TString dirName)
 {
   TFile* f = TFile::Open("analyzeEleId.root");
 
-  TTree* tree = (TTree*)f->Get(treeName);
+  TTree* tree = (TTree*)f->Get(dirName+"/tree");
 
-  TH1F* hGenPt = new TH1F("hGenPt", "hPt;p_{T} (GeV/c);Entries", 20, 0, 100);
-  TH1F* hGsfPt = new TH1F("hGsfPt", "hPt;p_{T} (GeV/c);Entries", 20, 0, 100);
-  TH1F* hPfaPt = new TH1F("hPfaPt", "hPt;p_{T} (GeV/c);Entries", 20, 0, 100);
+  TH1F* hGenPt = new TH1F(dirName+"_hGenPt", "Pt;p_{T} (GeV/c);Entries", 20, 0, 100);
+  TH1F* hGsfPt = new TH1F(dirName+"_hGsfPt", "Pt;p_{T} (GeV/c);Entries", 20, 0, 100);
+  TH1F* hPfaPt = new TH1F(dirName+"_hPfaPt", "Pt;p_{T} (GeV/c);Entries", 20, 0, 100);
 
-  TH1F* hGenEta = new TH1F("hGenEta", "hEta;#eta;Entries", 20, -3.15, 3.15);
-  TH1F* hGsfEta = new TH1F("hGsfEta", "hEta;#eta;Entries", 20, -3.15, 3.15);
-  TH1F* hPfaEta = new TH1F("hPfaEta", "hEta;#eta;Entries", 20, -3.15, 3.15);
+  TH1F* hGenEta = new TH1F(dirName+"_hGenEta", "Eta;#eta;Entries", 20, -3.15, 3.15);
+  TH1F* hGsfEta = new TH1F(dirName+"_hGsfEta", "Eta;#eta;Entries", 20, -3.15, 3.15);
+  TH1F* hPfaEta = new TH1F(dirName+"_hPfaEta", "Eta;#eta;Entries", 20, -3.15, 3.15);
 
-  TH1F* hGenPhi = new TH1F("hGenPhi", "hPhi;#phi;Entries", 20, -3.15, 3.15);
-  TH1F* hGsfPhi = new TH1F("hGsfPhi", "hPhi;#phi;Entries", 20, -3.15, 3.15);
-  TH1F* hPfaPhi = new TH1F("hPfaPhi", "hPhi;#phi;Entries", 20, -3.15, 3.15);
+  TH1F* hGenPhi = new TH1F(dirName+"_hGenPhi", "Phi;#phi;Entries", 20, -3.15, 3.15);
+  TH1F* hGsfPhi = new TH1F(dirName+"_hGsfPhi", "Phi;#phi;Entries", 20, -3.15, 3.15);
+  TH1F* hPfaPhi = new TH1F(dirName+"_hPfaPhi", "Phi;#phi;Entries", 20, -3.15, 3.15);
 
-  TH1F* hGsfPtRes = new TH1F("hGsfPtRes", "hPtRes;#Deltap_{T}/p_{T}^{Gen};Entries", 20, -5, 5);
-  TH1F* hPfaPtRes = new TH1F("hPfaPtRes", "hPtRes;#Deltap_{T}/p_{T}^{Gen};Entries", 20, -5, 5);
+  TH1F* hGsfPtRes = new TH1F(dirName+"_hGsfPtRes", "Pt Residual;#Deltap_{T}/p_{T}^{Gen};Entries", 50, -3, 3);
+  TH1F* hPfaPtRes = new TH1F(dirName+"_hPfaPtRes", "Pt Residual;#Deltap_{T}/p_{T}^{Gen};Entries", 50, -3, 3);
 
-  hGenPt->SetLineColor(kBlack);
-  hGsfPt->SetLineColor(kRed);
-  hPfaPt->SetLineColor(kBlue);
+  TH1F* hGsfMva = new TH1F(dirName+"_hGsfMva", "Mva;MVA;Entries", 100, -2, 1.5);
+  TH1F* hPfaMva = new TH1F(dirName+"_hPfaMva", "Mva;MVA;Entries", 100, -2, 1.5);
 
-  hGenEta->SetLineColor(kBlack);
-  hGsfEta->SetLineColor(kRed);
-  hPfaEta->SetLineColor(kBlue);
+  tree->Project(dirName+"_hGenPt", "genPt");
+  tree->Project(dirName+"_hGsfPt", "gsfPt", "gsfPt>0");
+  tree->Project(dirName+"_hPfaPt", "pfaPt", "pfaPt>0");
 
-  hGenPhi->SetLineColor(kBlack);
-  hGsfPhi->SetLineColor(kRed);
-  hPfaPhi->SetLineColor(kBlue);
+  tree->Project(dirName+"_hGenEta", "genEta", "genPt>0");
+  tree->Project(dirName+"_hGsfEta", "gsfEta", "gsfPt>0");
+  tree->Project(dirName+"_hPfaEta", "pfaEta", "pfaPt>0");
 
-  hGsfPtRes->SetLineColor(kRed);
-  hPfaPtRes->SetLineColor(kBlue);
+  tree->Project(dirName+"_hGenPhi", "genPhi", "genPt>0");
+  tree->Project(dirName+"_hGsfPhi", "gsfPhi", "gsfPt>0");
+  tree->Project(dirName+"_hPfaPhi", "pfaPhi", "pfaPt>0");
 
-  hGenEta->SetLineColor(kBlack);
-  hGsfEta->SetLineColor(kRed);
-  hPfaEta->SetLineColor(kBlue);
+  tree->Project(dirName+"_hGsfPtRes", "(gsfPt-genPt)/genPt", "gsfPt>0&&genPt>0");
+  tree->Project(dirName+"_hPfaPtRes", "(pfaPt-genPt)/genPt", "pfaPt>0&&genPt>0");
 
-  hGenPt->SetMinimum(0);
-  hGenEta->SetMinimum(0);
-  hGenPhi->SetMinimum(0);
-  hGsfPtRes->SetMinimum(0);
+  tree->Project(dirName+"_hGsfMva", "gsfMva", "gsfPt>0");
+  tree->Project(dirName+"_hPfaMva", "pfaMva", "pfaPt>0");
 
-  tree->Project("hGenPt", "genElePt");
-  tree->Project("hGsfPt", "gsfElePt");
-  tree->Project("hPfaPt", "pfaElePt", "pfaElePt>10");
+  tree->Scan("gsfPt:pfaPt:gsfE:pfaE:gsfEcalE:pfaEcalE:gsfMva:pfaMva:gsfToPfaMatch", "gsfPt>0 && gsfMva<-2");
 
-  tree->Project("hGenEta", "genEleEta");
-  tree->Project("hGsfEta", "gsfEleEta");
-  tree->Project("hPfaEta", "pfaEleEta", "pfaElePt>10");
-
-  tree->Project("hGenPhi", "genElePhi");
-  tree->Project("hGsfPhi", "gsfElePhi");
-  tree->Project("hPfaPhi", "pfaElePhi", "pfaElePt>10");
-
-  tree->Project("hGsfPtRes", "(gsfElePt-genElePt)/genElePt");
-  tree->Project("hPfaPtRes", "(pfaElePt-genElePt)/genElePt", "pfaElePt>10");
-
-  TCanvas* c = 0;
-
-  c = new TCanvas("cPt", "cPt", 600, 600);
-  hGenPt->Draw();
-  hGsfPt->Draw("sames");
-  hPfaPt->Draw("sames");
-  c->Print("cPt.png");
-
-  c = new TCanvas("cEta", "cEta", 600, 600);
-  hGenEta->Draw();
-  hGsfEta->Draw("sames");
-  hPfaEta->Draw("sames");
-  c->Print("cEta.png");
-
-  c = new TCanvas("cPhi", "cPhi", 600, 600);
-  hGenPhi->Draw();
-  hGsfPhi->Draw("sames");
-  hPfaPhi->Draw("sames");
-  c->Print("cPhi.png");
-
-  c = new TCanvas("cRes", "cRes", 600, 600);
-  hGsfPtRes->Draw();
-  hPfaPtRes->Draw("sames");
-  c->Print("cRes.png");
-  
+  overlay(dirName+"_Pt", hGenPt, hGsfPt, hPfaPt);
+  overlay(dirName+"_Eta", hGenEta, hGsfEta, hPfaEta);
+  overlay(dirName+"_Phi", hGenPhi, hGsfPhi, hPfaPhi);
+  overlay(dirName+"_Res", 0, hGsfPtRes, hPfaPtRes);
+  overlay(dirName+"_Mva", 0, hGsfMva, hPfaMva);
 
 }
 
+void overlay(TString name, TH1* h1, TH1* h2, TH1* h3)
+{
+  TCanvas* c = new TCanvas("c"+name, name, 600, 600);
+  TString opt = "";
+  double statPosY = 0.98;
+  const double dY = 0.25;
+  if ( h1 ) 
+  {
+    h1->SetLineColor(kBlack);
+    h1->SetMinimum(0);
+    h1->Draw(opt);
 
+    c->Update();
+    TPaveStats* st = (TPaveStats*)c->GetPrimitive("stats");
+    if ( st )
+    {
+      st->SetName(h1->GetName());
+      st->SetLineColor(h1->GetLineColor());
+      st->SetY1NDC(statPosY-dY);
+      st->SetY2NDC(statPosY);
+      statPosY-=dY;
+    }
+
+    opt = "sames";
+  }
+
+  if ( h2 )
+  {
+    h2->SetLineColor(kRed);
+    h2->SetMinimum(0);
+    h2->Draw(opt);
+
+    c->Update();
+    TPaveStats* st = (TPaveStats*)c->GetPrimitive("stats");
+    if ( st )
+    {
+      st->SetName(h2->GetName());
+      st->SetLineColor(h2->GetLineColor());
+      st->SetY1NDC(statPosY-dY);
+      st->SetY2NDC(statPosY);
+      statPosY-=dY;
+    }
+
+    opt = "sames";
+  }
+
+  if ( h3 )
+  {
+    h3->SetLineColor(kBlue);
+    h3->SetMinimum(0);
+    h3->Draw(opt);
+
+    c->Update();
+    TPaveStats* st = (TPaveStats*)c->GetPrimitive("stats");
+    if ( st )
+    {
+      st->SetName(h3->GetName());
+      st->SetLineColor(h3->GetLineColor());
+      st->SetY1NDC(statPosY-dY);
+      st->SetY2NDC(statPosY);
+      statPosY-=dY;
+    }
+
+    opt = "sames";
+  }
+
+  c->Print(name+".png");
+}
