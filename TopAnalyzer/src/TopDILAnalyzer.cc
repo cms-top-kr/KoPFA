4731875817de23c00208709104679b471f2ba5c9
tjkim <>
2011-05-13 07:35:52 +0000

    back to previous version

--- TopAnalyzer/src/TopDILAnalyzer.cc
+++ TopAnalyzer/src/TopDILAnalyzer.cc
@@ -4,9 +4,9 @@
 #include "DataFormats/PatCandidates/interface/Electron.h"
 #include "DataFormats/PatCandidates/interface/MET.h"
 
-typedef TopDILAnalyzer<pat::Muon,pat::Muon,pat::MET> TopMuMuAnalyzer;
-typedef TopDILAnalyzer<pat::Muon,pat::Electron,pat::MET> TopMuElAnalyzer;
-typedef TopDILAnalyzer<pat::Electron,pat::Electron,pat::MET> TopElElAnalyzer;
+typedef TopDILAnalyzer<pat::Muon,pat::Muon> TopMuMuAnalyzer;
+typedef TopDILAnalyzer<pat::Muon,pat::Electron> TopMuElAnalyzer;
+typedef TopDILAnalyzer<pat::Electron,pat::Electron> TopElElAnalyzer;
 
 DEFINE_FWK_MODULE(TopMuMuAnalyzer);
 DEFINE_FWK_MODULE(TopMuElAnalyzer);
