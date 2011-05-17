b34a35ef55f80a3995f31e6b15d1e0bf22b5574e
bhlee <>
2011-05-17 14:25:56 +0000

    *** empty log message ***

--- TopAnalyzer/python/Sources/MU/MC/Spring11/patTuple_ZJets_cff.py
+++ TopAnalyzer/python/Sources/MU/MC/Spring11/patTuple_ZJets_cff.py
@@ -1,21 +1,30 @@
-/castor/cern.ch/user/b/bhlee/2011mc/Spring11/Mar29PromptReco_v8/ZJets/patTuple_skim_1_1_Rf5.root
-/castor/cern.ch/user/b/bhlee/2011mc/Spring11/Mar29PromptReco_v8/ZJets/patTuple_skim_2_1_8c4.root
-/castor/cern.ch/user/b/bhlee/2011mc/Spring11/Mar29PromptReco_v8/ZJets/patTuple_skim_3_1_IEo.root
-/castor/cern.ch/user/b/bhlee/2011mc/Spring11/Mar29PromptReco_v8/ZJets/patTuple_skim_4_1_7kU.root
-/castor/cern.ch/user/b/bhlee/2011mc/Spring11/Mar29PromptReco_v8/ZJets/patTuple_skim_5_1_twh.root
-/castor/cern.ch/user/b/bhlee/2011mc/Spring11/Mar29PromptReco_v8/ZJets/patTuple_skim_6_1_lnw.root
-/castor/cern.ch/user/b/bhlee/2011mc/Spring11/Mar29PromptReco_v8/ZJets/patTuple_skim_7_1_6BI.root
-/castor/cern.ch/user/b/bhlee/2011mc/Spring11/Mar29PromptReco_v8/ZJets/patTuple_skim_8_1_U2L.root
-/castor/cern.ch/user/b/bhlee/2011mc/Spring11/Mar29PromptReco_v8/ZJets/patTuple_skim_9_1_ASe.root
-/castor/cern.ch/user/b/bhlee/2011mc/Spring11/Mar29PromptReco_v8/ZJets/patTuple_skim_10_1_BdS.root
-/castor/cern.ch/user/b/bhlee/2011mc/Spring11/Mar29PromptReco_v8/ZJets/patTuple_skim_11_1_Dss.root
-/castor/cern.ch/user/b/bhlee/2011mc/Spring11/Mar29PromptReco_v8/ZJets/patTuple_skim_12_1_GNA.root
-/castor/cern.ch/user/b/bhlee/2011mc/Spring11/Mar29PromptReco_v8/ZJets/patTuple_skim_13_1_651.root
-/castor/cern.ch/user/b/bhlee/2011mc/Spring11/Mar29PromptReco_v8/ZJets/patTuple_skim_14_1_Gp2.root
-/castor/cern.ch/user/b/bhlee/2011mc/Spring11/Mar29PromptReco_v8/ZJets/patTuple_skim_15_1_Ozr.root
-/castor/cern.ch/user/b/bhlee/2011mc/Spring11/Mar29PromptReco_v8/ZJets/patTuple_skim_16_1_vfZ.root
-/castor/cern.ch/user/b/bhlee/2011mc/Spring11/Mar29PromptReco_v8/ZJets/patTuple_skim_17_1_xVH.root
-/castor/cern.ch/user/b/bhlee/2011mc/Spring11/Mar29PromptReco_v8/ZJets/patTuple_skim_18_1_BJe.root
-/castor/cern.ch/user/b/bhlee/2011mc/Spring11/Mar29PromptReco_v8/ZJets/patTuple_skim_19_1_Fjp.root
-/castor/cern.ch/user/b/bhlee/2011mc/Spring11/Mar29PromptReco_v8/ZJets/patTuple_skim_20_1_CPD.root
+import FWCore.ParameterSet.Config as cms
 
+readFiles = cms.untracked.vstring()
+source = cms.Source("PoolSource",
+    noEventSort = cms.untracked.bool(True),
+    duplicateCheckMode = cms.untracked.string("noDuplicateCheck"),
+    fileNames = readFiles
+)
+readFiles.extend([
+"rfio:/castor/cern.ch/user/b/bhlee/2011mc/Spring11/Mar29PromptReco_v8/ZJets/patTuple_skim_1_1_Rf5.root",
+"rfio:/castor/cern.ch/user/b/bhlee/2011mc/Spring11/Mar29PromptReco_v8/ZJets/patTuple_skim_2_1_8c4.root",
+"rfio:/castor/cern.ch/user/b/bhlee/2011mc/Spring11/Mar29PromptReco_v8/ZJets/patTuple_skim_3_1_IEo.root",
+"rfio:/castor/cern.ch/user/b/bhlee/2011mc/Spring11/Mar29PromptReco_v8/ZJets/patTuple_skim_4_1_7kU.root",
+"rfio:/castor/cern.ch/user/b/bhlee/2011mc/Spring11/Mar29PromptReco_v8/ZJets/patTuple_skim_5_1_twh.root",
+"rfio:/castor/cern.ch/user/b/bhlee/2011mc/Spring11/Mar29PromptReco_v8/ZJets/patTuple_skim_6_1_lnw.root",
+"rfio:/castor/cern.ch/user/b/bhlee/2011mc/Spring11/Mar29PromptReco_v8/ZJets/patTuple_skim_7_1_6BI.root",
+"rfio:/castor/cern.ch/user/b/bhlee/2011mc/Spring11/Mar29PromptReco_v8/ZJets/patTuple_skim_8_1_U2L.root",
+"rfio:/castor/cern.ch/user/b/bhlee/2011mc/Spring11/Mar29PromptReco_v8/ZJets/patTuple_skim_9_1_ASe.root",
+"rfio:/castor/cern.ch/user/b/bhlee/2011mc/Spring11/Mar29PromptReco_v8/ZJets/patTuple_skim_10_1_BdS.root",
+"rfio:/castor/cern.ch/user/b/bhlee/2011mc/Spring11/Mar29PromptReco_v8/ZJets/patTuple_skim_11_1_Dss.root",
+"rfio:/castor/cern.ch/user/b/bhlee/2011mc/Spring11/Mar29PromptReco_v8/ZJets/patTuple_skim_12_1_GNA.root",
+"rfio:/castor/cern.ch/user/b/bhlee/2011mc/Spring11/Mar29PromptReco_v8/ZJets/patTuple_skim_13_1_651.root",
+"rfio:/castor/cern.ch/user/b/bhlee/2011mc/Spring11/Mar29PromptReco_v8/ZJets/patTuple_skim_14_1_Gp2.root",
+"rfio:/castor/cern.ch/user/b/bhlee/2011mc/Spring11/Mar29PromptReco_v8/ZJets/patTuple_skim_15_1_Ozr.root",
+"rfio:/castor/cern.ch/user/b/bhlee/2011mc/Spring11/Mar29PromptReco_v8/ZJets/patTuple_skim_16_1_vfZ.root",
+"rfio:/castor/cern.ch/user/b/bhlee/2011mc/Spring11/Mar29PromptReco_v8/ZJets/patTuple_skim_17_1_xVH.root",
+"rfio:/castor/cern.ch/user/b/bhlee/2011mc/Spring11/Mar29PromptReco_v8/ZJets/patTuple_skim_18_1_BJe.root",
+"rfio:/castor/cern.ch/user/b/bhlee/2011mc/Spring11/Mar29PromptReco_v8/ZJets/patTuple_skim_19_1_Fjp.root",
+"rfio:/castor/cern.ch/user/b/bhlee/2011mc/Spring11/Mar29PromptReco_v8/ZJets/patTuple_skim_20_1_CPD.root",
+])
