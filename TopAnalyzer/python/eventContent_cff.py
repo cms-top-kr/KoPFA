880c8b116754ac89147d25c0d9223856b2d83181
tjkim <>
2011-05-19 20:09:26 +0000

    event content for top analysis

---
+++ TopAnalyzer/python/eventContent_cff.py
@@ -0,0 +1,17 @@
+import FWCore.ParameterSet.Config as cms
+
+## EventContents
+from PhysicsTools.PatAlgos.patEventContent_cff import *
+pf2patEventContent = cms.untracked.vstring()
+pf2patEventContent += patTriggerEventContent
+pf2patEventContent += patExtraAodEventContent
+pf2patEventContent += patEventContentNoCleaning
+pf2patEventContent +=[
+        'keep edmMergeableCounter_*_*_*',
+        'keep *_goodOfflinePrimaryVertices*_*_*',
+        'keep *_particleFlow_*_*',
+        'keep *_acceptedMuons_*_*',
+        'keep *_acceptedElectrons_*_*',
+        'keep double_*PFlow*_rho_PAT',
+]
+
