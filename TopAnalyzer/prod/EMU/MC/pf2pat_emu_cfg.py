8857e7cfd706d7909687460d2fcfcc71c36f911b
tjkim <>
2011-05-20 09:16:10 +0000

    separate leptons without isolation cut while keeping less isolated leptons in jet

--- TopAnalyzer/prod/EMU/MC/pf2pat_emu_cfg.py
+++ TopAnalyzer/prod/EMU/MC/pf2pat_emu_cfg.py
@@ -1,39 +1,44 @@
 import FWCore.ParameterSet.Config as cms
 
 from KoPFA.TopAnalyzer.pf2pat_template_cfg import *
+from KoPFA.TopAnalyzer.eventContent_cff import *
+from KoPFA.TopAnalyzer.tools import *
 
-#Apply PF2PAT
 postfix = "PFlow"
 jetAlgo="AK5"
 usePF2PAT(process,runPF2PAT=True, jetAlgo=jetAlgo, runOnMC=True, postfix=postfix)
 
-updateEventContent(process)
-
 #REMOVE ISOLATION FROM PF2PAT!!!
-process.pfIsolatedMuonsPFlow.combinedIsolationCut = cms.double(999)
-process.pfIsolatedElectronsPFlow.combinedIsolationCut = cms.double(999)
+addLooseLeptons(process)
+
+# top projections in PF2PAT:
+topProjection(process,postfix)
+
+# output
+process.out.outputCommands +=pf2patEventContent
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+process.MessageLogger.cerr.FwkReport.reportEvery = 100
+
+process.patMuonFilter.minNumber = 1 
+process.patElectronFilter.minNumber = 1
 
 ## Source
 #process.source = cms.Source("PoolSource",
 #                                fileNames = cms.untracked.vstring(
-#  '/store/data/Run2011A/MuEG/RECO/PromptReco-v1/000/161/312/C69F1A78-0958-E011-AC6B-003048F118C4.root'
+#  '/store/data/Run2011A/DoubleMu/RECO/PromptReco-v1/000/161/312/9CD2FBA7-E557-E011-B433-001D09F29849.root',
+#  '/store/data/Run2011A/DoubleElectron/AOD/PromptReco-v1/000/161/312/0E8D9A70-E857-E011-B3DC-001D09F29321.root',
+#  '/store/data/Run2011A/MuEG/AOD/PromptReco-v1/000/161/312/04850A2C-F757-E011-9A74-003048F024DC.root'
 #  )
 #)
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
-
-#process.p += process.hltHighLevelMuElMC 
+#process.p += process.hltHighLevelMuElMC
+process.p += process.nEventsHLT
 process.p += getattr(process,"patPF2PATSequence"+postfix)
+process.p += process.looseLeptonSequence
 process.p += process.acceptedElectrons
 process.p += process.acceptedMuons
 process.p += process.patElectronFilter
 process.p += process.patMuonFilter
+process.p += process.nEventsFiltered
 
-# top projections in PF2PAT:
-getattr(process,"pfNoPileUp"+postfix).enable = True
-getattr(process,"pfNoMuon"+postfix).enable = True
-getattr(process,"pfNoElectron"+postfix).enable = True
-getattr(process,"pfNoTau"+postfix).enable = False # to use tau-cleaned jet collection : True
-getattr(process,"pfNoJet"+postfix).enable = True
-
-process.MessageLogger.cerr.FwkReport.reportEvery = 100
--- TopAnalyzer/prod/MU/MC/pf2pat_muon_cfg.py
+++ TopAnalyzer/prod/MU/MC/pf2pat_muon_cfg.py
@@ -1,39 +1,44 @@
 import FWCore.ParameterSet.Config as cms
 
 from KoPFA.TopAnalyzer.pf2pat_template_cfg import *
+from KoPFA.TopAnalyzer.eventContent_cff import *
+from KoPFA.TopAnalyzer.tools import *
 
 postfix = "PFlow"
 jetAlgo="AK5"
-usePF2PAT(process,runPF2PAT=True, jetAlgo=jetAlgo, runOnMC=True, postfix=postfix)
-
-updateEventContent(process)
+usePF2PAT(process,runPF2PAT=True, jetAlgo=jetAlgo, runOnMC=MC, postfix=postfix)
 
 #REMOVE ISOLATION FROM PF2PAT!!!
-process.pfIsolatedMuonsPFlow.combinedIsolationCut = cms.double(999)
-process.pfIsolatedElectronsPFlow.combinedIsolationCut = cms.double(999)
+addLooseLeptons(process)
+
+# top projections in PF2PAT:
+topProjection(process,postfix)
+
+# output
+process.out.outputCommands +=pf2patEventContent
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+process.MessageLogger.cerr.FwkReport.reportEvery = 100
 
 process.patMuonFilter.minNumber = 2
+process.patElectronFilter.minNumber = 0
 
 ## Source
 #process.source = cms.Source("PoolSource",
 #                                fileNames = cms.untracked.vstring(
-#  '/store/data/Run2011A/DoubleMu/RECO/PromptReco-v1/000/161/312/9CD2FBA7-E557-E011-B433-001D09F29849.root'
+#  '/store/data/Run2011A/DoubleMu/RECO/PromptReco-v1/000/161/312/9CD2FBA7-E557-E011-B433-001D09F29849.root',
+#  '/store/data/Run2011A/DoubleElectron/AOD/PromptReco-v1/000/161/312/0E8D9A70-E857-E011-B3DC-001D09F29321.root',
+#  '/store/data/Run2011A/MuEG/AOD/PromptReco-v1/000/161/312/04850A2C-F757-E011-9A74-003048F024DC.root'
 #  )
 #)
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
-
-#process.p += process.hltHighLevelMuMuMC 
+#process.p += process.hltHighLevelMuMuMC
+process.p += process.nEventsHLT
 process.p += getattr(process,"patPF2PATSequence"+postfix)
+process.p += process.looseLeptonSequence
+process.p += process.acceptedElectrons
 process.p += process.acceptedMuons
+process.p += process.patElectronFilter
 process.p += process.patMuonFilter
-
-# top projections in PF2PAT:
-getattr(process,"pfNoPileUp"+postfix).enable = True
-getattr(process,"pfNoMuon"+postfix).enable = True
-getattr(process,"pfNoElectron"+postfix).enable = True
-getattr(process,"pfNoTau"+postfix).enable = False # to use tau-cleaned jet collection : True
-getattr(process,"pfNoJet"+postfix).enable = True
-
-process.MessageLogger.cerr.FwkReport.reportEvery = 100
+process.p += process.nEventsFiltered
 
