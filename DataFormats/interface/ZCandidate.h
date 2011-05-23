af83745b6fe2c6adcf2bc976cc6124c9d9187227
tjkim <>
2011-05-23 18:56:12 +0000

    fix bug:make it compatible with previous version

--- DataFormats/interface/ZCandidate.h
+++ DataFormats/interface/ZCandidate.h
@@ -22,16 +22,16 @@ namespace Ko{
     }
 
     ZCandidate(const reco::Candidate::LorentzVector& leg1, const reco::Candidate::LorentzVector& leg2, const float& sign) {
-      const Ko::Lepton lep1(leg1, (int) 0);
-      const Ko::Lepton lep2(leg2, (int) 0);
-      leg1_= lep1 ;
-      leg2_= lep2 ;
+      lv1_= leg1 ;
+      lv2_= leg2 ;
       mass_ = (leg1_+leg2_).M();
       sign_ = sign;
     }
 
     const Ko::Lepton& leg1() const { return leg1_; }
     const Ko::Lepton& leg2() const { return leg2_; }
+    const reco::Candidate::LorentzVector& lv1() const { return lv1_;}
+    const reco::Candidate::LorentzVector& lv2() const { return lv2_;}
 
     float mass() const { return mass_; }
     float sign() const { return sign_; }
@@ -41,6 +41,8 @@ namespace Ko{
 
     Ko::Lepton leg1_;
     Ko::Lepton leg2_;
+    reco::Candidate::LorentzVector lv1_;
+    reco::Candidate::LorentzVector lv2_;
 
     float mass_;
     float sign_;
