825df102cc5834080b46d9de3147b9a82656e1cd
jhgoh <>
2011-06-13 05:39:43 +0000

    Updated deltaR cone size

--- TopAnalyzer/python/Sources/ELE/MC/Spring11/patTuple_QCDPt80to170BCtoE_cff.py
+++ TopAnalyzer/python/Sources/ELE/MC/Spring11/patTuple_QCDPt80to170BCtoE_cff.py
@@ -8,5 +8,16 @@ source = cms.Source("PoolSource",
 )
 
 readFiles.extend([
-    'rfio:/castor/cern.ch/user/j/jhgoh/TopAnalysis/pf2pat/ElEl/MC/20110520/QCDPt80to170BCtoE.root',
+    'rfio:/castor/cern.ch/cms/store/user/jhgoh/TopAnalysis/pat/ElEl_/MC/20110601_1/QCDPt80to170BCtoE/patTuple_skim_1_1_g2m.root',
+    'rfio:/castor/cern.ch/cms/store/user/jhgoh/TopAnalysis/pat/ElEl_/MC/20110601_1/QCDPt80to170BCtoE/patTuple_skim_2_1_cdb.root',
+    'rfio:/castor/cern.ch/cms/store/user/jhgoh/TopAnalysis/pat/ElEl_/MC/20110601_1/QCDPt80to170BCtoE/patTuple_skim_3_1_zWg.root',
+    'rfio:/castor/cern.ch/cms/store/user/jhgoh/TopAnalysis/pat/ElEl_/MC/20110601_1/QCDPt80to170BCtoE/patTuple_skim_4_1_CNR.root',
+    'rfio:/castor/cern.ch/cms/store/user/jhgoh/TopAnalysis/pat/ElEl_/MC/20110601_1/QCDPt80to170BCtoE/patTuple_skim_5_2_QCc.root',
+    'rfio:/castor/cern.ch/cms/store/user/jhgoh/TopAnalysis/pat/ElEl_/MC/20110601_1/QCDPt80to170BCtoE/patTuple_skim_6_0_Jy1.root',
+    'rfio:/castor/cern.ch/cms/store/user/jhgoh/TopAnalysis/pat/ElEl_/MC/20110601_1/QCDPt80to170BCtoE/patTuple_skim_7_1_e84.root',
+    'rfio:/castor/cern.ch/cms/store/user/jhgoh/TopAnalysis/pat/ElEl_/MC/20110601_1/QCDPt80to170BCtoE/patTuple_skim_8_2_NTm.root',
+    'rfio:/castor/cern.ch/cms/store/user/jhgoh/TopAnalysis/pat/ElEl_/MC/20110601_1/QCDPt80to170BCtoE/patTuple_skim_9_0_Hus.root',
+    'rfio:/castor/cern.ch/cms/store/user/jhgoh/TopAnalysis/pat/ElEl_/MC/20110601_1/QCDPt80to170BCtoE/patTuple_skim_10_0_DJh.root',
+    'rfio:/castor/cern.ch/cms/store/user/jhgoh/TopAnalysis/pat/ElEl_/MC/20110601_1/QCDPt80to170BCtoE/patTuple_skim_11_2_Aur.root',
+    'rfio:/castor/cern.ch/cms/store/user/jhgoh/TopAnalysis/pat/ElEl_/MC/20110601_1/QCDPt80to170BCtoE/patTuple_skim_12_0_LBf.root',
 ])
