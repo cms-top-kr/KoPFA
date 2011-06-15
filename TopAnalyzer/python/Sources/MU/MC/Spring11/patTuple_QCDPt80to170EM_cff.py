eb700cf6f97393df68d70c9149523ead68ad043a
jhgoh <>
2011-06-15 02:47:37 +0000

    Spring11 QCD

---
+++ TopAnalyzer/python/Sources/MU/MC/Spring11/patTuple_QCDPt80to170EM_cff.py
@@ -0,0 +1,12 @@
+import FWCore.ParameterSet.Config as cms
+
+readFiles = cms.untracked.vstring()
+source = cms.Source("PoolSource",
+    noEventSort = cms.untracked.bool(True),
+    duplicateCheckMode = cms.untracked.string("noDuplicateCheck"),
+    fileNames = readFiles
+)
+readFiles.extend([
+    ## "ElEl" in the path is due to typo while producing patTuple. This will be fixed after processing is finished.
+    'rfio:/castor/cern.ch/cms/store/user/jhgoh/TopAnalysis/pat/ElEl/MC/20110601_1/QCDPt80to170EM.root',
+])
