d3e4d0a93f2eb50b042d816ba8f3a86f99b1fbd8
tjkim <>
2011-08-25 12:56:59 +0000

    add hamburg style

--- TopAnalyzer/test/style.h
+++ TopAnalyzer/test/style.h
@@ -1,4 +1,5 @@
 #include "TLatex.h"
+#include "TPaveText.h"
 
 void SetLatex(double x, double y){
   TLatex *label= new TLatex;
@@ -9,13 +10,12 @@ void SetLatex(double x, double y){
   label->DrawLatex(x,y-0.05,"#sqrt{s} = 7 TeV, #DeltaR=0.4");
 }
 
-void SetLatex2(double x, double y){
+void SetLabel(double x, double y, double lumi){
   TLatex *label= new TLatex;
   label->SetNDC();
   label->SetTextSize(0.05);
-  //label->DrawLatex(x,y,"CMS Preliminary 2010");
-  //label->DrawLatex(x,y-0.05,"11.4 nb^{-1} at #sqrt{s} = 7 TeV");
-  label->DrawLatex(x,y-0.05,"#sqrt{s} = 7 TeV");
+  label->DrawLatex(x,y,"CMS Preliminary");
+  label->DrawLatex(x,y-0.05,Form("%1.1f fb^{-1} at #sqrt{s} = 7 TeV",lumi/1000));
 }
 
 
@@ -23,6 +23,18 @@ void SetLegend(TH1* h1, TH1* h2, TString& leg1, TString& leg2, TString& o1, TStr
   TLegend *l= new TLegend(x1,y1,x2,y2);
   l->AddEntry(h1,Form("%s",leg1.Data()),Form("%s",o1.Data()));
   l->AddEntry(h2,Form("%s",leg2.Data()),Form("%s",o2.Data()));
+  l->SetTextSize(0.04);
+  l->SetFillColor(0);
+  l->SetLineColor(0);
+  l->Draw();
+}
+
+void SetLegend(TH1* h1, TH1* h2, TH1* h3, TGraphAsymmErrors* h4, TString& leg1, TString& leg2, TString& leg3, TString& leg4, TString& o1, TString& o2, TString& o3, TString& o4, double x1, double y1, double x2, double y2){
+  TLegend *l= new TLegend(x1,y1,x2,y2);
+  l->AddEntry(h1,Form("%s",leg1.Data()),Form("%s",o1.Data()));
+  l->AddEntry(h2,Form("%s",leg2.Data()),Form("%s",o2.Data()));
+  l->AddEntry(h3,Form("%s",leg3.Data()),Form("%s",o3.Data()));
+  l->AddEntry(h4,Form("%s",leg4.Data()),Form("%s",o4.Data()));
   l->SetTextSize(0.04);
   l->SetFillColor(0);
   l->SetLineColor(0);
@@ -126,4 +138,52 @@ void clearXErrorBar(TGraphAsymmErrors * gr)
    }
 }
 
+//This is for Hamburg style
+void DrawCMSLabels(bool cmsprelim=true, double luminosity=0.0, double textSize=0.04)
+{
+  // Draw official labels (CMS Preliminary, luminosity and CM energy) above plot
+
+  TPaveText *label = new TPaveText();
+
+  label -> SetX1NDC(gStyle->GetPadLeftMargin());
+  label -> SetY1NDC(1.0-gStyle->GetPadTopMargin());
+  label -> SetX2NDC(1.0-gStyle->GetPadRightMargin());
+  label -> SetY2NDC(1.0);
+  label -> SetTextFont(42);
+
+  if (cmsprelim)
+    {
+      label -> AddText(Form("CMS Preliminary, %2.1f fb^{-1} at #sqrt{s}=7 TeV",luminosity/1000));
+    }
+  else
+    {
+      label -> AddText(Form("%2.1f fb^{-1} at #sqrt{s}=7 TeV",luminosity));
+    }
+
+  label->SetFillStyle(0);
+  label->SetBorderSize(0);
+  if(textSize!=0) label->SetTextSize(textSize);
+  label->SetTextAlign(32);
+  label->Draw("same");
+}
+
+void DrawDecayChLabel(TString decaychannel="", double textSize=0.04)
+{
+  // Draw label for Decay Channel in upper left corner of plot
+
+  TPaveText *decch = new TPaveText();
+
+  decch -> AddText(decaychannel);
+
+  decch -> SetX1NDC(      gStyle->GetPadLeftMargin() + gStyle->GetTickLength()        );
+  decch -> SetY1NDC(1.0 - gStyle->GetPadTopMargin()  - gStyle->GetTickLength() - 0.05 );
+  decch -> SetX2NDC(      gStyle->GetPadLeftMargin() + gStyle->GetTickLength() + 0.15 );
+  decch -> SetY2NDC(1.0 - gStyle->GetPadTopMargin()  - gStyle->GetTickLength()        );
+
+  decch -> SetFillStyle(0);
+  decch -> SetBorderSize(0);
+  if(textSize!=0) decch->SetTextSize(textSize);
+  decch -> SetTextAlign(12);
+  decch -> Draw("same");
+}
 
