1df5c08ed3e4053e39a8b67e8cfed96177c617da
tjkim <>
2011-08-29 16:46:53 +0000

    update style for unfolding

--- TopAnalyzer/test/style.h
+++ TopAnalyzer/test/style.h
@@ -29,6 +29,17 @@ void SetLegend(TH1* h1, TH1* h2, TString& leg1, TString& leg2, TString& o1, TStr
   l->Draw();
 }
 
+void SetLegend(TH1* h1, TGraphAsymmErrors* h2, TString& leg1, TString& leg2, TString& o1, TString& o2, double x1, double y1, double x2, double y2){
+  TLegend *l= new TLegend(x1,y1,x2,y2);
+  l->AddEntry(h1,Form("%s",leg1.Data()),Form("%s",o1.Data()));
+  l->AddEntry(h2,Form("%s",leg2.Data()),Form("%s",o2.Data()));
+  l->SetTextSize(0.04);
+  l->SetFillColor(0);
+  l->SetLineColor(0);
+  l->Draw();
+}
+
+
 void SetLegend(TH1* h1, TH1* h2, TH1* h3, TGraphAsymmErrors* h4, TString& leg1, TString& leg2, TString& leg3, TString& leg4, TString& o1, TString& o2, TString& o3, TString& o4, double x1, double y1, double x2, double y2){
   TLegend *l= new TLegend(x1,y1,x2,y2);
   l->AddEntry(h1,Form("%s",leg1.Data()),Form("%s",o1.Data()));
@@ -49,6 +60,21 @@ void SetLegend(TGraphAsymmErrors* h1, TGraphAsymmErrors* h2, TString& leg1, TStr
   l->SetFillColor(0);
   l->SetLineColor(0);
   l->Draw();
+}
+
+void SetHistoStyle(TH1* h, double lwidth, double lcolor, double lstyle, double msize, double mcolor, double mstyle, double min, double max, TString& xtitle, TString& ytitle){
+  h->SetLineWidth(lwidth);
+  h->SetLineColor(lcolor);
+  h->SetLineStyle(lstyle);
+  h->SetMarkerSize(msize);
+  h->SetMarkerColor(mcolor);
+  h->SetMarkerStyle(mstyle);
+  h->SetMaximum(max);
+  h->SetMinimum(min);
+  h->SetStats(0);
+  h->SetTitle(0);
+  h->GetXaxis()->SetTitle(xtitle.Data());
+  h->GetYaxis()->SetTitle(ytitle.Data());
 }
 
 void StyleSimple(TH1* h1, TH1* h2, TString& xtitle, TString& ytitle ){
@@ -107,7 +133,7 @@ void Style2( TGraphAsymmErrors * h1, TGraphAsymmErrors * h2){
 
 void Print(TCanvas* c, TString& dir, const TString& hName, const TString& name, bool print){
   if(print){
-    c->Print(Form("%s/c_%s_%s.pdf",dir.Data(),hName.Data(),name.Data()));
+//    c->Print(Form("%s/c_%s_%s.pdf",dir.Data(),hName.Data(),name.Data()));
     c->Print(Form("%s/c_%s_%s.eps",dir.Data(),hName.Data(),name.Data()));   
     c->Print(Form("%s/c_%s_%s.png",dir.Data(),hName.Data(),name.Data()));
   }
