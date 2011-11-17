1f9c6820b4cae024f4699495438ec1785cb9aaaa
tjkim <>
2011-11-17 17:26:44 +0000

    reduce contents in pat

--- TopAnalyzer/python/eventContent_cff.py
+++ TopAnalyzer/python/eventContent_cff.py
@@ -2,18 +2,22 @@ import FWCore.ParameterSet.Config as cms
 
 ## EventContents
 from PhysicsTools.PatAlgos.patEventContent_cff import *
-pf2patEventContent = cms.untracked.vstring()
+pf2patEventContent = cms.untracked.vstring('drop *')
 pf2patEventContent += patTriggerEventContent
 pf2patEventContent += patExtraAodEventContent
-pf2patEventContent += patEventContentNoCleaning
+#pf2patEventContent += patEventContentNoCleaning
 pf2patEventContent +=[
         'keep edmMergeableCounter_*_*_*',
         'keep *_goodOfflinePrimaryVertices*_*_*',
-        'keep *_particleFlow_*_*',
+        'drop *_offlinePrimaryVertices*_*_*',
+        'drop *_towerMaker_*_*',
+#        'keep *_particleFlow_*_*',
         'keep *_acceptedMuons_*_*',
         'keep *_acceptedElectrons_*_*',
+        'keep *_selectedPatJetsPFlow_*_*',
+        'keep *_patMETsPFlow_*_*',
         'keep double_*PFlow*_rho_PAT',
-        'keep *_pfJets*_*_*',
+#        'keep *_pfJets*_*_*',
         'keep *_addPileupInfo_*_*',
 ]
 
