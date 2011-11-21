a9c11d43da384c208bf605c224d3b5d4c1c82860
tjkim <>
2011-11-21 10:59:38 +0000

    update for 2011 full dataset

--- TopAnalyzer/prod/MU/RD/pf2pat_muon_cfg.py
+++ TopAnalyzer/prod/MU/RD/pf2pat_muon_cfg.py
@@ -4,6 +4,7 @@ from KoPFA.TopAnalyzer.pf2pat_template_cfg import *
 from KoPFA.TopAnalyzer.eventContent_cff import *
 from KoPFA.TopAnalyzer.tools import *
 
+
 postfix = "PFlow"
 jetAlgo="AK5"
 usePF2PAT(process,runPF2PAT=True, jetAlgo=jetAlgo, runOnMC=False, postfix=postfix)
@@ -23,7 +24,7 @@ topProjection(process,postfix)
 # output
 process.out.outputCommands +=pf2patEventContent
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
 process.MessageLogger.cerr.FwkReport.reportEvery = 100
 
 process.patMuonFilter.minNumber = 2
@@ -32,6 +33,7 @@ process.patElectronFilter.minNumber = 0
 ## Source
 process.source = cms.Source("PoolSource",
                                 fileNames = cms.untracked.vstring(
+  '/store/data/Run2011B/DoubleMu/AOD/PromptReco-v1/000/180/252/085F0838-4305-E111-BDD0-003048CF94A8.root',
   '/store/data/Run2011A/DoubleMu/RECO/PromptReco-v1/000/161/312/9CD2FBA7-E557-E011-B433-001D09F29849.root',
   '/store/data/Run2011A/DoubleElectron/AOD/PromptReco-v1/000/161/312/0E8D9A70-E857-E011-B3DC-001D09F29321.root',
   '/store/data/Run2011A/MuEG/AOD/PromptReco-v1/000/161/312/04850A2C-F757-E011-9A74-003048F024DC.root'
