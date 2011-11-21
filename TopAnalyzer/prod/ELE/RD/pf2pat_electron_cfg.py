e4d391fae73b9b918bc7adb71bc050599707b6fc
tjkim <>
2011-11-21 10:56:55 +0000

    add gsf electron

--- TopAnalyzer/prod/ELE/RD/pf2pat_electron_cfg.py
+++ TopAnalyzer/prod/ELE/RD/pf2pat_electron_cfg.py
@@ -1,5 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
+from PhysicsTools.PatAlgos.patTemplate_cfg import *
 from KoPFA.TopAnalyzer.pf2pat_template_cfg import *
 from KoPFA.TopAnalyzer.eventContent_cff import *
 from KoPFA.TopAnalyzer.tools import *
@@ -23,7 +24,7 @@ topProjection(process,postfix)
 # output
 process.out.outputCommands +=pf2patEventContent
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
 process.MessageLogger.cerr.FwkReport.reportEvery = 100
 
 process.patMuonFilter.minNumber = 0 
@@ -32,14 +33,19 @@ process.patElectronFilter.minNumber = 2
 ## Source
 process.source = cms.Source("PoolSource",
                                 fileNames = cms.untracked.vstring(
-  '/store/data/Run2011A/DoubleMu/RECO/PromptReco-v1/000/161/312/9CD2FBA7-E557-E011-B433-001D09F29849.root',
+  '/store/data/Run2011B/DoubleElectron/AOD/PromptReco-v1/000/180/252/08D65FBF-0B05-E111-83AA-003048D2C0F0.root',
   '/store/data/Run2011A/DoubleElectron/AOD/PromptReco-v1/000/161/312/0E8D9A70-E857-E011-B3DC-001D09F29321.root',
   '/store/data/Run2011A/MuEG/AOD/PromptReco-v1/000/161/312/04850A2C-F757-E011-9A74-003048F024DC.root'
   )
 )
 
+# remove MC matching from the default sequence
+removeMCMatching(process, ['All'])
+
 #process.p += process.hltHighLevelElElRD
 process.p += process.nEventsHLT
+process.p += process.patDefaultSequence
+process.p += process.acceptedGsfElectrons
 process.p += getattr(process,"patPF2PATSequence"+postfix)
 process.p += process.looseLeptonSequence
 process.p += process.acceptedElectrons
@@ -48,3 +54,6 @@ process.p += process.patElectronFilter
 process.p += process.patMuonFilter
 process.p += process.nEventsFiltered
 
+process.out.outputCommands +=[
+        'keep *_acceptedGsfElectrons_*_*',
+]
