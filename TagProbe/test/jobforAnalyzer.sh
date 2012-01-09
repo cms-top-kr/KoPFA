1d03b6e6e51a06f0c7be22e764a0bb7170f764e5
tjkim <>
2012-01-09 16:27:35 +0000

    run all analyzer

---
+++ TagProbe/test/jobforAnalyzer.sh
@@ -0,0 +1,4 @@
+cmsRun PFElectron_TnP_Analyzer_Id_cfg.py & 
+cmsRun PFElectron_TnP_Analyzer_PFId_cfg.py & 
+cmsRun PFElectron_TnP_Analyzer_Trigger_cfg.py & 
+cmsRun PFElectron_TnP_Analyzer_Iso_cfg.py &
