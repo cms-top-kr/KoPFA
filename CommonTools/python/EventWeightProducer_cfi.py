0c0a5eaff73b187a741074c23c9fd3912fdd30c9
tjkim <>
2012-01-22 18:15:09 +0000

    update weight producer

--- CommonTools/python/EventWeightProducer_cfi.py
+++ CommonTools/python/EventWeightProducer_cfi.py
@@ -1,6 +1,8 @@
 import FWCore.ParameterSet.Config as cms
 
+from KoPFA.CommonTools.PileUpWeight_cff import *
+
 PUweight = cms.EDProducer("EventWeightProducer",
-    PUweight = cms.vdouble(0.24698,0.878399,1.62972,1.74644,1.48563,0.999695,0.740525,0.457214,0.268234,0.184943,0.127889,0.0274048,0.0959167,0.383667),
-    PUdefault = cms.double(0.0),
+  PileUpRD = PileUpRD2011,
+  PileUpMC = PoissonOneXDist,
 )
