798231cd1b39871bcfe2577abbcc4a0baaabc1a3
tjkim <>
2012-01-22 19:58:34 +0000

    make simple example to produce mc and data comparison plot

--- TagProbe/test/plotTnP.C
+++ TagProbe/test/plotTnP.C
@@ -10,82 +10,20 @@
 void plotTnP(){
   
   using namespace std;
-  TFile * f = new TFile("result2/plot_bgauss/Efficiency_ID.root");
-  //TFile * f_MC = new TFile("result/plot/Efficiency_ID.root");
-  TFile * f_MC = new TFile("result2/plot_bgauss/Efficiency_ID_mc.root");
-  //TFile * f_MCUnbias = new TFile("result/plot/Efficiency_ID_mcUnbias.root");
-
-  TFile * f_Iso = new TFile("result2/plot_bgauss/Efficiency_Iso.root");
-  //TFile * f_Iso_MC = new TFile("result/plot/Efficiency_Iso.root");
-  TFile * f_Iso_MC = new TFile("result2/plot_bgauss/Efficiency_Iso_mc.root");
-  //TFile * f_Iso_MCUnbias = new TFile("result/plot/Efficiency_Iso_mcUnbias.root");
+  TFile * f = new TFile("result/result_PFId_Data.root");
+  TFile * f_MC = new TFile("result/result_PFId_MC.root");
 
   // Data vs MC
-  cout << "Data vs MC" << endl;
-  TString l1 = "Data";
-  TString l2 = "MC";
+  string dir = "tnpPFId";
+  string flag = "PFId_abseta";
+  string var = "abseta_PLOT";
+  string method = "fit_eff_plots";
+  string canvas = dir+"/"+flag+"/"+method;
+  
+  plot2Eff(f,f_MC,"Data","MC", canvas, canvas, var, var, var,"PFId");
 
-  bool datavsmc = true;
-  bool datavstruth = false;
-  bool datavsunbiased = false;
-  bool h2d = true;
-  bool chargedependency = false;
-
-
-  if( chargedependency ){
-    plot2Eff(f,f_MC,l1,l2,"tnpTree/isIDMuon_charge/fit_eff_plots","tnpTree/isIDMuon_charge/fit_eff_plots","charge_PLOT","charge_PLOT","charge","isIDMuon");
-    plot2Eff(f_Iso,f_Iso_MC,l1,l2,"tnpTreeIso/isIso12_charge/fit_eff_plots","tnpTreeIso/isIso12_charge/fit_eff_plots","charge_PLOT","charge_PLOT","charge","isIso12");
-  }
-
-  if( datavsmc ){
-    //plot2Eff(f,f_MC,l1,l2,"tnpTree/isPFMuon_abseta/fit_eff_plots","tnpTree/isPFMuon_abseta/fit_eff_plots","abseta_PLOT","abseta_PLOT","abseta","isPFMuon");
-    //plot2Eff(f,f_MC,l1,l2,"tnpTree/isPFMuon_pt/fit_eff_plots","tnpTree/isPFMuon_pt/fit_eff_plots","pt_PLOT","pt_PLOT","pt","isPFMuon");
-
-    plot2Eff(f,f_MC,l1,l2,"tnpTree/isIDMuon_abseta/fit_eff_plots","tnpTree/isIDMuon_abseta/fit_eff_plots","abseta_PLOT","abseta_PLOT","abseta","isIDMuon");
-    plot2Eff(f,f_MC,l1,l2,"tnpTree/isIDMuon_pt/fit_eff_plots","tnpTree/isIDMuon_pt/fit_eff_plots","pt_PLOT","pt_PLOT","pt","isIDMuon");
-
-    plot2Eff(f_Iso,f_Iso_MC,l1,l2,"tnpTreeIso/isIso_abseta/fit_eff_plots","tnpTreeIso/isIso_abseta/fit_eff_plots","abseta_PLOT","abseta_PLOT","abseta","isIso");
-    plot2Eff(f_Iso,f_Iso_MC,l1,l2,"tnpTreeIso/isIso_pt/fit_eff_plots","tnpTreeIso/isIso_pt/fit_eff_plots","pt_PLOT","pt_PLOT","pt","isIso");
-
-    plot2Eff(f_Iso,f_Iso_MC,l1,l2,"tnpTreeIso/isIso12_abseta/fit_eff_plots","tnpTreeIso/isIso12_abseta/fit_eff_plots","abseta_PLOT","abseta_PLOT","abseta","isIso12");
-    plot2Eff(f_Iso,f_Iso_MC,l1,l2,"tnpTreeIso/isIso12_pt/fit_eff_plots","tnpTreeIso/isIso12_pt/fit_eff_plots","pt_PLOT","pt_PLOT","pt","isIso12");
-
-  }
-  // Data vs MC truth
-  if( datavstruth ){ 
-    cout << "Data vs MC truth" << endl;
-    plot2Eff(f,f_MC,"Data","MC truth","tnpTree/isPFMuon_abseta/fit_eff_plots","tnpTree/isPFMuon_abseta_mcTrue/cnt_eff_plots","abseta_PLOT","abseta_PLOT_mcTrue_true","abseta_mcTrue","isPFMuon");
-    plot2Eff(f,f_MC,"Data","MC truth","tnpTree/isPFMuon_pt/fit_eff_plots","tnpTree/isPFMuon_pt_mcTrue/cnt_eff_plots","pt_PLOT","pt_PLOT_mcTrue_true","pt_mcTrue","isPFMuon");
-
-    plot2Eff(f,f_MC,"Data","MC truth","tnpTree/isIDMuon_abseta/fit_eff_plots","tnpTree/isIDMuon_abseta_mcTrue/cnt_eff_plots","abseta_PLOT","abseta_PLOT_mcTrue_true","abseta_mcTrue","isIDMuon");
-    plot2Eff(f,f_MC,"Data","MC truth","tnpTree/isIDMuon_pt/fit_eff_plots","tnpTree/isIDMuon_pt_mcTrue/cnt_eff_plots","pt_PLOT","pt_PLOT_mcTrue_true","pt_mcTrue","isIDMuon");
-
-    plot2Eff(f_Iso,f_Iso_MC,"Data","MC truth","tnpTreeIso/isIso_abseta/fit_eff_plots","tnpTreeIso/isIso_abseta_mcTrue/cnt_eff_plots","abseta_PLOT","abseta_PLOT_mcTrue_true","abseta_mcTrue","Iso");
-    plot2Eff(f_Iso,f_Iso_MC,"Data","MC truth","tnpTreeIso/isIso_pt/fit_eff_plots","tnpTreeIso/isIso_pt_mcTrue/cnt_eff_plots","pt_PLOT","pt_PLOT_mcTrue_true","pt_mcTrue","Iso");
-  }
-  // Data vs MC unbiased truth
-  if( datavsunbiased =){
-    cout << "Data vs MC unbiased truth" << endl;
-    plot2Eff(f,f_MCUnbias,"Data","MC(unbiased) truth ","tnpTree/isPFMuon_abseta/fit_eff_plots","tnpTree/isPFMuon_abseta_mcTrue/cnt_eff_plots","abseta_PLOT","abseta_PLOT_mcTrue_true","abseta_mcTrue_Unbiased","isPFMuon");
-    plot2Eff(f,f_MCUnbias,"Data","MC(unbiased) truth","tnpTree/isPFMuon_pt/fit_eff_plots","tnpTree/isPFMuon_pt_mcTrue/cnt_eff_plots","pt_PLOT","pt_PLOT_mcTrue_true","pt_mcTrue_Unbiased","isPFMuon");
-
-    plot2Eff(f,f_MCUnbias,"Data","MC(unbiased) truth","tnpTree/isIDMuon_abseta/fit_eff_plots","tnpTree/isIDMuon_abseta_mcTrue/cnt_eff_plots","abseta_PLOT","abseta_PLOT_mcTrue_true","abseta_mcTrue_Unbiased","isIDMuon");
-    plot2Eff(f,f_MCUnbias,"Data","MC(unbiased) truth","tnpTree/isIDMuon_pt/fit_eff_plots","tnpTree/isIDMuon_pt_mcTrue/cnt_eff_plots","pt_PLOT","pt_PLOT_mcTrue_true","pt_mcTrue_Unbiased","isIDMuon");
-
-    plot2Eff(f_Iso,f_Iso_MCUnbias,"Data","MC(unbiased) truth","tnpTreeIso/isIso_abseta/fit_eff_plots","tnpTreeIso/isIso_abseta_mcTrue/cnt_eff_plots","abseta_PLOT","abseta_PLOT_mcTrue_true","abseta_mcTrue_Unbiased","Iso");
-    plot2Eff(f_Iso,f_Iso_MCUnbias,"Data","MC(unbiased) truth","tnpTreeIso/isIso_pt/fit_eff_plots","tnpTreeIso/isIso_pt_mcTrue/cnt_eff_plots","pt_PLOT","pt_PLOT_mcTrue_true","pt_mcTrue_Unbiased","Iso");
-
-  }
-
-  if( h2d == true){
-    //2D histograms
-    //plot2DEff(f, f_MC, "tnpTree/isPFMuon_pt_abseta/fit_eff_plots","pt_abseta_PLOT","isPFMuon");
-    plot2DEff(f, f_MC, "tnpTree/isIDMuon_pt_abseta/fit_eff_plots","pt_abseta_PLOT","isIDMuon");
-    plot2DEff(f_Iso, f_Iso_MC, "tnpTreeIso/isIso_pt_abseta/fit_eff_plots","pt_abseta_PLOT","isIso");
-    plot2DEff(f_Iso, f_Iso_MC, "tnpTreeIso/isIso12_pt_abseta/fit_eff_plots","pt_abseta_PLOT","isIso12");
-    
-  }
-
+  // 2D
+  //plot2DEff(f, f_MC, "tnpTree/isIDMuon_pt_abseta/fit_eff_plots","pt_abseta_PLOT","isIDMuon");
   
 }
 
