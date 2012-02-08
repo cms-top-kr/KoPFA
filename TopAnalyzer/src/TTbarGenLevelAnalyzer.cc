dde12e3feef184bed5c2c0164d1ec95f562ba74b
jhgoh <>
2012-02-08 18:49:34 +0000

    Restored TTbarGenEvent

--- TopAnalyzer/src/TTbarGenLevelAnalyzer.cc
+++ TopAnalyzer/src/TTbarGenLevelAnalyzer.cc
@@ -35,29 +35,15 @@ public:
   virtual void analyze(const edm::Event& event, const edm::EventSetup& eventSetup);
 
 private:
-//  const reco::GenParticle* getDaughter(const reco::GenParticle* genParticle, const int pdgId) const;
-//  bool isBJet(const reco::GenJet* j) const;
-//  bool isJetFromDecay(const reco::GenParticle* p, const reco::GenJet* j) const;
-//  template<typename T>
-//  void setLeadingParticle(T* p, T*& p1, T*& p2) const;
 
 private:
   edm::InputTag genParticleLabel_;
   edm::InputTag genJetLabel_;
   edm::InputTag genMETLabel_;
 
-/*
-  double electronMaxEta_, electronMinPt_;
-  double muonMaxEta_, muonMinPt_;
-  double jetMaxEta_, jetMinPt_;
-  double metMinPt_;
-
-  TH1F* hGenTTbarM_;
-*/
   TTree* tree_;
   Ko::TTbarGenEvent* ttbarGenEvent_;
 
-//  double m_tt_, m_llbbnn_, m_lljjmet_, m_llbjbjmet_;
 };
 
 TTbarGenLevelAnalyzer::TTbarGenLevelAnalyzer(const edm::ParameterSet& pset)
@@ -73,23 +59,9 @@ TTbarGenLevelAnalyzer::TTbarGenLevelAnalyzer(const edm::ParameterSet& pset)
 void TTbarGenLevelAnalyzer::beginJob()
 {
   edm::Service<TFileService> fs;
-/*
-  const int ttbarMassNbins = 14;
-  const double ttbarMassBins[ttbarMassNbins] = {
-    0, 350, 400, 450, 500, 550, 600, 650, 700, 750, 800, 900, 1000, 1400
-  };
-
-  hGenTTbarM_ = fs->make<TH1F>("hGenTTbarM", "Generator level t#bar{t} mass;M(t#bar{t}) [GeV/c^{2}]", ttbarMassNbins-1, ttbarMassBins);
-*/
 
   tree_ = fs->make<TTree>("tree", "Tree for ttbar dilepton generator level study");
   tree_->Branch("ttbarGen", "Ko::TTbarGenEvent", &ttbarGenEvent_);
-/*
-  tree_->Branch("m_tt", &m_tt_, "m_tt/D");
-  tree_->Branch("m_llbbnn", &m_llbbnn_, "m_llbbnn/D");
-  tree_->Branch("m_lljjmet", &m_lljjmet_, "m_lljjmet/D");
-  tree_->Branch("m_llbjbjmet", &m_llbjbjmet_, "m_llbjbjmet/D");
-*/
 }
 
 void TTbarGenLevelAnalyzer::analyze(const edm::Event& event, const edm::EventSetup& eventSetup)
