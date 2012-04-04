88ee3692d7a1ac86653dc243930cf1fa8cc92f94
tjkim <>
2012-04-04 09:12:25 +0000

    add histogram with status 2 bquark

--- TopAnalyzer/src/TopDecayGenHisto.cc
+++ TopAnalyzer/src/TopDecayGenHisto.cc
@@ -43,10 +43,13 @@ private:
   TH1F* hMtt_Full;
   TH1F* hMtt_LPJ;
   TH1F* hMtt_LPJ_Pton;
+  TH1F* hMtt_LPJ_Pton_Statu2;
   TH1F* hMtt_DIL;
   TH1F* hMtt_DIL_Pton;
+  TH1F* hMtt_DIL_Pton_Statu2;
 
   TH2F* hMtt_bquarkPt;
+  TH2F* hMtt_bquarkStatu2Pt;
   TH2F* hMtt_leptonPt;
   TH2F* hMtt_bquarkEta;
   TH2F* hMtt_leptonEta;
@@ -72,9 +75,12 @@ TopDecayGenHisto::TopDecayGenHisto(const edm::ParameterSet& pset)
   hMtt_Full = fs->make<TH1F>( "hMtt_Full"  , "M_{tt}", 2500,  0., 2500. );
   hMtt_LPJ = fs->make<TH1F>( "hMtt_LPJ"  , "M_{tt}", 2500,  0., 2500. );
   hMtt_LPJ_Pton = fs->make<TH1F>( "hMtt_LPJ_Pton"  , "M_{tt}", 2500,  0., 2500. );
+  hMtt_LPJ_Pton_Statu2 = fs->make<TH1F>( "hMtt_LPJ_Pton_Statu2"  , "M_{tt}", 2500,  0., 2500. );
   hMtt_DIL = fs->make<TH1F>( "hMtt_DIL"  , "M_{tt}", 2500,  0., 2500. );
   hMtt_DIL_Pton = fs->make<TH1F>( "hMtt_DIL_Pton"  , "M_{tt}", 2500,  0., 2500. );
+  hMtt_DIL_Pton_Statu2 = fs->make<TH1F>( "hMtt_DIL_Pton_Statu2"  , "M_{tt}", 2500,  0., 2500. );
   hMtt_bquarkPt = fs->make<TH2F>( "hMtt_bquarkPt", "M_{tt} vs bquarkPt", 500, 0, 500, 2500, 0, 2500);
+  hMtt_bquarkStatu2Pt = fs->make<TH2F>( "hMtt_bquarkStatu2Pt", "M_{tt} vs bquarkPt", 500, 0, 500, 2500, 0, 2500);
   hMtt_leptonPt = fs->make<TH2F>( "hMtt_leptonPt", "M_{tt} vs leptonPt", 500, 0, 500, 2500, 0, 2500);
   hMtt_bquarkEta = fs->make<TH2F>( "hMtt_bquarkEta", "M_{tt} vs bquarkEta", 30, 0, 3, 2500, 0, 2500);
   hMtt_leptonEta = fs->make<TH2F>( "hMtt_leptonEta", "M_{tt} vs leptonEta", 30, 0, 3, 2500, 0, 2500);
@@ -94,8 +100,11 @@ bool TopDecayGenHisto::filter(edm::Event& iEvent, const edm::EventSetup& eventSe
 
   std::vector<double> leptonPt(2,static_cast<double>(-1));
   std::vector<double> bquarkPt(2,static_cast<double>(-1));
+  std::vector<double> bquarkStatu2Pt(2,static_cast<double>(-1));
   std::vector<double> leptonEta(2,static_cast<double>(-1));
   std::vector<double> bquarkEta(2,static_cast<double>(-1));
+  std::vector<double> bquarkStatu2Eta(2,static_cast<double>(-1));
+  
 
   using namespace std;
   using namespace edm;
@@ -131,6 +140,17 @@ bool TopDecayGenHisto::filter(edm::Event& iEvent, const edm::EventSetup& eventSe
       if ( abs(daugh->pdgId()) == 5 ) {
          bquarkPt[ntop] = daugh->pt();
          bquarkEta[ntop] = fabs( daugh->eta() );
+         unsigned int nbDaughters = daugh->numberOfDaughters();
+         for ( unsigned ibDaughter=0; ibDaughter<nbDaughters; ++ibDaughter ) {
+           const reco::Candidate* decay = daugh->daughter(ibDaughter);
+           int decayId = abs(decay->pdgId());
+           int decayStatus = decay->status();
+           if( decayId == 5 ){
+             assert( decayStatus == 2);
+              bquarkStatu2Pt[ntop] = decay->pt();
+              bquarkStatu2Eta[ntop] = fabs( decay->eta() );
+           }
+         }
       }
       if ( abs(daugh->pdgId()) != 24 ) continue; 
       unsigned int nWDaughters = daugh->numberOfDaughters();
@@ -245,15 +265,23 @@ bool TopDecayGenHisto::filter(edm::Event& iEvent, const edm::EventSetup& eventSe
   hMtt_Full->Fill(Mtt);
   bool visibleDIL = leptonPt[0] > 20 && leptonPt[1] > 20 && leptonEta[0] < 2.4 && leptonEta[1] < 2.4 && bquarkPt[0] > 30 && bquarkPt[1] > 30 && bquarkEta[1] < 2.4 && bquarkEta[2] < 2.4;
   bool visibleLPJ = ( ( leptonPt[0] > 30 && leptonEta[0] < 2.1 && leptonPt[1] < 0 ) || ( leptonPt[0] < 0 && leptonPt[1] > 30 && leptonEta[1] < 2.1 ) ) && bquarkPt[0] > 30 && bquarkPt[1] > 30 && bquarkEta[0] < 2.4 && bquarkEta[1] < 2.4;
+  bool visibleDILStatu2 = leptonPt[0] > 20 && leptonPt[1] > 20 && leptonEta[0] < 2.4 && leptonEta[1] < 2.4 && bquarkStatu2Pt[0] > 30 && bquarkStatu2Pt[1] > 30 && bquarkStatu2Eta[1] < 2.4 && bquarkStatu2Eta[2] < 2.4;
+  bool visibleLPJStatu2 = ( ( leptonPt[0] > 30 && leptonEta[0] < 2.1 && leptonPt[1] < 0 ) || ( leptonPt[0] < 0 && leptonPt[1] > 30 && leptonEta[1] < 2.1 ) ) && bquarkStatu2Pt[0] > 30 && bquarkStatu2Pt[1] > 30 && bquarkStatu2Eta[0] < 2.4 && bquarkStatu2Eta[1] < 2.4;
+  
 
   if( ( hadronic[0] == true &&  hadronic[1] == false ) || ( hadronic[0] == false &&  hadronic[1] == true) ) hMtt_LPJ->Fill(Mtt);
   if( leptonPt[0] > 0  && leptonPt[1] > 0) hMtt_DIL->Fill(Mtt);
   if( visibleLPJ ) hMtt_LPJ_Pton->Fill(Mtt);
   if( visibleDIL ) hMtt_DIL_Pton->Fill(Mtt);
+  if( visibleLPJStatu2 ) hMtt_LPJ_Pton_Statu2->Fill(Mtt);
+  if( visibleDILStatu2 ) hMtt_DIL_Pton_Statu2->Fill(Mtt);
+
   if(leptonPt[0] > 0) hMtt_leptonPt->Fill( leptonPt[0], Mtt);
   if(leptonPt[1] > 0) hMtt_leptonPt->Fill( leptonPt[1], Mtt);
   if(bquarkPt[0] > 0) hMtt_bquarkPt->Fill( bquarkPt[0], Mtt);
   if(bquarkPt[1] > 0) hMtt_bquarkPt->Fill( bquarkPt[1], Mtt);
+  if(bquarkStatu2Pt[0] > 0) hMtt_bquarkStatu2Pt->Fill( bquarkStatu2Pt[0], Mtt);
+  if(bquarkStatu2Pt[1] > 0) hMtt_bquarkStatu2Pt->Fill( bquarkStatu2Pt[1], Mtt);
   if(leptonEta[0] > 0) hMtt_leptonEta->Fill( leptonEta[0], Mtt);
   if(leptonEta[1] > 0) hMtt_leptonEta->Fill( leptonEta[1], Mtt);
   if(bquarkEta[0] > 0) hMtt_bquarkEta->Fill( bquarkEta[0], Mtt);
