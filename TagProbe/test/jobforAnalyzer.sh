ecac7c10b5b6253a4d88c9d441c765f2a8452478
tjkim <>
2012-05-12 14:23:18 +0000

    update script

--- TagProbe/test/jobforAnalyzer.sh
+++ TagProbe/test/jobforAnalyzer.sh
@@ -1,4 +1,8 @@
-cmsRun PFElectron_TnP_Analyzer_Id_cfg.py & 
-cmsRun PFElectron_TnP_Analyzer_PFId_cfg.py & 
-cmsRun PFElectron_TnP_Analyzer_Trigger_cfg.py & 
+#Extra study
+#cmsRun PFElectron_TnP_Analyzer_Id_cfg.py & 
+#cmsRun PFElectron_TnP_Analyzer_Trigger_cfg.py & 
+
+#For TOP-11-013
+cmsRun PFElectron_TnP_Analyzer_TightId_cfg.py &
+cmsRun PFElectron_TnP_Analyzer_PFId_cfg.py &
 cmsRun PFElectron_TnP_Analyzer_Iso_cfg.py &
