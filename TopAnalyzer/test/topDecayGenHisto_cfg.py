c3a1c0dbbac5deaf3ce6d8cd052f2bc24b2ba3a6
tjkim <>
2012-05-21 14:23:20 +0000

    weight for MC@NLO

--- TopAnalyzer/test/topDecayGenHisto_cfg.py
+++ TopAnalyzer/test/topDecayGenHisto_cfg.py
@@ -27,12 +27,16 @@ process.printTree = cms.EDAnalyzer("ParticleListDrawer",
 process.source = cms.Source("PoolSource",
   fileNames = cms.untracked.vstring(
     #pickRelValInputFiles() # <-- picks automatically RelVal input files for the current release
-    'rfio:/castor/cern.ch/user/t/tjkim/DIMUSKIM/MC/Fall11/TTJets/patTuple_skim_95_0_860.root'
+    #'rfio:/castor/cern.ch/user/t/tjkim/DIMUSKIM/MC/Fall11/TTJets/patTuple_skim_95_0_860.root'
+    #'file:/afs/cern.ch/work/c/cbern/public/CMGOutput/patTuple_PF2PAT_1142.root'
+    #'/store/cmst3/group/cmgtools/CMG/TTJets_TuneZ2_7TeV-madgraph-tauola/Fall11-PU_S6_START42_V14B-v1/AODSIM/V4/PFAOD_0.root'
+    'rfio:/castor/cern.ch/user/t/tjkim/MC/Fall11/test/Fall11_TT_TuneZ2_7TeV-mcatnlo_0.root'
   )
 )
 
 process.topDecayGenHisto = cms.EDFilter('TopDecayGenHisto',
   applyFilter =  cms.untracked.bool(True),
+  applyWeight =  cms.untracked.bool(True),
   semiLeptonic = cms.bool(False),
   semiLeptonicMuon = cms.bool(False),
   semiLeptonicElectron = cms.bool(False),
