39e80faae3a7798cddb67ab35f4f51825b278131
tjkim <>
2012-05-24 23:29:22 +0000

    event filter for 52X

---
+++ CommonTools/python/eventFilter_cfi.py
@@ -0,0 +1,16 @@
+import FWCore.ParameterSet.Config as cms
+
+from HLTrigger.HLTfilters.hltHighLevel_cfi import *
+
+#EventFilter = cms.EDFilter("EventFilter",
+#    filters = cms.vstring("primaryVertexFilterPath",
+#                          "HBHENoiseFilterPath",
+#                          "noscrapingFilterPath",
+#                         )
+#)
+
+EventFilter = hltHighLevel.clone()
+EventFilter.TriggerResultsTag = cms.InputTag("TriggerResults","","PAT")
+EventFilter.HLTPaths = cms.vstring('primaryVertexFilterPath','HBHENoiseFilterPath','noscrapingFilterPath')
+#EventFilter.HLTPaths = cms.vstring('primaryVertexFilterPath','HBHENoiseFilterPath','noscrapingFilterPath','totalKinematicsFilterPath')
+#EventFilter.throw = cms.bool(False)
