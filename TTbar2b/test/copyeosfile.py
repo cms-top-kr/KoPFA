426c3ab9e6f9a84aab59b2566a5de950d7029006
tjkim <>
2012-09-27 12:50:03 +0000

    copy eos file

---
+++ TTbar2b/test/copyeosfile.py
@@ -0,0 +1,1 @@
+xrdcp root://eoscms//eos/cms/store/cmst3/group/cmgtools/CMG/TTH_HToBB_M-135_8TeV-pythia6/Summer12-PU_S7_START52_V9-v1/AODSIM/VX/PFAOD*.root root://eoscms//eos/cms/store/cmst3/group/cmgtools/CMG/TTH_HToBB_M-135_8TeV-pythia6/Summer12-PU_S7_START52_V9-v1/AODSIM/V5/
