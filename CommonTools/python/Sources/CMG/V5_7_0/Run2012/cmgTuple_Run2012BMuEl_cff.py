83c7689b0405a8092a78c6b5bd63fda565d217df
shsong79 <>
2012-10-08 06:57:41 +0000

    for V5_7_0

--- CommonTools/python/Sources/CMG/V5_7_0/Run2012/cmgTuple_Run2012BMuEl_cff.py
+++ CommonTools/python/Sources/CMG/V5_7_0/Run2012/cmgTuple_Run2012BMuEl_cff.py
@@ -3,7 +3,7 @@ import FWCore.ParameterSet.Config as cms
 source = cms.Source(
 	"PoolSource",
 	noEventSort = cms.untracked.bool(True),
-	duplicateCheckMode = cms.untracked.string(noDuplicateCheck),
+	duplicateCheckMode = cms.untracked.string("noDuplicateCheck"),
 	fileNames = cms.untracked.vstring()
 )
 source.fileNames.extend([
