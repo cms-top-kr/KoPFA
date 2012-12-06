b4e857c2249d05e360fe84aee0acd9f990a6baed
tjkim <>
2012-12-06 09:54:22 +0000

    update

--- TTbar2b/test/syn.C
+++ TTbar2b/test/syn.C
@@ -1,8 +1,8 @@
 void syn(){
 
-  TFile * f_mm = new TFile("/afs/cern.ch/work/t/tjkim/public/store/top/paper/v1/vallot_Run2012MuMu.root");
-  TFile * f_ee = new TFile("/afs/cern.ch/work/t/tjkim/public/store/top/paper/v1/vallot_Run2012ElEl.root");
-  TFile * f_em = new TFile("/afs/cern.ch/work/t/tjkim/public/store/top/paper/v1/vallot_Run2012MuEl.root");
+  TFile * f_mm = new TFile("/afs/cern.ch/work/t/tjkim/public/store/top/paper/v2/vallot_Run2012MuMu.root");
+  TFile * f_ee = new TFile("/afs/cern.ch/work/t/tjkim/public/store/top/paper/v2/vallot_Run2012ElEl.root");
+  TFile * f_em = new TFile("/afs/cern.ch/work/t/tjkim/public/store/top/paper/v2/vallot_Run2012MuEl.root");
 
   TTree * t_mm = (TTree *) f_mm->Get("MuMu/tree");
   TTree * t_ee = (TTree *) f_ee->Get("ElEl/tree");
@@ -12,8 +12,8 @@ void syn(){
   TH1F * h_ee = (TH1F *) f_ee->Get("ElEl/EventSummary");
   TH1F * h_em = (TH1F *) f_em->Get("MuEl/EventSummary");
 
-  TCut run = "RUN >= 190456 && RUN <=196531";
-  //TCut run = "";
+  //TCut run = "RUN >= 190456 && RUN <=196531";
+  TCut run = "";
  
   TCut step1 = "ZMass > 20 && isIso > 0 && PairSign < 0" + run;
   TCut step2 = "abs(ZMass-91.2) > 15";
@@ -22,19 +22,19 @@ void syn(){
   TCut step5 = "nbjets30_CSVL >= 1";
 
   cout << "step0a" << endl;
-  cout << "MuMu = " << h_mm->GetBinContent(1) << endl;
-  cout << "ElEl = " << h_ee->GetBinContent(1) << endl;
-  cout << "MuEl = " << h_em->GetBinContent(1) << endl;
+  cout << "MuMu = " << (int) h_mm->GetBinContent(1) << endl;
+  cout << "ElEl = " << (int) h_ee->GetBinContent(1) << endl;
+  cout << "MuEl = " << (int) h_em->GetBinContent(1) << endl;
 
   cout << "step0b" << endl;
-  cout << "MuMu = " << h_mm->GetBinContent(4) << endl;
-  cout << "ElEl = " << h_ee->GetBinContent(4) << endl;
-  cout << "MuEl = " << h_em->GetBinContent(4) << endl;
+  cout << "MuMu = " << (int) h_mm->GetBinContent(3) << endl;
+  cout << "ElEl = " << (int) h_ee->GetBinContent(3) << endl;
+  cout << "MuEl = " << (int) h_em->GetBinContent(3) << endl;
 
   cout << "step0c" << endl;
-  cout << "MuMu = " << h_mm->GetBinContent(3) << endl;
-  cout << "ElEl = " << h_ee->GetBinContent(3) << endl;
-  cout << "MuEl = " << h_em->GetBinContent(3) << endl;
+  cout << "MuMu = " << (int) h_mm->GetBinContent(4) << endl;
+  cout << "ElEl = " << (int) h_ee->GetBinContent(4) << endl;
+  cout << "MuEl = " << (int) h_em->GetBinContent(4) << endl;
 
   cout << "step1" << endl;
   cout << "MuMu = " << t_mm->GetEntries(step1) << endl;
