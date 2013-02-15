a798a3c506433d0f782150e498408e25a6e41f8a
youngjo <>
2013-02-15 16:22:04 +0000

    added it for b Discriminator CSV

--- TTbar2b/test/bVariation.C
+++ TTbar2b/test/bVariation.C
@@ -1,11 +1,22 @@
-void bVariation(){
+void bVariation(int ii, int jj){
 
   gROOT->ProcessLine(".L tdrstyle.C");
   defaultStyle();
 
-  TH1* h = bHist("TTBB_01Sep2012_CSVT_addb");
-  TH1* h_up = bHist("TTBB_01Sep2012_CSVTup_addb");
-  TH1* h_dw = bHist("TTBB_01Sep2012_CSVTdw_addb");
+  TString samples[2] = {"hMC_TTbarll","hMCSig_TTbarbb"};
+  TString histNames[2] = {"jet30bDiscriminator1b","nbJet30_CSVT"};
+  TString xtitles[2] = {"CSV distriminator","b-Jet Multiplicity (CSVT)"};
+
+  TString sample = samples[ii];
+  TString histName = histNames[jj];
+  TString xtitle = xtitles[jj];  
+
+  TString histN =Form("Step_5/%s_Step_5_%s",sample.Data(),histName.Data());
+
+
+  TH1* h = bHist("csvt/TTBB_08Feb2013_CSVTv2puweight_bweight30CSVT", histN);
+  TH1* h_up = bHist("csvt2/TTBB_08Feb2013_CSVTv2puweight_bweight30CSVTuplight", histN);
+  TH1* h_dw = bHist("csvt2/TTBB_08Feb2013_CSVTv2puweight_bweight30CSVTdwlight", histN);
 
   double s = h->Integral();
   double s_up = h_up->Integral();
@@ -16,18 +27,28 @@ void bVariation(){
   h_dw->Scale(1.0/s_dw);
 
   TCanvas * c = new TCanvas("c","c",1);
-  c->SetLogy();
+//  c->SetLogy();
   h->SetTitle("");
-  h->GetXaxis()->SetTitle("b-Jet Multiplicity");
+  h->GetXaxis()->SetTitle(xtitle.Data());
   h->GetYaxis()->SetTitle("Normalized Entries");
   h_up->SetLineColor(4);
   h_dw->SetLineColor(2);
+
+    double ymax = h->GetMaximum(); 
+    if(ymax< h_up->GetMaximum()) ymax=h_up->GetMaximum();
+    if(ymax< h_dw->GetMaximum()) ymax=h_dw->GetMaximum();
+    
+    ymax=ymax*1.5; // for linear
+    //ymax=ymax*100; // for logy
+
+    h->SetMaximum(ymax);
+
   h->Draw();
   h_up->Draw("same");
   h_dw->Draw("same");
 
 
-  for(int i=1; i <= 5 ;i++){
+  for(int i=1; i <= h->GetNbinsX() ;i++){
     double central = h->GetBinContent(i);
     double up = h_up->GetBinContent(i);
     double dw = h_dw->GetBinContent(i);
@@ -37,9 +58,9 @@ void bVariation(){
   }
 
   TLegend *l = new TLegend(0.78,0.76,0.89,0.88);
-  l->AddEntry(h_up,"b SF up","L");
-  l->AddEntry(h,   "b SF","L");
-  l->AddEntry(h_dw,"b SF dw","L");
+  l->AddEntry(h_up,"l SF up","L");
+  l->AddEntry(h,   "l SF","L");
+  l->AddEntry(h_dw,"l SF dw","L");
 
   l->SetTextSize(0.04);
   l->SetFillColor(0);
@@ -47,20 +68,21 @@ void bVariation(){
 
   l->Draw();
 
-  c->Print("bVariation_CSVT.eps");
+  c->Print(Form("bVariation_CSVT_%s_%s.eps",sample.Data(), histName.Data()));
+
 }
 
-TH1* bHist(const TString & path){
+TH1* bHist(const TString & path, TString & name){
 
   TFile * f_MuMu = new TFile(path+"/MuMu/MuMu.root");
   TFile * f_MuEl = new TFile(path+"/MuEl/MuEl.root");
   TFile * f_ElEl = new TFile(path+"/ElEl/ElEl.root");
 
-  TH1* h_ttbb_MuMu = (TH1*) f_MuMu->Get("Step_4/hMCSig_TTbarbb_Step_4_nbJet30_CSVT");
-  TH1* h_ttbb_MuEl = (TH1*) f_MuEl->Get("Step_4/hMCSig_TTbarbb_Step_4_nbJet30_CSVT");
-  TH1* h_ttbb_ElEl = (TH1*) f_ElEl->Get("Step_4/hMCSig_TTbarbb_Step_4_nbJet30_CSVT");
+  TH1* h_ttbb_MuMu = (TH1*) f_MuMu->Get(name.Data());
+  TH1* h_ttbb_MuEl = (TH1*) f_MuEl->Get(name.Data());
+  TH1* h_ttbb_ElEl = (TH1*) f_ElEl->Get(name.Data());
 
-  TH1* h_ttbb = new TH1F("h_ttbb","h_ttbb",5,0,5);
+  TH1* h_ttbb = new TH1F("h_ttbb","h_ttbb",h_ttbb_MuMu->GetNbinsX(),h_ttbb_MuMu->GetXaxis()->GetXmin() ,h_ttbb_MuMu->GetXaxis()->GetXmax() );
 
   h_ttbb->Add(h_ttbb_MuMu);
   h_ttbb->Add(h_ttbb_MuEl);
