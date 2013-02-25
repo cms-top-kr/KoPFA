71c209cf2e49b7c990612d8d33485d42446c967c
taeyeon <>
2013-02-25 20:05:09 +0000

    for Muon POG

--- IsoAnalyzer/python/cmgMuonIsoAnalyzer_cfi.py
+++ IsoAnalyzer/python/cmgMuonIsoAnalyzer_cfi.py
@@ -1,18 +1,40 @@
 import FWCore.ParameterSet.Config as cms
 
-MuonIsoAnalysis = cms.EDProducer("cmgMuonIsoAnalyzer",
+MuonAnalysis = cms.EDProducer("cmgMuonIsoAnalyzer",
   muonLabel = cms.InputTag('cmgMuonSel'),
+  electronLabel = cms.InputTag('cmgElectronSel'),
   beamSpotLabel = cms.InputTag('offlineBeamSpot'),
-  metLabel = cms.InputTag('cmgPFMET'),
-  jetLabel = cms.InputTag('cmgPFJetSelCHS'),
+  metLabel =  cms.InputTag('JetEnergyScale','MET'),
+  jetLabel =  cms.InputTag('JetEnergyScale','Jets'),
   genParticlesLabel = cms.InputTag('genParticlesPruned'),
   vertexLabel = cms.untracked.InputTag('offlinePrimaryVertices'),
   useZMassWindow = cms.untracked.bool(False),
-  applyPFId = cms.untracked.bool(False),
+  applyPFId = cms.untracked.bool(True),
   numberOfLeptons    = cms.untracked.uint32(2), 
+  numberOfJets    = cms.untracked.uint32(2),
   qcdMC = cms.untracked.bool(False), 
-#  qcdMC = cms.untracked.bool(True),  #for QCD MC
+  #qcdMC = cms.untracked.bool(True),  #for QCD MC
   useEventCounter = cms.bool( True ),
+  bTagSets = cms.untracked.VPSet(
+    cms.untracked.PSet(
+        algo = cms.untracked.string("combinedSecondaryVertexBJetTags"),
+        name = cms.untracked.string("CSVL"),
+        cutValue = cms.untracked.double(0.244),
+        isCutMin = cms.untracked.bool(True),
+    ),
+    cms.untracked.PSet(
+        algo = cms.untracked.string("combinedSecondaryVertexBJetTags"),
+        name = cms.untracked.string("CSVM"),
+        cutValue = cms.untracked.double(0.679),
+        isCutMin = cms.untracked.bool(True),
+    ),
+    cms.untracked.PSet(
+        algo = cms.untracked.string("combinedSecondaryVertexBJetTags"),
+        name = cms.untracked.string("CSVT"),
+        cutValue = cms.untracked.double(0.898),
+        isCutMin = cms.untracked.bool(True),
+    ),
+  ),
   filters = cms.untracked.vstring(
       'prePathCounter',
       'postPathCounter',
