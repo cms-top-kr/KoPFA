4d15cc59a1d1f3aa0b439824bb49bb7c2d889db1
taeyeon <>
2013-02-25 20:06:45 +0000

    muon POG

--- IsoAnalyzer/test/cmgMuonIso_cfg.py
+++ IsoAnalyzer/test/cmgMuonIso_cfg.py
@@ -11,11 +11,13 @@ process.source = cms.Source("PoolSource",
 )
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
-process.MessageLogger.cerr.FwkReport.reportEvery = 10000
+process.MessageLogger.cerr.FwkReport.reportEvery = 1000
 
 process.load("KoPFA.CMGAnalyzer.topAnalysis_cff")
 process.load("KoPFA.CommonTools.eventFilter_cfi")
 process.load("KoPFA.IsoAnalyzer.cmgMuonIsoAnalyzer_cfi")
+#process.load("KoPFA.IsoAnalyzer.cmgEMuonIsoAnalyzer_cfi")
+process.load("KoPFA.CMGAnalyzer.ZFilter_cfi")
 
 #Data
 process.load("KoPFA.CommonTools.Sources.CMG.V5_10_0.Run2012.cmgTuple_Run2012AMuMu_cff")
@@ -28,17 +30,42 @@ process.load("KoPFA.CommonTools.Sources.CMG.V5_10_0.Run2012.cmgTuple_Run2012AMuM
 #process.load("KoPFA.CommonTools.Sources.CMG.V5_10_0.Summer12.cmgTuple_ZJets_cff")
 #process.load("KoPFA.CommonTools.Sources.CMG.V5_10_0.Summer12.patTuple_QCD_cff")
 
+runOnMC = True
+runOn2012 = True
+
+from CMGTools.Common.Tools.applyJSON_cff import applyJSON
+json = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions12/8TeV/Prompt/Cert_190456-203002_8TeV_PromptReco_Collisions12_JSON_v2.txt'
+if not runOnMC:
+    applyJSON(process, json )
+
+process.nEventsFilter = cms.EDProducer("EventCountProducer")
+
+process.load("KoPFA.CMGAnalyzer.TtFullLepKinSolutionProducer_cfi")
+process.load("KoPFA.CMGAnalyzer.TtFullLepMaosSolutionProducer_cfi")
+
+from KoPFA.CMGAnalyzer.JetEnergyScale_cfi import *
+JetEnergyScale.electronLabel = cms.InputTag("cmgElectronSel")
+JetEnergyScale.muonLabel = cms.InputTag("cmgMuonSel")
+
 nEventsPatHLT = cms.EDProducer("EventCountProducer")
 process.nEventsFilter = cms.EDProducer("EventCountProducer")
 
-process.p = cms.Path(
+process.p5 = cms.Path(
     process.hltHighLevelMuMu*
+    #process.hltHighLevelMuEl*
     process.nEventsPatHLT*
     process.EventFilter*
     process.nEventsFilter*
-    process.MuonIsoAnalysis
+    JetEnergyScale*
+    process.MuonAnalysis
 )
 
 process.TFileService = cms.Service("TFileService",
     fileName = cms.string('vallot.root')
 )
+
+
+
+
+##################################################################
+
