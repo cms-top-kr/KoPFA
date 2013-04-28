29d0e05934932390827e4eb72c58a022bec80f21
tjkim <>
2013-04-28 14:52:52 +0000

    first version of macros at 7 TeV

---
+++ TTbar2b/macros/7TeV/ana7TeVCSVWeight.C
@@ -0,0 +1,138 @@
+#include <iostream>
+#include <map>
+#include "TCut.h"
+#include "TROOT.h"
+#include "TSystem.h"
+
+using namespace std;
+
+void ana7TeVCSVWeight(){
+
+  gSystem->Load("libFWCoreFWLite.so");
+  gSystem->Load("libDataFormatsFWLite.so");
+  gSystem->Load("libDataFormatsPatCandidates.so");
+
+  gSystem->Load("libPhysicsToolsRooStatsCms.so");
+
+  AutoLibraryLoader::enable();
+
+  gSystem->CompileMacro("$CMSSW_BASE/src/KoPFA/CommonTools/macros/TopAnalyzerLite.cc", "k");
+
+  ana("ElEl","TTBB_27Apr2013_CSVWeight/ElEl");
+  ana("MuMu","TTBB_27Apr2013_CSVWeight/MuMu");
+  //ana("MuEl","TTBB_27Apr2013_CSVWeight/MuEl");
+}
+
+
+void ana(string decayMode = "ElEl", string imageOutDir = "")
+{
+
+  //gSystem->CompileMacro("$CMSSW_BASE/src/KoPFA/CommonTools/macros/TopAnalyzerLite.cc", "k");
+  bool createplots = true;
+  TopAnalyzerLite* analyzer = new TopAnalyzerLite(decayMode, imageOutDir, createplots);
+
+  const std::string mcPath = "/afs/cern.ch/work/b/bhlee/public/store/top/ntuple/7TeV/v3_20130412_V00-00-08/";
+  const std::string rdPath = "/afs/cern.ch/work/b/bhlee/public/store/top/ntuple/7TeV/v3_20130412_V00-00-08/";
+ 
+  gROOT->ProcessLine(".L $CMSSW_BASE/src/KoPFA/CommonTools/macros/tdrstyle.C");
+  defaultStyle();
+
+  analyzer->addRealData(rdPath+"/vallot_Run2011"+decayMode+".root", 4982);
+
+  TCut visible = "nGenJet20 >= 4 && nGenbJet20 >=2 && genLep1_pt > 20 && genLep2_pt > 20 && abs( genLep1_eta ) < 2.4 && abs( genLep2_eta ) < 2.4" ;
+  TCut sigcut = "nGenbJet20 >= 4";
+  TCut ttcc = "nGencJet20 >= 2";
+
+  //when you use inclusive sample
+  analyzer->addMCSig("TTbarbb", "t#bar{t} + bb", mcPath+"/vallot_TTbarTuneZ2.root", 165.6, kRed+2, true, visible && sigcut);
+  analyzer->addMCBkg("TTbarcc", "t#bar{t} + cc", mcPath+"/vallot_TTbarTuneZ2.root", 165.6, kRed+1, visible && !sigcut && ttcc );
+  analyzer->addMCBkg("TTbarll", "t#bar{t} + ll", mcPath+"/vallot_TTbarTuneZ2.root", 165.6, kRed, visible && !sigcut && !ttcc);
+  analyzer->addMCBkg("TTbarOthers", "t#bar{t} others", mcPath+"/vallot_TTbarTuneZ2.root", 165.6, kRed-7, !visible);
+
+  analyzer->addMCBkg("Wl", "W #rightarrow l#nu", mcPath+"/vallot_WJetsToLNu.root", 31314, kGreen-3); 
+
+  analyzer->addMCBkg("VV", "Dibosons", mcPath+"/vallot_ZZ.root", 7.4 , kGray+4);
+  analyzer->addMCBkg("WW", "Dibosons", mcPath+"/vallot_WW.root", 4.51, kGray+4);
+  analyzer->addMCBkg("WZ", "Dibosons", mcPath+"/vallot_WZ.root", 0.61, kGray+4);
+
+  analyzer->addMCBkg("SingleTop", "Single top", mcPath+"/vallot_SingleToptW.root", 7.87, kMagenta);
+  analyzer->addMCBkg("SingleTopBar", "Single top", mcPath+"/vallot_SingleTopBartW.root", 7.87, kMagenta);
+  analyzer->addMCBkg("DYll", "Z/#gamma* #rightarrow ll", mcPath+"/vallot_ZJets.root", 3048, kAzure-2);
+  analyzer->addMCBkg("DYll10To50", "Z/#gamma* #rightarrow ll", mcPath+"/vallot_ZJets10To50.root", 11908.83, kAzure-2);
+
+  analyzer->addDataBkg("QCD", "QCD", rdPath+"/vallot_Run2011"+decayMode+".root", 1.0, kYellow);
+
+  //addMonitorPlot
+  //analyzer->addMonitorPlot("nbJet", "nbjets", "b-Jet Multiplicity;b-Jet Multiplicity;Events", 5, 0, 5, 0.1, 3,false);
+  //use common histogram names for top analysis
+  gROOT->ProcessLine(".L addVariables.h");
+  addTopVariables(analyzer); //add Top analysis related variables for plotting
+
+  TCut ZSel = "ZMass > 12 && lep1_relIso03 < 0.15 && lep2_relIso03 < 0.15 && PairSign < 0 && abs(ZMass-91) < 15 && MET < 30 && nJet30 >= 2 && nbjets30_CSVT == 0";
+
+  analyzer->addCutStep(ZSel, "jet1_bDisCSV,jet2_bDisCSV,jet1pt_bDisCSV,jet2pt_bDisCSV", 0.5, "bweight30CSVT", "Step_5");
+
+  //QCD invert isolation for base shape
+  analyzer->replaceDataBkgCut("QCD", "ZMass > 12 && lep1_relIso03 < 0.15 && lep2_relIso03 < 0.15 && PairSign < 0", "ZMass > 12 && lep1_relIso03 > 0.15 && lep2_relIso03 > 0.15 && PairSign < 0");
+
+  //global weight
+  if( decayMode == "MuMu"){
+    analyzer->setEventWeightVar("puweight*0.94");
+  }else if( decayMode == "MuEl"){
+    analyzer->setEventWeightVar("puweight*0.95");
+  }else if( decayMode == "ElEl"){
+    analyzer->setEventWeightVar("puweight*0.96");
+  }
+
+  //TTBB weight for DY and QCD samples
+  if( decayMode == "ElEl"){
+    analyzer->setEventWeight("QCD", "Step_1", 0.311);
+    analyzer->setEventWeight("QCD", "Step_2", 0.311);
+    analyzer->setEventWeight("QCD", "Step_3", 0.591);
+    analyzer->setEventWeight("QCD", "Step_4", 0.077);
+    analyzer->setEventWeight("QCD", "Step_5", 0.0587);
+/*
+    analyzer->setEventWeight("DYll", "Step_1", 1.0);
+    analyzer->setEventWeight("DYll", "Step_2", 1.0);
+    analyzer->setEventWeight("DYll", "Step_3", 1.410);
+    analyzer->setEventWeight("DYll", "Step_4", 1.231);
+    analyzer->setEventWeight("DYll", "Step_5", 1.459);
+
+    analyzer->setEventWeight("DYll10To50", "Step_1", 1.0);
+    analyzer->setEventWeight("DYll10To50", "Step_2", 1.0);
+    analyzer->setEventWeight("DYll10To50", "Step_3", 1.410);
+    analyzer->setEventWeight("DYll10To50", "Step_4", 1.231);
+    analyzer->setEventWeight("DYll10To50", "Step_5", 1.459);
+*/
+  }else if( decayMode == "MuMu") {
+    analyzer->setEventWeight("QCD", "Step_1", 0.008);
+    analyzer->setEventWeight("QCD", "Step_2", 0.008);
+    analyzer->setEventWeight("QCD", "Step_3", 0.016);
+    analyzer->setEventWeight("QCD", "Step_4", 0.007);
+    analyzer->setEventWeight("QCD", "Step_5", 0.0043);
+/*
+    analyzer->setEventWeight("DYll", "Step_1", 1.0);
+    analyzer->setEventWeight("DYll", "Step_2", 1.0);
+    analyzer->setEventWeight("DYll", "Step_3", 1.362);
+    analyzer->setEventWeight("DYll", "Step_4", 1.231);
+    analyzer->setEventWeight("DYll", "Step_5", 1.128);
+ 
+    analyzer->setEventWeight("DYll10To50", "Step_1", 1.0);
+    analyzer->setEventWeight("DYll10To50", "Step_2", 1.0);
+    analyzer->setEventWeight("DYll10To50", "Step_3", 1.362);
+    analyzer->setEventWeight("DYll10To50", "Step_4", 1.231);
+    analyzer->setEventWeight("DYll10To50", "Step_5", 1.128);
+*/
+  }else{
+    analyzer->setEventWeight("QCD", "Step_1", 0.020);
+    analyzer->setEventWeight("QCD", "Step_2", 0.020);
+    analyzer->setEventWeight("QCD", "Step_3", 0.002);
+    analyzer->setEventWeight("QCD", "Step_4", 0.002);
+    analyzer->setEventWeight("QCD", "Step_5", 0.0018);
+  }
+
+  analyzer->applyCutSteps();
+  analyzer->saveHistograms();
+  analyzer->printCutFlow();
+}
+
---
+++ TTbar2b/macros/7TeV/makePlotsCSVWeight.C
@@ -0,0 +1,149 @@
+#include <iostream>
+#include <iomanip>
+
+TFile* f;
+TString outDirName = ".";
+
+const int nBkg = 9;
+const char* bkgNames[]  = {"hMCSig_TTbarbb", "hMC_TTbarcc", "hMC_TTbarll","hMC_TTbarOthers", "hMC_Wl",             "hMC_VV",   "hMC_SingleTop", "hMC_DYll", "hDataBkg_QCD"};
+const char* bkgLabels[] = {"t#bar{t}+bb",    "t#bar{t}+cc", "t#bar{t}+ll","t#bar{t} others", "W #rightarrow l#nu", "Dibosons", "Single top",    "Z/#gamma* #rightarrow ll", "QCD" };
+const Color_t color[]   = { kBlue+2,         kOrange+2,      kRed,        kRed-7,            kGreen-3,              kGray+4,    kMagenta,       kAzure-2                   ,kYellow };
+const int     style[]   = { 3354,            1001,           1001,        1001,              1001,                  1001,       1001,           1001,                      1001 };
+const int nExclude = 1;
+const char* Exclude[] = { "hDataBkg_QCD" }
+
+const int nSig = 1;
+const char* sigNames[] = {"hMCSig_TTbarbb"};
+const char* sigLabels[] = {"t#bar{t}+bb"};
+const Color_t color_sig[] = {kBlue+2};
+const int style_sig[] = {3354};
+bool stackSig = true;
+
+void cutStepPlots(const char* cutStep, const char* histName, const char* histTitle,
+                  double minY, double maxY, bool doLogY);
+TLegend* buildLegend(double y);
+TPaveText* getHeader(double lumi, TString channelName = "");
+
+void makePlotsCSVWeight(TString noteNumber = "TTBB_27Apr2013_CSVWeight/v1")
+{
+
+  gROOT->ProcessLine(".L $CMSSW_BASE/src/KoPFA/CommonTools/macros/tdrstyle.C");
+  defaultStyle();
+
+  TString path = "TTBB_27Apr2013_CSVWeight";
+
+  f = TFile::Open(path+"/merged.root");
+
+  outDirName += "/"+noteNumber;
+  gSystem->Exec("mkdir "+outDirName);
+
+  cutStepPlots("Step_5", "jet1_bDisCSV", "b-Discriminator (CSV)", "Events/0.05", "b-Discriminator (CSV)", "LL", 0.1, 100000, false);
+  cutStepPlots("Step_5", "jet2_bDisCSV", "b-Discriminator (CSV)", "Events/0.05", "b-Discriminator (CSV)", "LL", 0.1, 170000, false);
+  cutStepPlots("Step_5", "jet1_bDisCSV", "b-Discriminator (CSV)", "Events/0.05", "b-Discriminator (CSV)", "LL", 0.1, 400000000, true, "log");
+  cutStepPlots("Step_5", "jet2_bDisCSV", "b-Discriminator (CSV)", "Events/0.05", "b-Discriminator (CSV)", "LL", 0.1, 70000000, true, "log");
+
+}
+
+void cutStepPlots(const char* cutStep, const char* histName, const char* histTitle, const TString & ytitle, const TString & xtitle, const TString & decayMode, 
+                  double minY, double maxY, bool doLogY, const TString & postfix = "")
+{
+  TString channelName = "All";
+  if( decayMode == "MM") channelName = "#mu#mu";
+  if( decayMode == "EE") channelName = "ee";
+  if( decayMode == "ME") channelName = "e#mu";
+  
+  TH1F* hData = (TH1F*)f->Get(Form("%s/hData_%s_%s", cutStep, cutStep, histName));
+  if ( !hData ) { cout << Form("%s/hData_%s_%s", cutStep, cutStep, histName) << "\n"; return; }
+
+  THStack* hStack = new THStack(TString("h_")+cutStep+"_"+histName, histTitle);
+
+  TLegend* leg = buildLegend( (double) nExclude*0.02 );
+
+  if ( hData->GetEntries() > 0 ) leg->AddEntry(hData, "Data", "p");
+
+  TH1F* hs[nBkg];
+ 
+  for ( int i=0; i<nBkg; ++i )
+  {
+    TH1F* h = (TH1F*)f->Get(Form("%s/%s_%s_%s", cutStep, bkgNames[i], cutStep, histName));
+    hs[i] = h;
+    h->SetFillColor(color[i]);
+    h->SetFillStyle(style[i]);
+
+    ///remove indicated backgrounds
+    bool exclude = false;
+    for(int k = 0 ; k < nExclude ; k++) {
+      if( bkgNames[i] == Exclude[k] ) exclude = true;
+    }
+    if( exclude ) continue;
+
+    hStack->Add(h);
+  }
+
+  for ( int i=nBkg-1; i>=0; --i )
+  {
+    ///remove indicated backgrounds
+    bool exclude = false;
+    for(int k = 0 ; k < nExclude ; k++) {
+      if( bkgNames[i] == Exclude[k] ) exclude = true;
+    }
+    if( exclude ) continue;
+
+    leg->AddEntry(hs[i], bkgLabels[i], "f");
+  }
+
+
+  // Be ready for draw
+  hData->SetMinimum(minY);
+  hData->SetMaximum(maxY*0.25);
+
+  TCanvas* c = new TCanvas(Form("c%s_%s_%s%s", decayMode.Data(), cutStep , histName, postfix.Data() ), Form("c%s_%s_%s%s", decayMode.Data(), cutStep , histName, postfix.Data()), 1);
+  if ( doLogY ) c->SetLogy();
+  hData->Draw();
+  hData->GetXaxis()->SetTitle(xtitle);
+  hData->GetYaxis()->SetTitle(ytitle);
+  leg->Draw();
+  getHeader(5.0, Form("%s channel",channelName.Data()))->Draw();
+  hStack->Draw("same");
+  hData->Draw("same");
+  hData->Draw("sameaxis");
+  c->Print(Form("%s/c%s_%s_%s%s.eps", outDirName.Data(), decayMode.Data(), cutStep, histName, postfix.Data() ));
+  c->Print(Form("%s/c%s_%s_%s%s.pdf", outDirName.Data(), decayMode.Data(), cutStep, histName, postfix.Data() ));
+
+}
+
+TLegend* buildLegend(double y)
+{
+  TLegend* leg = new TLegend(0.73,0.55+y,0.85,0.88,NULL,"brNDC");
+
+  leg->SetBorderSize(1);
+  leg->SetTextFont(62);
+  leg->SetTextSize(0.04);
+  leg->SetLineColor(0);
+  leg->SetLineStyle(1);
+  leg->SetLineWidth(1);
+  leg->SetFillColor(0);
+  leg->SetFillStyle(1001);
+
+  return leg;
+}
+
+TPaveText* getHeader(double lumi, TString channelName)
+{
+  TPaveText* pt = new TPaveText(0.18,0.75,0.18,0.88,"brNDC");
+
+  pt->SetBorderSize(1);
+  pt->SetTextFont(42);
+  pt->SetTextSize(0.04);
+  pt->SetLineColor(0);
+  pt->SetLineStyle(1);
+  pt->SetLineWidth(1);
+  pt->SetFillColor(0);
+  pt->SetFillStyle(1001);
+  pt->SetTextAlign(12);
+  pt->AddText("CMS Preliminary");
+  pt->AddText(Form("%.1f fb^{-1} at  #sqrt{s} = 7 TeV", lumi));
+  if ( channelName != "" ) pt->AddText(channelName);
+
+  return pt;
+}
