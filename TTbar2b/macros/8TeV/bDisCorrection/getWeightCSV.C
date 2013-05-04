3371adc7fccf63c59cecce6c025de4fe774fbcfa
tjkim <>
2013-05-04 15:53:34 +0000

    update to remove Z+b process

--- TTbar2b/macros/8TeV/bDisCorrection/getWeightCSV.C
+++ TTbar2b/macros/8TeV/bDisCorrection/getWeightCSV.C
@@ -6,37 +6,60 @@ void getWeightCSV(){
   TString path = "../TTBB_27Apr2013_CSVWeight";
   TFile * f = new TFile(path+"/merged.root");
 
-  double XBins[] = {0.0, 0.05,  0.1, 0.15,  0.2, 0.25, 0.3,0.35, 0.4, 0.45, 0.5, 0.6, 0.7, 0.8,0.9};//8 bins
+  double XBins[] = {0.0, 0.05,  0.1, 0.15,  0.2, 0.25, 0.3,0.35, 0.4, 0.5, 0.6, 0.7, 0.8, 1.0};//8 bins
   int nX = sizeof(XBins)/sizeof(double) - 1;
 
-  double XBins2[] = {0.0,  0.1, 0.15, 0.2, 0.25, 0.3, 0.4, 0.5, 0.6, 0.7, 0.9};//8 bins
+  double XBins2[] = {0.0, 0.05,  0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.5, 0.6, 0.8, 1.0};//8 bins
   int nX2 = sizeof(XBins2)/sizeof(double) - 1;
 
   plot(f, "jet1_bDisCSV","csv_first", nX, XBins);
   plot(f, "jet2_bDisCSV","csv_second", nX2, XBins2);
 
+  //plot(f, "jet1_bDis","ptorder_first", nX, XBins);
+  //plot(f, "jet2_bDis","ptorder_second", nX2, XBins2);
+
 }
 
 void plot(TFile *f, const TString & variable, const TString & name, int nX, double XBins[] ){
 
-  TH1F * h_DataSub       = (TH1F * ) f->Get(Form("Step_5/hDataSub_Step_5_%s", variable.Data()));
-  TH1F * h_MCSig_TTbarbb = (TH1F * ) f->Get(Form("Step_5/hMCSig_TTbarbb_Step_5_%s", variable.Data()));
-  TH1F * h_MC_TTbarcc    = (TH1F * ) f->Get(Form("Step_5/hMC_TTbarcc_Step_5_%s", variable.Data()));
-  TH1F * h_MC_TTbarll    = (TH1F * ) f->Get(Form("Step_5/hMC_TTbarll_Step_5_%s", variable.Data()));
-  TH1F * h_MC_TTbarOthers    = (TH1F * ) f->Get(Form("Step_5/hMC_TTbarOthers_Step_5_%s", variable.Data()));
-  TH1F * h_MC_DYll    = (TH1F * ) f->Get(Form("Step_5/hMC_DYll_Step_5_%s", variable.Data()));
+  TH1F * h_DataSub       = (TH1F * ) f->Get(Form("NoTag/hDataSub_NoTag_%s", variable.Data()));
+  TH1F * h_MCSig_TTbar = (TH1F * ) f->Get(Form("NoTag/hMCSig_TTbar_NoTag_%s", variable.Data()));
+  //TH1F * h_MCSig_TTbarbb = (TH1F * ) f->Get(Form("NoTag/hMCSig_TTbarbb_NoTag_%s", variable.Data()));
+  //TH1F * h_MC_TTbarcc    = (TH1F * ) f->Get(Form("NoTag/hMC_TTbarcc_NoTag_%s", variable.Data()));
+  //TH1F * h_MC_TTbarll    = (TH1F * ) f->Get(Form("NoTag/hMC_TTbarll_NoTag_%s", variable.Data()));
+  //TH1F * h_MC_TTbarOthers    = (TH1F * ) f->Get(Form("NoTag/hMC_TTbarOthers_NoTag_%s", variable.Data()));
+  //TH1F * h_MC_DYll    = (TH1F * ) f->Get(Form("NoTag/hMC_DYll_NoTag_%s", variable.Data()));
+  TH1F * h_MC_DYb    = (TH1F * ) f->Get(Form("NoTag/hMC_DYb_NoTag_%s", variable.Data()));
+  TH1F * h_MC_DYc    = (TH1F * ) f->Get(Form("NoTag/hMC_DYc_NoTag_%s", variable.Data()));
+  TH1F * h_MC_DYLF    = (TH1F * ) f->Get(Form("NoTag/hMC_DYLF_NoTag_%s", variable.Data()));
 
+  TH1F * h_MC_DYb_sys = h_MC_DYb->Clone();
+  h_MC_DYb_sys->Scale(0.2);  
+
+  //remove Z+b process 
   TH1F * h_Data = h_DataSub->Clone();
-  h_Data->Add(h_MC_DYll);  
-  h_Data->Add(h_MCSig_TTbarbb, -1);  
-  TH1F * h_MC = h_MC_DYll->Clone();
+  h_Data->Add(h_MCSig_TTbar, -1);  
+  h_Data->Add(h_MC_DYLF);
+  h_Data->Add(h_MC_DYc);
+  //h_Data->Add(h_MC_DYb);
+  TH1F * h_DataUp = h_Data->Clone();
+  TH1F * h_DataDw = h_Data->Clone();
+  h_DataUp->Add(h_MC_DYb_sys,1);
+  h_DataDw->Add(h_MC_DYb_sys,-1);
+  TH1F * h_MC = h_MC_DYLF->Clone();
+  h_MC->Add(h_MC_DYc);
+  //h_MC->Add(h_MC_DYb);
 
   h_MC = (TH1F*) h_MC->Rebin(nX, "h_MC", XBins);
   h_Data = (TH1F*) h_Data->Rebin(nX, "h_Data", XBins);
+  h_DataUp = (TH1F*) h_DataUp->Rebin(nX, "h_DataUp", XBins);
+  h_DataDw = (TH1F*) h_DataDw->Rebin(nX, "h_DataDw", XBins);
  
   TCanvas * c = new TCanvas(Form("c_norm_%s", name.Data()) , Form("c_norm_%s", name.Data()) ,1);
   h_MC->Scale(1.0/h_MC->Integral());
   h_Data->Scale(1.0/h_Data->Integral());
+  h_DataUp->Scale(1.0/h_DataUp->Integral());
+  h_DataDw->Scale(1.0/h_DataDw->Integral());
   h_MC->SetTitle(""); 
   h_MC->GetYaxis()->SetTitle("Normalized Entries"); 
   h_MC->Draw();
@@ -64,11 +87,11 @@ void plot(TFile *f, const TString & variable, const TString & name, int nX, doub
   h_SF->Sumw2();
   h_SF->Divide(h_Data, h_MC);
 
-  TF1* functioncsv = new TF1("functioncsv", "pol2");
+  TF1* functioncsv = new TF1("functioncsv", "pol4");
   functioncsv->SetLineColor(2);
   h_SF->Fit(functioncsv);
-  h_SF->SetMinimum(0.0);
-  h_SF->SetMaximum(3.5);
+  h_SF->SetMinimum(0.4);
+  h_SF->SetMaximum(2.0);
   h_SF->GetXaxis()->SetTitle("b-Discriminator (CSV)");
   h_SF->GetYaxis()->SetTitle("Scale Factor (Data/MC)");
   h_SF->SetStats(0);
@@ -81,9 +104,29 @@ void plot(TFile *f, const TString & variable, const TString & name, int nX, doub
   TH1F* functioncsv_err = (TH1F*) h_SF->Clone();
   (TVirtualFitter::GetFitter())->GetConfidenceIntervals(functioncsv_err,0.68);
   functioncsv_err->SetFillColor(2);
-  functioncsv_err->SetFillStyle(3005);
+  functioncsv_err->SetFillStyle(3001);
   functioncsv_err->SetStats(0);
   functioncsv_err->Draw("e3 same");
+
+  TH1F * h_SFUp = h_MC->Clone();
+  TH1F * h_SFDw = h_MC->Clone();
+  h_SFUp->Reset();
+  h_SFDw->Reset();
+  h_SFUp->Sumw2();
+  h_SFDw->Sumw2();
+  h_SFUp->Divide(h_DataUp, h_MC);
+  h_SFDw->Divide(h_DataDw, h_MC);
+
+  TF1* functioncsvUp = new TF1("functioncsvUp", "pol4");
+  functioncsvUp->SetLineColor(4);
+  h_SFUp->Fit(functioncsvUp,"N");
+  functioncsvUp->Draw("same");
+
+  TF1* functioncsvDw = new TF1("functioncsvDw", "pol4");
+  functioncsvDw->SetLineColor(4);
+  h_SFDw->Fit(functioncsvDw,"N");
+  functioncsvDw->Draw("same");
+
   TLatex *label= new TLatex;
   label->SetNDC();
   label->SetTextSize(0.05);
