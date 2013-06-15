0a3653792110739425a85e79944753eaf0c13425
jhgoh <>
2013-06-15 19:52:37 +0000

    cleanup

--- RivetAnalyzer/test/runRivetAnalyzer_cfg.py
+++ RivetAnalyzer/test/runRivetAnalyzer_cfg.py
@@ -2,26 +2,9 @@ import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("RIVET")
 
-# import of standard configurations
-process.load('Configuration.StandardSequences.Services_cff')
-process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
-process.load('FWCore.MessageService.MessageLogger_cfi')
-process.load('Configuration.EventContent.EventContent_cff')
-process.load('SimGeneral.MixingModule.mixNoPU_cfi')
-process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
-process.load('Configuration.StandardSequences.MagneticField_38T_cff')
-process.load('Configuration.StandardSequences.Generator_cff')
-process.load('IOMC.EventVertexGenerators.VtxSmearedRealistic8TeVCollision_cfi')
-process.load('GeneratorInterface.Core.genFilterSummary_cff')
-process.load('Configuration.StandardSequences.EndOfProcess_cff')
-#process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
-#from Configuration.AlCa.GlobalTag import GlobalTag
-#process.GlobalTag = GlobalTag(process.GlobalTag, 'START53_V7C::All', '')
-
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
 process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32(10000)
 process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))
-
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(),
 )
@@ -32,7 +15,7 @@ cmgFullVer = 'V5_B/PAT_CMG_V5_13_0'
 for i in range(1,1184):
     process.source.fileNames.append("%s%s/%s/patTuple_%d.root" % (cmgBaseDir, sampleName, cmgFullVer, i))
 
-### Setup pythia for the hadronization
+### Rebuild HepMC
 #process.load('Configuration.GenProduction.EightTeV.Hadronizer_MgmMatchTuneZ2star_8TeV_madgraph_tauola_cff')
 process.load('KoPFA.RivetAnalyzer.GenParticles2HepMC_cfi')
 
