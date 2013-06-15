1ccf1378ef02b2228483fe597b5420af830eab62
jhgoh <>
2013-06-15 19:49:07 +0000

    GenParticle from AODSIM, use cmg-patTuple

--- RivetAnalyzer/test/runRivetAnalyzer_cfg.py
+++ RivetAnalyzer/test/runRivetAnalyzer_cfg.py
@@ -19,32 +19,30 @@ process.load('Configuration.StandardSequences.EndOfProcess_cff')
 #process.GlobalTag = GlobalTag(process.GlobalTag, 'START53_V7C::All', '')
 
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
-process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32(1000)
-
-process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(10000))
+process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32(10000)
+process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))
 
 process.source = cms.Source("PoolSource",
-    fileNames = cms.untracked.vstring(
-        #'file:E87AF0D3-4C4D-E211-95FA-003048D37694.root',
-        #'file:out.root',
-        'file:/data/local/data01/common/CMS/AODSIM/Summer12/TTJets_MassiveBinDECAY_TuneZ2star_8TeV-madgraph-tauola/PU_S7_START52_V9-v2/A6323CBD-A0B8-E111-97B4-002618943958.root',
-    ),
+    fileNames = cms.untracked.vstring(),
 )
 
+cmgBaseDir = '/store/cmst3/user/cmgtools/CMG'
+sampleName = '/TTJets_FullLeptMGDecays_8TeV-madgraph-tauola/Summer12_DR53X-PU_S10_START53_V7C-v2/AODSIM'
+cmgFullVer = 'V5_B/PAT_CMG_V5_13_0'
+for i in range(1,1184):
+    process.source.fileNames.append("%s%s/%s/patTuple_%d.root" % (cmgBaseDir, sampleName, cmgFullVer, i))
+
 ### Setup pythia for the hadronization
-process.load('Configuration.GenProduction.EightTeV.Hadronizer_MgmMatchTuneZ2star_8TeV_madgraph_tauola_cff')
-process.generator = cms.EDProducer("GenParticles2HepMCConverter",
-    genParticles = cms.InputTag("genParticles"),
-    genRunInfo = cms.InputTag("generator"),
-    genEventInfo = cms.InputTag("generator"),
-)
+#process.load('Configuration.GenProduction.EightTeV.Hadronizer_MgmMatchTuneZ2star_8TeV_madgraph_tauola_cff')
+process.load('KoPFA.RivetAnalyzer.GenParticles2HepMC_cfi')
 
 ## Setup RIVET analyzer
 process.load("GeneratorInterface.RivetInterface.rivetAnalyzer_cfi")
+
+process.p = cms.Path(process.rivetAnalyzer)
 process.rivetAnalyzer.AnalysisNames = cms.vstring('CMS_TOP_12_028')
 process.rivetAnalyzer.CrossSection= cms.double('157.5')
+process.rivetAnalyzer.OutputFile = 'rivet.aida'
 
-#process.p = cms.Path(process.generator * process.rivetAnalyzer)
-#Pprocess.p = cms.Path(process.rivetAnalyzer)
 process.p = cms.Path(process.generator * process.rivetAnalyzer)
 
