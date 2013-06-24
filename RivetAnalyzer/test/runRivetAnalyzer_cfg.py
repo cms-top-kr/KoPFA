a74b6ff09e43dd41dba61fa615ea5f5ecb1a5d79
jhgoh <>
2013-06-24 10:21:37 +0000

    ghost b hadron reclustering

--- RivetAnalyzer/test/runRivetAnalyzer_cfg.py
+++ RivetAnalyzer/test/runRivetAnalyzer_cfg.py
@@ -15,17 +15,24 @@ cmgFullVer = 'V5_B/PAT_CMG_V5_13_0'
 for i in range(1,1184):
     process.source.fileNames.append("%s%s/%s/patTuple_%d.root" % (cmgBaseDir, sampleName, cmgFullVer, i))
 
+### Add ghost B hadron
+process.load('KoPFA.CommonTools.genJetsWithGhostBHadrons_cff')
+process.genParticlesWithGhostB.userPdgId = 7
+
 ### Rebuild HepMC
 #process.load('Configuration.GenProduction.EightTeV.Hadronizer_MgmMatchTuneZ2star_8TeV_madgraph_tauola_cff')
+process.load("SimGeneral.HepPDTESSource.pythiapdt_cfi")
 process.load('KoPFA.RivetAnalyzer.GenParticles2HepMC_cfi')
+process.generator.genParticles = 'genParticlesWithGhostB'
 
 ## Setup RIVET analyzer
 process.load("GeneratorInterface.RivetInterface.rivetAnalyzer_cfi")
 
 process.p = cms.Path(process.rivetAnalyzer)
 process.rivetAnalyzer.AnalysisNames = cms.vstring('CMS_TOP_12_028')
-process.rivetAnalyzer.CrossSection= cms.double('157.5')
+process.rivetAnalyzer.CrossSection = cms.double(13.43)
 process.rivetAnalyzer.OutputFile = 'rivet.aida'
 
-process.p = cms.Path(process.generator * process.rivetAnalyzer)
+process.p = cms.Path(process.genParticlesWithGhostB * process.generator * process.rivetAnalyzer)
+#process.p = cms.Path(process.generator * process.rivetAnalyzer)
 
