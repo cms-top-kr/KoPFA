4de7dcaf04c7e944aa9f04bac37ca3e42f18e738
Geonmo <ry840901@gmail.com>
2013-11-27 23:46:30 +0900

    Delete DiMuonAnalyzer due to obsolte

--- DiMuonAnalyzer/test/pileup/noPileupSequence/dimuonanalyzer_nopileup_1_cfg.py
+++
@@ -1,8 +0,0 @@
-from KoPFA.DiMuonAnalyzer.dimuonanalyzer_cff import process
-
-import FWCore.ParameterSet.Config as cms
-
-process.load("KoPFA.DiMuonAnalyzer.RD.NoPileupSequence.patTuple_Commissioning10_cff")
-
-process.VertexFilter.min = 1
-process.VertexFilter.max = 1
--- DiMuonAnalyzer/test/pileup/noPileupSequence/dimuonanalyzer_pileup_3_cfg.py
+++
@@ -1,8 +0,0 @@
-from KoPFA.DiMuonAnalyzer.dimuonanalyzer_cff import process
-
-import FWCore.ParameterSet.Config as cms
-
-process.load("KoPFA.DiMuonAnalyzer.RD.NoPileupSequence.patTuple_Run2010A_Jul16thReReco_cff")
-
-process.VertexFilter.min = 2
-process.VertexFilter.max = 9999
